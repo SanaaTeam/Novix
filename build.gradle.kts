7f17f28bdfbe893dbcb887d8a2dc1a69e9371658
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-03 21:52:46 +0300

    use all project instead of sub project

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kover)
 }
-subprojects {
+allprojects {
     apply(plugin = "org.jetbrains.kotlinx.kover")
     kover {
         reports {
@@ -22,16 +22,66 @@ subprojects {
                     )
                 }
             }
-
-
-
-            verify {
-                rule {
-                    bound {
-                        minValue = 80
+            total {
+                verify {
+                    rule {
+                        bound {
+                            minValue = 80
+                        }
                     }
                 }
             }
         }
     }
 }
+/*
+dependencies {
+    kover(projects.domain.authentication.repository)
+    kover(projects.domain.movies.repository)
+    kover(projects.domain.actors.repository)
+    kover(projects.domain.series)
+    kover(projects.domain.savedContent.repository)
+    kover(projects.domain.search.repository)
+    kover(projects.data.search.repository)
+    kover(projects.data.savedContent.repository)
+    kover(projects.data.userProfile.repository)
+    kover(projects.data.remoteDataSource)
+    kover(projects.data.localDataSource)
+    kover(projects.data.actors.dataSource)
+    kover(projects.data.actors.repository)
+    kover(projects.data.authentication.dataSource.local)
+    kover(projects.data.movies.dataSource.remote)
+    kover(projects.data.savedContent.dataSource.remote)
+    kover(projects.data.search.dataSource.remote)
+    kover(projects.data.series.dataSource.remote)
+    kover(projects.data.savedContent.dataSource.remote)
+    kover(projects.data.actors.dataSource.local)
+    kover(projects.data.authentication.dataSource.local)
+    kover(projects.data.movies.dataSource.local)
+    kover(projects.data.savedContent.dataSource.local)
+    kover(projects.data.search.dataSource.local)
+    kover(projects.data.series.dataSource.local)
+    kover(projects.data.userProfile.dataSource.local)
+    kover(projects.app)
+    kover(projects.domain)
+    kover(projects.feature.authentication.presentation)
+    kover(projects.feature.authentication.api)
+    kover(projects.feature.home.api)
+    kover(projects.feature.home.presentation)
+    kover(projects.feature.userProfile.api)
+    kover(projects.feature.userProfile.presentation)
+    kover(projects.feature.savedContent.api)
+    kover(projects.feature.savedContent.presentation)
+    kover(projects.feature.playlists.api)
+    kover(projects.feature.playlists.presentation)
+    kover(projects.feature.search.api)
+    kover(projects.feature.search.presentation)
+    kover(projects.feature.mediaDetails.api)
+    kover(projects.feature.mediaDetails.presentation)
+    kover(projects.feature.onboarding.api)
+    kover(projects.feature.onboarding.presentation)
+    kover(projects.data.authentication.repository)
+    kover(projects.data.movies.repository)
+    kover(projects.data.series.repository)
+    kover(projects.data.search.repository)
+}*/
