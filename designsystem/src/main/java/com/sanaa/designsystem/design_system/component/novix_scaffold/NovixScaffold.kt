62a3f3c3353c3dfb97e68dc5a3498c9bc1ded183
haider <haideraljawasim@gmail.com>
2025-07-04 05:21:28 +0300

    delete unused imports in NovixScaffold

--- designsystem/src/main/java/com/sanaa/designsystem/design_system/component/novix_scaffold/NovixScaffold.kt
+++ designsystem/src/main/java/com/sanaa/designsystem/design_system/component/novix_scaffold/NovixScaffold.kt
@@ -1,8 +1,5 @@
 package com.sanaa.designsystem.design_system.component.novix_scaffold
 
-import android.graphics.BlurMaskFilter
-import android.graphics.Paint
-import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
@@ -13,17 +10,10 @@ import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
-import androidx.compose.ui.graphics.asAndroidPath
-import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.graphics.withSave
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.design_system.theme.NovixTheme
 import com.sanaa.designsystem.design_system.theme.Theme
-import kotlin.math.sqrt
 
 @Composable
 fun NovixScaffold(
@@ -34,7 +24,7 @@ fun NovixScaffold(
     bottomBar: @Composable () -> Unit = {},
     contentBackground: Color = Theme.colors.surface,
     contentColor: Color = contentColorFor(backgroundColor),
-    backgroundShapes:  @Composable () -> Unit = {},
+    backgroundShapes: @Composable () -> Unit = {},
     content: @Composable () -> Unit,
 ) {
     Scaffold(
@@ -52,7 +42,7 @@ fun NovixScaffold(
                 .background(contentBackground)
                 .padding(innerPadding)
         ) {
-           backgroundShapes()
+            backgroundShapes()
             content()
         }
     }
@@ -63,7 +53,9 @@ fun NovixScaffold(
 @Composable
 private fun PreviewNovixScaffold() {
     NovixTheme(true) {
-        NovixScaffold {
+        NovixScaffold(
+            backgroundShapes = { NovixBackgroundShapes() }
+        ) {
             Box(modifier = Modifier.fillMaxSize())
         }
     }
