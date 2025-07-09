558c389d61899bf13f1180e2d10c6baa54115065
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-10 02:46:08 +0300

    add search section

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/SearchScreen.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/SearchScreen.kt
@@ -1,21 +1,42 @@
 package com.sanaa.presentation.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.design_system.component.novix_scaffold.NovixScaffold
+import com.sanaa.designsystem.design_system.component.top_bar.AppTopBar
+import com.sanaa.presentation.screen.componants.SearchSection
 
 @Composable
 fun SearchScreen(modifier: Modifier = Modifier) {
-    
+
 }
 
 @Composable
-fun SearchScreenContent(modifier: Modifier = Modifier) {
-    
+fun SearchScreenContent() {
+    NovixScaffold(
+        modifier = Modifier.statusBarsPadding(),
+        topBar = {
+            AppTopBar(
+                screenTitle = "Search"
+            )
+        }
+    ){
+        Column(
+            modifier = Modifier
+                .padding(top = 50.dp)
+        ) {
+            SearchSection()        }
+
+    }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun SearchScreenPreview() {
-    
+    SearchScreenContent()
 }
