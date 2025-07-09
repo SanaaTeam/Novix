5954848d6ea6414913415556b95b2c758c861655
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-09 18:24:47 +0300

    replace list of genres in media filters from string to enum be list of enum of genre

--- domain/vod/src/main/java/usecase/search/MediaFilters.kt
+++ domain/vod/src/main/java/usecase/search/MediaFilters.kt
@@ -1,7 +1,9 @@
 package usecase.search
 
+import entity.Genre
+
 data class MediaFilters(
     val releaseYear: Int? = null,
-    val genres: List<String>? = null,
+    val genres: List<Genre>? = null,
     val imdbRating: Float? = null
 )
