e43895c07f5833097323fdace05750551488399a
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-09 18:55:25 +0300

    implement SearchRemoteDataSource

---
+++ data/remote_data_source/search/src/main/java/com/sanaa/search/network/SearchRemoteDataSource.kt
@@ -0,0 +1,45 @@
+package com.sanaa.search.network
+
+import com.sanaa.search.dto.ActorSearchDto
+import com.sanaa.search.dto.MovieSearchDto
+import com.sanaa.search.dto.TvSearchDto
+import com.sanaa.search.response.SearchResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+
+
+class SearchRemoteDataSource(private val client: HttpClient) {
+    private val BASE_URL = "https://api.themoviedb.org/3"
+    private val API_KEY = "75d83fe5482f3d79987ef22e6dae9800"
+
+    suspend fun searchPerson(query: String, page: Int = 1): SearchResponse<ActorSearchDto> {
+        return client.get("$BASE_URL/search/person") {
+            parameter("query", query)
+            parameter("page", page)
+            parameter("language", "en-US")
+            parameter("include_adult", false)
+            parameter("api_key", API_KEY)
+        }.body()
+    }
+
+    suspend fun searchTv(query: String, page: Int = 1): SearchResponse<TvSearchDto> {
+        return client.get("$BASE_URL/search/tv") {
+            parameter("query", query)
+            parameter("page", page)
+            parameter("language", "en-US")
+            parameter("api_key", API_KEY)
+        }.body()
+    }
+
+    suspend fun searchMovie(query: String, page: Int = 1): SearchResponse<MovieSearchDto> {
+        return client.get("$BASE_URL/search/movie") {
+            parameter("query", query)
+            parameter("page", page)
+            parameter("language", "en-US")
+            parameter("api_key", API_KEY)
+        }.body()
+    }
+}
