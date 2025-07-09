0969cf631363e8bf9f6b0f5e2c9c1c1240600766
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-09 10:32:19 +0300

    remove mapper from language enum class in domain models

--- domain/vod/src/main/java/entity/Language.kt
+++ domain/vod/src/main/java/entity/Language.kt
@@ -3,9 +3,4 @@ package entity
 enum class Language(val code: String) {
     ENGLISH("en"),
     ARABIC("ar");
-
-    companion object {
-        fun fromCode(code: String): Language? =
-            Language.entries.find { it.code == code }
-    }
 }
