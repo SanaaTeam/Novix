7dfb08cdb87be9cd504ff76d29e77887008e99c4
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-07-10 18:28:07 +0300

    add default values for SearchResponse fields

--- data/remote_data_source/search/src/main/java/com/sanaa/search/response/SearchResponse.kt
+++ data/remote_data_source/search/src/main/java/com/sanaa/search/response/SearchResponse.kt
@@ -4,9 +4,16 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
- data class SearchResponse<T>(
-     val page: Int,
-     val results: List<T>,
-     @SerialName("total_pages") val totalPages: Int,
-     @SerialName("total_results") val totalResults: Int
- )
+data class SearchResponse<T>(
+    @SerialName("page")
+    val page: Int = 1,
+
+    @SerialName("results")
+    val results: List<T> = emptyList(),
+
+    @SerialName("total_pages")
+    val totalPages: Int = 1,
+
+    @SerialName("total_results")
+    val totalResults: Int = 0
+)
