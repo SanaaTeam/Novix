a9e6bde40ac74a9777a5c84f76a44f53e2ad82d0
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-10 17:56:01 +0300

    edit string resources files

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/SearchHistoryContent.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/SearchHistoryContent.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.R
@@ -31,14 +32,13 @@ fun SearchHistoryContent(
     Column(modifier = Modifier.padding(top = 12.dp)) {
 
         SectionHeader(
-            title = "Recent viewed",
-            actionText = "Clear all",
+            title = stringResource(R.string.recent_viewed),
+            actionText = stringResource(R.string.clear_all),
             onActionClick = onClearRecentViewClicked
         )
         LazyRow(
             modifier = Modifier
-                .padding(start = 16.dp, bottom = 24.dp)
-                .fillMaxWidth(),
+                .padding(bottom = 24.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             itemsIndexed(recentViewed) {index, item ->
@@ -53,8 +53,8 @@ fun SearchHistoryContent(
             }
         }
         SectionHeader(
-            title = "Recent search",
-            actionText = "Clear all",
+            title = stringResource(R.string.recent_search),
+            actionText = stringResource(R.string.clear_all),
             onActionClick = onClearRecentSearchClicked
         )
         LazyColumn ( modifier = Modifier
