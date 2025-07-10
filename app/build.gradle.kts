10ef032afed10e25d11f6fad7ea77591bb908db0
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-10 19:34:26 +0300

    add dependency for data.remoteDataSource.search module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ android {
 }
 
 dependencies {
+    implementation(projects.data.remoteDataSource.search)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
