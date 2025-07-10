983d1e042b3d77fcb7281d41a2bef9ca2ad70ef4
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-10 15:37:39 +0300

    remove redundant code

--- domain/vod/src/main/java/entity/Genre.kt
+++ domain/vod/src/main/java/entity/Genre.kt
@@ -1,30 +1,29 @@
 package entity
 
 enum class Genre {
-    ACTION,//
-    ADVENTURE,//
-    COMEDY,//
-    DRAMA,//
-    HORROR,//
-    SCIENCE_FICTION,//
-    FANTASY,//
-    ROMANCE,//
-    THRILLER,//
-    DOCUMENTARY,//
-    ANIMATION,//
-    CRIME,//
-    FAMILY,//
-    HISTORY,//
-    KIDS,//
-    MYSTERY,//
-    MUSIC,//
-    NEWS,//
-    REALITY,//
-    SOAP,//
-    TALK,//
-    WAR,//
-    WESTERN,//
-    TV_MOVIE,//
-    ACTION_AND_ADVENTURE,//
-    SCI_FI_AND_FANTASY//
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    SCIENCE_FICTION,
+    FANTASY,
+    ROMANCE,
+    THRILLER,
+    DOCUMENTARY,
+    ANIMATION,
+    CRIME,
+    FAMILY,
+    HISTORY,
+    KIDS,
+    MYSTERY,
+    MUSIC,
+    NEWS,
+    REALITY,
+    SOAP,
+    TALK,
+    WAR,//   WESTERN,
+    TV_MOVIE,
+    ACTION_AND_ADVENTURE,
+    SCI_FI_AND_FANTASY
 }
