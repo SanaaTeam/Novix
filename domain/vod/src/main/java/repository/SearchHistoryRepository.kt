f2e77d53094a619261893780d71dab29e0b819cb
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-10 12:14:10 +0300

    refactor getSearchHistory to optimize data retrieve

--- domain/vod/src/main/java/repository/SearchHistoryRepository.kt
+++ domain/vod/src/main/java/repository/SearchHistoryRepository.kt
@@ -1,10 +1,11 @@
 package repository
 
 import usecase.search.RecentViewedItem
+import usecase.search.SearchHistory
 import usecase.search.SearchHistoryInputItem
 
 interface SearchHistoryRepository {
-    suspend fun getSearchHistory(): List<SearchHistoryInputItem>
+    suspend fun getSearchHistory(): List<SearchHistory>
     suspend fun clearSearchHistory()
     suspend fun addSearchHistoryItem(item: SearchHistoryInputItem)
     suspend fun removeSearchHistoryItem(id: Long)
--- domain/vod/src/main/java/usecase/GetSearchHistoryUseCase.kt
+++ domain/vod/src/main/java/usecase/GetSearchHistoryUseCase.kt
@@ -1,10 +1,11 @@
 package usecase
 
 import repository.SearchHistoryRepository
+import usecase.search.SearchHistory
 import usecase.search.SearchHistoryInputItem
 
 class GetSearchHistoryUseCase(
     private val historyRepo: SearchHistoryRepository
 ) {
-    suspend fun execute(): List<SearchHistoryInputItem> = historyRepo.getSearchHistory()
+    suspend fun execute(): List<SearchHistory> = historyRepo.getSearchHistory()
 }
