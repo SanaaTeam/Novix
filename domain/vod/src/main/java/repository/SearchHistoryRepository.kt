271539120ffc2434da9cc0ba0f55072cca9b68d2
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-10 23:36:39 +0300

    use flow to track search history

--- domain/vod/src/main/java/repository/SearchHistoryRepository.kt
+++ domain/vod/src/main/java/repository/SearchHistoryRepository.kt
@@ -1,14 +1,15 @@
 package repository
 
+import kotlinx.coroutines.flow.Flow
 import usecase.search.RecentViewedItem
 import usecase.search.SearchHistory
 
 interface SearchHistoryRepository {
-    suspend fun getSearchHistory(): List<SearchHistory>
+    suspend fun getSearchHistory(): Flow<List<SearchHistory>>
     suspend fun addSearchHistory(query: String)
     suspend fun clearSearchHistory()
     suspend fun removeSearchHistoryItem(id: Int)
-    suspend fun getRecentViewed(): List<RecentViewedItem>
+    suspend fun getRecentViewed(): Flow<List<RecentViewedItem>>
     suspend fun addRecentViewedItem(item: RecentViewedItem)
     suspend fun clearRecentViewed()
 }
