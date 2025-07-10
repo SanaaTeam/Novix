d70b1885372c5268d8fa71c7f1845c1bab0aff69
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-10 19:34:05 +0300

    add Search NetworkModule and RemoteDataSource in AppModule

--- app/src/main/java/com/sanaa/novix/di/appModule.kt
+++ app/src/main/java/com/sanaa/novix/di/appModule.kt
@@ -1,7 +1,9 @@
 package com.sanaa.novix.di
 
+import com.sanaa.search.di.networkModule
+import com.sanaa.search.di.remoteDataSource
 import org.koin.dsl.module
 
 val appModule = module {
-    includes(firebaseModule, loggingModule)
+    includes(firebaseModule, loggingModule, networkModule, remoteDataSource)
 }
