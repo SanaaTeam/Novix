0faedbaea7ba5aa4b102fb925385e4a3c69f3188
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-10 22:12:13 +0300

    move KtorClient to networkModule for better dependency injection

--- data/remote_data_source/search/src/main/java/com/sanaa/search/di/networkModule.kt
+++ data/remote_data_source/search/src/main/java/com/sanaa/search/di/networkModule.kt
@@ -1,9 +1,30 @@
 package com.sanaa.search.di
 
-import com.sanaa.search.network.KtorClient
 import org.koin.dsl.module
 import io.ktor.client.*
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.plugins.logging.SIMPLE
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 val networkModule = module {
-    single<HttpClient> { KtorClient.client }
+    single<HttpClient> { HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json(Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+            })
+        }
+        install(Logging) {
+            logger = Logger.SIMPLE
+            level = LogLevel.ALL
+        }
+        engine {
+            requestTimeout = 30_000
+        }
+    } }
 }
