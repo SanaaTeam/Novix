14713c1df284114508c4a423bc5874f1e61f76ba
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-10 22:48:16 +0300

    edit RecentSearchItem after review

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/RecentSearchItem.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/RecentSearchItem.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.R
@@ -24,8 +26,7 @@ import com.sanaa.designsystem.design_system.theme.Theme
 
 @Composable
 fun RecentSearchItem(
-    text: String = "Shutter island",
-    isLast: Boolean = false,
+    text: String = stringResource(R.string.shutter_island),
     onCancelClicked: () -> Unit = {}
 ) {
     Column {
@@ -41,7 +42,8 @@ fun RecentSearchItem(
             Icon(
                 painter = painterResource(id = R.drawable.icon_clock),
                 contentDescription = null,
-                tint = Theme.colors.hint
+                tint = Theme.colors.hint,
+                modifier = Modifier.size(20.dp)
             )
             Text(
                 text = text,
@@ -53,18 +55,8 @@ fun RecentSearchItem(
                 painter = painterResource(id = R.drawable.icon_cancel),
                 contentDescription = null,
                 tint = Theme.colors.hint,
-                modifier = Modifier
+                modifier = Modifier.size(16.dp)
                     .clickable(onClick = onCancelClicked)
-            )
-        }
-        if (!isLast) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 16.dp)
-                    .padding(bottom = 12.dp)
-                    .height(1.dp)
-                    .background(color = Theme.colors.stroke)
             )
         }
     }
