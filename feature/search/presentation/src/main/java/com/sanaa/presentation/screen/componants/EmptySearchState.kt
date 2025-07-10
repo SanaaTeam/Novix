4cf86f3876c2c667ca5c86f2c7a30d444aee8f70
shahad-97 <shos8406@gmail.com>
2025-07-11 00:45:21 +0300

    refactor empty search state

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/EmptySearchState.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/EmptySearchState.kt
@@ -2,6 +2,7 @@ package com.sanaa.presentation.screen.componants
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -12,18 +13,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.R
+import com.sanaa.designsystem.design_system.theme.NovixTheme
 import com.sanaa.designsystem.design_system.theme.Theme
 
 
 @Composable
 fun EmptySearchState(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    icon : Painter,
+    text : String
 ) {
     Column(
         modifier = modifier
@@ -31,14 +36,19 @@ fun EmptySearchState(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(id =R.drawable.empty_search),
-            contentDescription = "Empty Search",
-            modifier = Modifier
-                .size(128.dp)
-        )
+        Box(
+            modifier = Modifier.size(128.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = icon,
+                contentDescription = "Empty Search",
+                modifier = Modifier
+                    .size(128.dp)
+            )
+        }
         Text(
-            text = stringResource(id = R.string.empty_search_message),
+            text = text,
             style = MaterialTheme.typography.bodySmall,
             textAlign = TextAlign.Center,
             color = Theme.colors.body,
@@ -49,10 +59,24 @@ fun EmptySearchState(
     }
 }
 
-@Preview(showBackground = true, heightDp = 800, widthDp = 360)
+@Preview(showBackground = true)
+@Composable
+fun ResultStatePreview() {
+   NovixTheme(false) {
+        EmptySearchState(
+            icon = painterResource(id = R.drawable.search_icons),
+            text = stringResource(id = R.string.no_search_result_message)
+        )
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun EmptySearchStatePreview() {
-    MaterialTheme {
-        EmptySearchState()
+    NovixTheme(false) {
+        EmptySearchState(
+            icon = painterResource(id = R.drawable.empty_search),
+            text = stringResource(id = R.string.empty_search_message)
+        )
     }
 }
