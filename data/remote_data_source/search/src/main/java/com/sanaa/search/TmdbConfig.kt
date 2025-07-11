36e394a1c8819fbb627476729c6fcc045b818cdf
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-11 23:17:48 +0300

    Get TMDb API key from BuildConfig

--- data/remote_data_source/search/src/main/java/com/sanaa/search/TmdbConfig.kt
+++ data/remote_data_source/search/src/main/java/com/sanaa/search/TmdbConfig.kt
@@ -1,15 +1,10 @@
 package com.sanaa.search
+import android.content.Context
 import java.io.File
 import java.util.*
 
-object TmdbConfig {
-    private val props: Properties by lazy {
-        File("keys.properties").takeIf { it.exists() }
-            ?.inputStream()?.use { Properties().apply { load(it) } }
-            ?: error("Missing keys.properties")
-    }
 
-    val apiKey: String by lazy {
-        props.getProperty("TMDB_API_KEY") ?: error("TMDB_API_KEY not found in keys.properties")
-    }
+object TmdbConfig {
+    val apiKey: String
+        get() = BuildConfig.TMDB_API_KEY
 }
