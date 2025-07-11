bd874d70c7d963593a9531505e246016a07c4edc
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-11 20:05:47 +0300

    refactor SearchViewModel

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/SearchViewModel.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/SearchViewModel.kt
@@ -2,30 +2,44 @@ package com.sanaa.presentation.screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sanaa.designsystem.R
 import com.sanaa.presentation.state.ActorUiModel
 import com.sanaa.presentation.state.MovieUiModel
 import com.sanaa.presentation.state.SearchScreenUiState
 import com.sanaa.presentation.state.TvShowUiModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import usecase.SearchActorsUseCase
+import usecase.SearchMoviesUseCase
+import usecase.SearchTvSeriesUseCase
 
-class SearchViewModel : ViewModel(), SearchScreenInteractionsListener {
+class SearchViewModel(
+    private val searchMoviesUseCase: SearchMoviesUseCase,
+    private val searchTvSeriesUseCase: SearchTvSeriesUseCase,
+    private val searchActorsUseCase: SearchActorsUseCase,
+) : ViewModel() {
+
     private val _uiState = MutableStateFlow(SearchScreenUiState())
     val uiState: StateFlow<SearchScreenUiState> = _uiState.asStateFlow()
 
     init {
-        loadMovies()
+        searchMediaByTab()
     }
 
-    override fun onTabSelected(index: Int) {
+    fun onTabSelected(index: Int) {
         _uiState.update { it.copy(selectedTabIndex = index) }
+        searchMediaByTab()
+    }
 
-        when (index) {
+    fun onSearchQueryChanged(query: String) {
+        _uiState.update { it.copy(searchQuery = query) }
+        searchMediaByTab()
+    }
+
+    private fun searchMediaByTab() {
+        when (_uiState.value.selectedTabIndex) {
             0 -> loadMovies()
             1 -> loadTvShows()
             2 -> loadActors()
@@ -35,17 +49,24 @@ class SearchViewModel : ViewModel(), SearchScreenInteractionsListener {
     private fun loadMovies() {
         viewModelScope.launch {
             _uiState.update { it.copy(isLoading = true, error = null) }
+            try {
+                val movies = searchMoviesUseCase.execute(
+                    query = _uiState.value.searchQuery,
+                    filters = null
+                ).map {
+                    MovieUiModel(
+                        id = it.id,
+                        title = it.title,
+                        imageUrl = it.posterImageUrl,
+                        rating = ""
+                    )
+                }
 
-            delay(1000)
-            val movies = listOf(
-                MovieUiModel(1, "The Green Mile", R.drawable.movie_poster, "9.9"),
-                MovieUiModel(2, "Prisoners", R.drawable.movie_poster, "9.0"),
-                MovieUiModel(3, "Se7en", R.drawable.movie_poster, "8.6"),
-                MovieUiModel(4, "Manifest", R.drawable.movie_poster, "8.2")
-            )
-
-            _uiState.update {
-                it.copy(isLoading = false, movies = movies)
+                _uiState.update {
+                    it.copy(isLoading = false, movies = movies)
+                }
+            } catch (e: Exception) {
+                _uiState.update { it.copy(isLoading = false, error = e.message ?: "Unknown error") }
             }
         }
     }
@@ -53,16 +74,24 @@ class SearchViewModel : ViewModel(), SearchScreenInteractionsListener {
     private fun loadTvShows() {
         viewModelScope.launch {
             _uiState.update { it.copy(isLoading = true, error = null) }
+            try {
+                val tvShows = searchTvSeriesUseCase.execute(
+                    query = _uiState.value.searchQuery,
+                    filters = null
+                ).map {
+                    TvShowUiModel(
+                        id = it.id,
+                        title = it.title,
+                        imageUrl = it.posterImageUrl,
+                        rating = ""
+                    )
+                }
 
-            delay(1000)
-            val tvShows = listOf(
-                TvShowUiModel(1, "Breaking Bad", R.drawable.movie_poster, "9.8"),
-                TvShowUiModel(2, "Dark", R.drawable.movie_poster, "9.0"),
-                TvShowUiModel(3, "Stranger Things", R.drawable.movie_poster, "8.7")
-            )
-
-            _uiState.update {
-                it.copy(isLoading = false, tvShows = tvShows)
+                _uiState.update {
+                    it.copy(isLoading = false, tvShows = tvShows)
+                }
+            } catch (e: Exception) {
+                _uiState.update { it.copy(isLoading = false, error = e.message ?: "Unknown error") }
             }
         }
     }
@@ -70,34 +99,21 @@ class SearchViewModel : ViewModel(), SearchScreenInteractionsListener {
     private fun loadActors() {
         viewModelScope.launch {
             _uiState.update { it.copy(isLoading = true, error = null) }
+            try {
+                val actors = searchActorsUseCase.execute(query = _uiState.value.searchQuery).map {
+                    ActorUiModel(
+                        id = it.id,
+                        name = it.name,
+                        imageUrl = it.profileImageUrl
+                    )
+                }
 
-            delay(1000)
-
-            val actors = listOf(
-                ActorUiModel(1, "Tom Hanks", R.drawable.movie_poster),
-                ActorUiModel(2, "Jake Gyllenhaal", R.drawable.movie_poster),
-                ActorUiModel(3, "Morgan Freeman", R.drawable.movie_poster)
-            )
-
-            _uiState.update {
-                it.copy(isLoading = false, actors = actors)
+                _uiState.update {
+                    it.copy(isLoading = false, actors = actors)
+                }
+            } catch (e: Exception) {
+                _uiState.update { it.copy(isLoading = false, error = e.message ?: "Unknown error") }
             }
         }
     }
-
-    override fun onSaveIconClicked(){
-
-    }
-    override fun onCancelClicked(){
-
-    }
-
-    override fun onClearRecentSearchClicked(){
-
-    }
-    override fun onClearRecentViewClicked(){
-
-    }
-
-
 }
