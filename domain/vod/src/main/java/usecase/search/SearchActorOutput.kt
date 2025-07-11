a932de4d5fda7e123eeb50db32cc56e46b692a13
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-11 21:24:44 +0300

    remove unnecessary language parameter from cache datasource interface

--- domain/vod/src/main/java/usecase/search/SearchActorOutput.kt
+++ domain/vod/src/main/java/usecase/search/SearchActorOutput.kt
@@ -1,7 +1,10 @@
 package usecase.search
 
+import entity.Actor
+
 data class SearchActorOutput(
     val id: Int,
     val name: String,
     val profileImageUrl: String,
+    val gender: Actor.Gender
 )
