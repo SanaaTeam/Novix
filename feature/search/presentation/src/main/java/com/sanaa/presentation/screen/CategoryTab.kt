e07820984d6c8100dd06884da34813598bff67fd
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-11 03:26:23 +0300

    refactor category tab

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/CategoryTab.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/CategoryTab.kt
@@ -1,24 +1,28 @@
 package com.sanaa.presentation.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
-import androidx.compose.material3.TabRow
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.design_system.component.chips.CategoryChip
 
 @Composable
-fun CategoryTabSection(
+fun CategoryTab(
     categories: List<String>,
     selectedIndex: Int,
     onCategorySelected: (Int) -> Unit
 ) {
 
     Column {
-        TabRow(
+        ScrollableTabRow(
             selectedTabIndex = selectedIndex,
             containerColor = Color.Transparent,
             contentColor = Color.Transparent,
+            edgePadding = 8.dp,
             indicator = {},
             divider = {}
         ) {
@@ -33,10 +37,10 @@ fun CategoryTabSection(
                     CategoryChip(
                         text = title,
                         isSelected = selectedIndex == index,
-                        onClick = { onCategorySelected(index) }
+                        onClick = { onCategorySelected(index) },
+                        modifier = Modifier.padding(end = 8.dp, start = 8.dp)
                     )
                 }
-
             }
         }
     }
