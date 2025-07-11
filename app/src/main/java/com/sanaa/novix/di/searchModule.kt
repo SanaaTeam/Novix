b7797196eff205351378a60c5be3113651c365b0
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-12 02:22:31 +0300

    add searchModule

--- app/src/main/java/com/sanaa/novix/di/searchModule.kt
+++ app/src/main/java/com/sanaa/novix/di/searchModule.kt
@@ -1,9 +1,17 @@
 package com.sanaa.novix.di
 
 import com.sanaa.presentation.filter_bottomsheet.FilterViewModel
+import com.sanaa.presentation.screen.SearchViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val searchModule = module {
     viewModel { FilterViewModel() }
+    viewModel {
+        SearchViewModel(
+            get(),
+            get(),
+            get(),
+        )
+    }
 }
