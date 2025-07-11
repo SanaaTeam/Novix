5aee1c9459d51faa065455d33af6f84a04269d73
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-11 23:17:33 +0300

    explicitly type SearchRemoteDataSource in DI

--- data/remote_data_source/search/src/main/java/com/sanaa/search/di/RemoteDataSourceModule.kt
+++ data/remote_data_source/search/src/main/java/com/sanaa/search/di/RemoteDataSourceModule.kt
@@ -1,8 +1,9 @@
 package com.sanaa.search.di
 
 import com.sanaa.search.SearchRemoteDataSourceImpl
+import com.sanaa.search.dataSource.SearchRemoteDataSource
 import org.koin.dsl.module
 
-val remoteDataSource =module {
-    single { SearchRemoteDataSourceImpl(get(),get()) }
+val remoteDataSource = module {
+    single<SearchRemoteDataSource> { SearchRemoteDataSourceImpl(get(), get()) }
 }
