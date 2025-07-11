2de5bd34d111a469b856edc622fc2399b67373cc
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-11 22:00:11 +0300

    remove unnecessary actor gender parameter

--- domain/vod/src/main/java/usecase/search/SearchActorOutput.kt
+++ domain/vod/src/main/java/usecase/search/SearchActorOutput.kt
@@ -1,10 +1,7 @@
 package usecase.search
 
-import entity.Actor
-
 data class SearchActorOutput(
     val id: Int,
     val name: String,
     val profileImageUrl: String,
-    val gender: Actor.Gender
 )
