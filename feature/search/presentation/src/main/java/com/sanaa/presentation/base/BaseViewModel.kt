5e81f1bdaaabd54a45ce2ba02a2b6815ac24a3c1
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-12 04:04:22 +0300

    add BaseViewModel

---
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/base/BaseViewModel.kt
@@ -0,0 +1,41 @@
+package com.sanaa.presentation.base
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<T>(
+    initialState: T,
+    val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : ViewModel() {
+    private val _state: MutableStateFlow<T> by lazy { MutableStateFlow(initialState) }
+    val state: StateFlow<T> by lazy { _state.asStateFlow() }
+
+    protected fun updateState(updater: (T) -> T) {
+        _state.update(updater)
+    }
+
+    protected fun <T> tryToExecute(
+        callee: suspend () -> T,
+        onSuccess: (T) -> Unit = {},
+        onError: (exception: Exception) -> Unit = {},
+        dispatcher: CoroutineDispatcher = defaultDispatcher,
+    ) {
+        viewModelScope.launch(dispatcher) {
+            try {
+                val result = callee()
+                onSuccess(result)
+            } catch (exception: Exception) {
+                onError(exception)
+            }
+        }
+    }
+
+}
