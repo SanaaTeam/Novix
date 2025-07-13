fa369f93c4d1e0dcc3fa587f937ea62f85bcc1aa
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-14 01:56:05 +0300

    add new use case class, `GetReviewsByMovieId`   this class utilizes the renamed repository method to fetch reviews for a given movie ID.

--- domain/vod/src/main/java/details/repository/MovieRepository.kt
+++ domain/vod/src/main/java/details/repository/MovieRepository.kt
@@ -10,6 +10,6 @@ interface MovieRepository {
     suspend fun getImages(id: Int): List<String>
     suspend fun getCast(id: Int): List<Actor>
     suspend fun getSimilarMoviesById(id: Int): List<Movie>
-    suspend fun getReviews(id: Int): List<Review>
+    suspend fun getReviewsByMovieId(id: Int): List<Review>
     suspend fun getMoviesByCategory(category: Genre): List<Movie>
 }
