0cfd6e78984187a3f9417e544d449cb8f75df48d
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-14 02:41:32 +0300

    add unit test to verify GetSimilarMoviesByMovieId returns expected similar movies

--- domain/vod/src/test/java/usecases/details/movie/GetSimilarMoviesByMovieIdTest.kt
+++ domain/vod/src/test/java/usecases/details/movie/GetSimilarMoviesByMovieIdTest.kt
@@ -1,9 +1,16 @@
 package usecases.details.movie
 
+import com.google.common.truth.Truth.assertThat
 import details.repository.MovieRepository
 import details.usecase.movie.GetSimilarMoviesByMovieId
+import entity.Genre
+import entity.Movie
+import io.mockk.coEvery
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.LocalDate
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 class GetSimilarMoviesByMovieIdTest(){
     private lateinit var movieRepository: MovieRepository
@@ -14,4 +21,44 @@ class GetSimilarMoviesByMovieIdTest(){
         movieRepository = mockk()
         getSimilarMoviesByMovieId = GetSimilarMoviesByMovieId(movieRepository)
     }
+
+    @Test
+    fun `execute() should return similar movies when available`() = runTest {
+        // Given
+        val movieId = 1
+        val similarMovies = listOf(testMovie1, testMovie2)
+        coEvery { movieRepository.getSimilarMoviesByMovieId(movieId) } returns similarMovies
+
+        // When
+        val result = getSimilarMoviesByMovieId.execute(movieId)
+
+        // Then
+        assertThat(result).isEqualTo(similarMovies)
+    }
+
+
+    companion object {
+        private val testMovie1 = Movie(
+            id = 101,
+            posterImageUrl = "https://image.tmdb.org/t/p/w500/image1.jpg",
+            title = "Test Movie 1",
+            genres = listOf(Genre.ACTION),
+            imdbRating = 7.8f,
+            duration = 120,
+            releaseDate = LocalDate(2023, 5, 20),
+            overview = "Overview of movie 1"
+        )
+
+        private val testMovie2 = Movie(
+            id = 102,
+            posterImageUrl = "https://image.tmdb.org/t/p/w500/image2.jpg",
+            title = "Test Movie 2",
+            genres = listOf(Genre.SCIENCE_FICTION, Genre.ACTION),
+            imdbRating = 8.2f,
+            duration = 130,
+            releaseDate = LocalDate(2022, 8, 10),
+            overview = "Overview of movie 2"
+        )
+    }
+
 }
