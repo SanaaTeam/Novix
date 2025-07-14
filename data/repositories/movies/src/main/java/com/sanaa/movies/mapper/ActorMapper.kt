e39e7536a9b5cb98b5fe29f485a6485ce1a7814e
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-15 01:21:25 +0300

    implement ActorMapper to map ActorDto to Actor and vice versa

--- data/repositories/movies/src/main/java/com/sanaa/movies/mapper/ActorMapper.kt
+++ data/repositories/movies/src/main/java/com/sanaa/movies/mapper/ActorMapper.kt
@@ -1,2 +1,34 @@
 package com.sanaa.movies.mapper
 
+import com.sanaa.movies.dataSource.dto.ActorDto
+import entity.Actor
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+fun ActorDto.toDomain(): Actor{
+    return Actor(
+        id = id,
+        name = name ?: "Unknown",
+        imageUrl = profileImagePath ?: "",
+        gender = gender,
+        character = character,
+        biography = biography,
+        birthDate = null,
+        deathDate = null,
+        placeOfBirth = null,
+        region = null,
+        lastShow = null
+    )
+}
+
+
+fun Actor.toDto(): ActorDto{
+    return ActorDto(
+        id = id,
+        name = name,
+        profileImagePath = imageUrl,
+        gender = gender,
+        character = character,
+        biography = biography
+    )
+}
