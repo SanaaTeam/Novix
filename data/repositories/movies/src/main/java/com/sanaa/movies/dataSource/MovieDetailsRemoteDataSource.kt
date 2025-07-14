880cfbeec0b3bcfc7bffaee74d021c275220100a
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-14 22:21:20 +0300

    Create MovieDetailsRemoteDataSource interface
    This interface defines methods for fetching movie details, images, cast, similar movies, reviews, and movies by category from a remote data source.

---
+++ data/repositories/movies/src/main/java/com/sanaa/movies/dataSource/MovieDetailsRemoteDataSource.kt
@@ -0,0 +1,15 @@
+package com.sanaa.movies.dataSource
+
+import com.sanaa.movies.dataSource.dto.ActorDto
+import com.sanaa.movies.dataSource.dto.MovieDetailsDto
+import com.sanaa.movies.dataSource.dto.ReviewDto
+import entity.Genre
+
+interface MovieDetailsRemoteDataSource {
+    suspend fun fetchMovieDetails(id: Int): MovieDetailsDto
+    suspend fun fetchImages(id: Int): List<String>
+    suspend fun fetchCast(id: Int): List<ActorDto>
+    suspend fun fetchSimilarMoviesByMovieId(id: Int): List<MovieDetailsDto>
+    suspend fun fetchReviewsByMovieId(id: Int): List<ReviewDto>
+    suspend fun fetchMoviesByCategory(category: Genre): List<MovieDetailsDto>
+}
