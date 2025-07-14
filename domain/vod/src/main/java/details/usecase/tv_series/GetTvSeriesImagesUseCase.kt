095e64182b63ed09d1b97c6d97aebda892e930ff
haider <haideraljawasim@gmail.com>
2025-07-14 03:57:43 +0300

    implement GetTvSeriesByGenre and GetTvSeriesImages use cases

---
+++ domain/vod/src/main/java/details/usecase/tv_series/GetTvSeriesImagesUseCase.kt
@@ -0,0 +1,9 @@
+package details.usecase.tv_series
+
+import details.repository.TvSeriesRepository
+
+class GetTvSeriesImagesUseCase(
+    private val repository: TvSeriesRepository
+) {
+    suspend fun execute(id: Int) = repository.getTvSeriesImages(id)
+}
