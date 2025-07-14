daa2754a31c655222907a0f5cbe3b63e26204891
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-15 02:19:19 +0300

    add mapper from movie details dto to movie entity

--- data/repositories/movies/src/main/java/com/sanaa/movies/mapper/MovieDetailsMapper.kt
+++ data/repositories/movies/src/main/java/com/sanaa/movies/mapper/MovieDetailsMapper.kt
@@ -1,2 +1,19 @@
 package com.sanaa.movies.mapper
 
+import com.sanaa.movies.dataSource.dto.MovieDetailsDto
+import entity.Movie
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.toLocalDate
+
+fun MovieDetailsDto.toDomain(): Movie {
+    return Movie(
+        id = id,
+        posterImageUrl = posterImagePath?.let { "https://image.tmdb.org/t/p/w500$it" } ?: "",
+        title = title ?: "Unknown Title",
+        genres = genreIds?.mapNotNull { it.toGenre() } ?: emptyList(),
+        imdbRating = voteAverage ?: 0.0f,
+        duration = duration ?: 0,
+        releaseDate = releaseDate?.toLocalDate() ?: LocalDate(1970, 1, 1),
+        overview = overview ?: "No overview available"
+    )
+}
