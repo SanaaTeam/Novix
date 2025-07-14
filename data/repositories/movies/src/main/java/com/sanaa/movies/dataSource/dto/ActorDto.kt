918298e151e63b13b7736f85e5acde9fe1f85c19
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-15 01:21:01 +0300

    add biography, character and gender fields to ActorDto

--- data/repositories/movies/src/main/java/com/sanaa/movies/dataSource/dto/ActorDto.kt
+++ data/repositories/movies/src/main/java/com/sanaa/movies/dataSource/dto/ActorDto.kt
@@ -1,7 +1,9 @@
 package com.sanaa.movies.dataSource.dto
 
+import entity.Actor.Gender
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import java.time.LocalDate
 
 @Serializable
 data class ActorDto(
@@ -13,5 +15,14 @@ data class ActorDto(
 
     @SerialName("profile_path")
     val profileImagePath: String?,
+
+    @SerialName("gender")
+    val gender: Gender,
+
+    @SerialName("character")
+    val character: String?,
+
+    @SerialName("biography")
+    val biography: String
 
     )
