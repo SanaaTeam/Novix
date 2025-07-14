4bbee8803361da92ebbd1437d0f8e540aceb02a2
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-15 02:01:01 +0300

    add `toGenre()` function for converting integer IDs to `Genre` enum

--- data/repositories/movies/src/main/java/com/sanaa/movies/mapper/GenreMapper.kt
+++ data/repositories/movies/src/main/java/com/sanaa/movies/mapper/GenreMapper.kt
@@ -33,3 +33,37 @@ fun Genre.toDtoId(): Int {
         Genre.SCI_FI_AND_FANTASY -> 10765
     }
 }
+
+
+fun Int.toGenre(): Genre? {
+    return when (this) {
+        28 -> Genre.ACTION
+        12 -> Genre.ADVENTURE
+        35 -> Genre.COMEDY
+        18 -> Genre.DRAMA
+        27 -> Genre.HORROR
+        878 -> Genre.SCIENCE_FICTION
+        14 -> Genre.FANTASY
+        10749 -> Genre.ROMANCE
+        53 -> Genre.THRILLER
+        99 -> Genre.DOCUMENTARY
+        16 -> Genre.ANIMATION
+        80 -> Genre.CRIME
+        10751 -> Genre.FAMILY
+        36 -> Genre.HISTORY
+        10762 -> Genre.KIDS
+        9648 -> Genre.MYSTERY
+        10402 -> Genre.MUSIC
+        10763 -> Genre.NEWS
+        10764 -> Genre.REALITY
+        10766 -> Genre.SOAP
+        10767 -> Genre.TALK
+        10752 -> Genre.WAR
+        10768 -> Genre.WAR_AND_POLITICS
+        37 -> Genre.WESTERN
+        10770 -> Genre.TV_MOVIE
+        10759 -> Genre.ACTION_AND_ADVENTURE
+        10765 -> Genre.SCI_FI_AND_FANTASY
+        else -> null
+    }
+}
