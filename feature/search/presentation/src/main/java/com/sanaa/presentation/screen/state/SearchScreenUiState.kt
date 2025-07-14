dbaf3d8df34b5acb48c0bd03f128283da70ee5d2
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-14 22:31:05 +0300

    add last tab index

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/state/SearchScreenUiState.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/state/SearchScreenUiState.kt
@@ -15,6 +15,7 @@ data class SearchScreenUiState(
     val actors: List<ActorUiModel> = emptyList(),
     val error: String? = null,
     val filters: MediaFilters? = null,
+    val lastTabIndex: Int = -1
 )
 
 data class MovieUiModel(
