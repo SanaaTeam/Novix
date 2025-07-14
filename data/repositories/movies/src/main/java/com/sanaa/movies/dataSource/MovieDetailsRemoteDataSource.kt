f61993b5214944875be7120b758338060ccb4fbb
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-15 00:39:38 +0300

    use integer for movie category ID in MovieDetailsRemoteDataSource

--- data/repositories/movies/src/main/java/com/sanaa/movies/dataSource/MovieDetailsRemoteDataSource.kt
+++ data/repositories/movies/src/main/java/com/sanaa/movies/dataSource/MovieDetailsRemoteDataSource.kt
@@ -3,7 +3,6 @@ package com.sanaa.movies.dataSource
 import com.sanaa.movies.dataSource.dto.ActorDto
 import com.sanaa.movies.dataSource.dto.MovieDetailsDto
 import com.sanaa.movies.dataSource.dto.ReviewDto
-import entity.Genre
 
 interface MovieDetailsRemoteDataSource {
     suspend fun fetchMovieDetails(id: Int): MovieDetailsDto
@@ -11,5 +10,5 @@ interface MovieDetailsRemoteDataSource {
     suspend fun fetchCast(id: Int): List<ActorDto>
     suspend fun fetchSimilarMoviesByMovieId(id: Int): List<MovieDetailsDto>
     suspend fun fetchReviewsByMovieId(id: Int): List<ReviewDto>
-    suspend fun fetchMoviesByCategory(category: Genre): List<MovieDetailsDto>
+    suspend fun fetchMoviesByCategory(category: Int): List<MovieDetailsDto>
 }
