19b81c27a5e8de94d410a7a17867392e25fa57bd
haider <haideraljawasim@gmail.com>
2025-07-14 18:42:21 +0300

    add dto for tv series

---
+++ data/repositories/series/src/main/java/com/sanaa/series/dto/ReviewDto.kt
@@ -0,0 +1,29 @@
+package com.sanaa.series.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class ReviewDto(
+    @SerialName("id") val id: String = "",
+
+    @SerialName("author") val author: String = "",
+
+    @SerialName("author_details") val authorDetails: AuthorDetailsDto = AuthorDetailsDto(),
+
+    @SerialName("content") val content: String = "",
+
+    @SerialName("created_at") val createdAt: String = ""
+)
+
+@Serializable
+data class AuthorDetailsDto(
+    @SerialName("name") val name: String = "",
+
+    @SerialName("username") val username: String? = null,
+
+    @SerialName("avatar_path") val avatarPath: String? = null,
+
+    @SerialName("rating") val rating: Float? = null
+)
