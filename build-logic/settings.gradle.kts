4277c16809152fe5a29746065ededb499ad27009
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-14 17:57:28 +0300

    integrate plugins version catalog for build-logic

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -18,6 +18,11 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
 }
 
 include(":conventions")
