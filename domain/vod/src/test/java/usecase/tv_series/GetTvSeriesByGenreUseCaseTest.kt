78f8c8ea2780366b5a3bc211896d598a99004eb0
haider <haideraljawasim@gmail.com>
2025-07-14 05:35:46 +0300

    add unit test cases for Getting series info use cases

--- domain/vod/src/test/java/usecase/tv_series/GetTvSeriesByGenreUseCaseTest.kt
+++ domain/vod/src/test/java/usecase/tv_series/GetTvSeriesByGenreUseCaseTest.kt
@@ -3,12 +3,9 @@ package usecase.tv_series
 import details.repository.TvSeriesRepository
 import details.usecase.tv_series.GetTvSeriesByGenreUseCase
 import entity.Genre
-import entity.TvSeries
-import extensions.now
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import kotlinx.datetime.LocalDateTime
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -42,17 +39,4 @@ class GetTvSeriesByGenreUseCaseTest {
             // Then
             assert(result.isEmpty())
         }
-
-    val dummyTvSeries =
-        TvSeries(
-            id = 1,
-            title = "The Walking Dead",
-            overview = "",
-            releaseDate = LocalDateTime.now().date,
-            genres = listOf(Genre.WAR_AND_POLITICS),
-            imdbRating = 1.2f,
-            posterImageUrl = "Image",
-            seasonsCount = 5,
-            trailerUrl = "Url",
-        )
 }
---
+++ domain/vod/src/test/java/usecase/tv_series/GetTvSeriesReviewsUseCaseTest.kt
@@ -0,0 +1,68 @@
+package usecase.tv_series
+
+import details.repository.TvSeriesRepository
+import details.usecase.tv_series.GetTvSeriesReviewsUseCase
+import entity.Review
+import exceptions.NotFoundException
+import extensions.now
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.LocalDateTime
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class GetTvSeriesReviewsUseCaseTest {
+    private lateinit var getTvSeriesReviewsUseCase: GetTvSeriesReviewsUseCase
+    private val repository: TvSeriesRepository = mockk(relaxed = true)
+
+    @BeforeEach
+    fun setUp() {
+        getTvSeriesReviewsUseCase = GetTvSeriesReviewsUseCase(repository)
+    }
+
+    @Test
+    fun `execute() should return tv series reviews when id is provided`() = runTest {
+        //Given
+        val id = 1
+        coEvery { repository.getTvSeriesReviews(id) } returns listOf(dummyReview)
+        //When
+        val result = getTvSeriesReviewsUseCase.execute(id)
+        // Then
+        assert(result.isNotEmpty())
+    }
+
+    @Test
+    fun `execute() should return an empty list when no tv series reviews are found for the given id`() =
+        runTest {
+            //Given
+            val id = 1
+            coEvery { repository.getTvSeriesReviews(id) } returns emptyList()
+            //When
+            val result = getTvSeriesReviewsUseCase.execute(id)
+            // Then
+            assert(result.isEmpty())
+        }
+
+    @Test
+    fun `execute() should throw NotFoundException when no tv series reviews are found for the given id`() =
+        runTest {
+            //Given
+            val id = 1
+            coEvery { repository.getTvSeriesReviews(id) } throws NotFoundException("Message")
+
+            // When & Then
+            assertThrows<NotFoundException> { getTvSeriesReviewsUseCase.execute(id) }
+        }
+
+    val dummyReview = Review(
+        id = 1,
+        authorName = "Haider",
+        userHandle = "itsHaider",
+        avatarUrl = "Me",
+        rating = 1.7f,
+        content = "No content",
+        createdDate = LocalDateTime.now().date
+    )
+}
