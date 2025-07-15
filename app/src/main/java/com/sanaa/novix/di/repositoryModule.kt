ce20451f262082bf305f3d704d91bac159ed969b
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-15 03:47:20 +0300

    implement actor repository for fetching actors details

--- app/src/main/java/com/sanaa/novix/di/repositoryModule.kt
+++ app/src/main/java/com/sanaa/novix/di/repositoryModule.kt
@@ -1,9 +1,12 @@
 package com.sanaa.novix.di
 
+import com.sanaa.actors.ActorRemoteDataSourceImpl
+import com.sanaa.actors.repository.ActorRepositoryImpl
 import com.sanaa.search.dataSource.local.LocalSearchHistoryDataSource
 import com.sanaa.search.repository.SearchHistoryRepositoryImpl
 import com.sanaa.search.repository.SearchRepositoryImpl
 import com.sanaa.search.search_history.LocalSearchHistoryDataSourceImpl
+import details.repository.ActorRepository
 import org.koin.dsl.module
 import search.repository.SearchHistoryRepository
 import search.repository.SearchRepository
@@ -13,4 +16,7 @@ val repositoryModule = module {
     single { SearchRepositoryImpl(get(), get(), get()) }
     single<SearchHistoryRepository> { SearchHistoryRepositoryImpl(get()) }
     single<LocalSearchHistoryDataSource> { LocalSearchHistoryDataSourceImpl(get(), get()) }
+
+    single<ActorRepository> { ActorRepositoryImpl(get(), get()) }
+    single { ActorRemoteDataSourceImpl(get(), get(), get()) }
 }
