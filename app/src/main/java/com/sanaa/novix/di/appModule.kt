9a438164e663101976e5de6fa142d96cfc912c26
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-15 03:43:19 +0300

    integrate remote details dataSource into app module

--- app/src/main/java/com/sanaa/novix/di/appModule.kt
+++ app/src/main/java/com/sanaa/novix/di/appModule.kt
@@ -1,6 +1,7 @@
 package com.sanaa.novix.di
 
 import com.example.env_config.di.configModule
+import com.sanaa.actors.di.remoteDetailsDataSource
 import com.sanaa.search.di.networkModule
 import com.sanaa.search.di.remoteDataSource
 import com.sanaa.search.search_result.di.localDatabaseModule
@@ -9,7 +10,6 @@ import org.koin.dsl.module
 val appModule = module {
     includes(
         firebaseModule, loggingModule, configModule, repositoryModule, localDatabaseModule,
-        networkModule, remoteDataSource
+        networkModule, remoteDataSource, remoteDetailsDataSource
     )
-
 }
