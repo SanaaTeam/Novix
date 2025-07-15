53284f588d63cc3b25861639ee0de08586e21f01
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-15 07:04:43 +0300

    add material3 library to media details feature

--- feature/media_details/presentation/build.gradle.kts
+++ feature/media_details/presentation/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     implementation(libs.androidx.navigation.compose)
