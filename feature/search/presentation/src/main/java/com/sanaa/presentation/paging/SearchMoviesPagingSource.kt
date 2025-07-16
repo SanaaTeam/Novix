6e75c8d436a2f33b7ab0d27236c874a3c1898388
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-16 06:28:22 +0300

    add SearchMoviesPagingSource to presentation

---
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/paging/SearchMoviesPagingSource.kt
@@ -0,0 +1,39 @@
+package com.sanaa.presentation.paging
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import usecase.SearchMoviesUseCase
+import usecase.search.SearchMediaOutput
+
+class SearchMoviesPagingSource(
+    private val searchMoviesUseCase: SearchMoviesUseCase,
+    private val query: String,
+    private val filters: usecase.search.MediaFilters?
+) : PagingSource<Int, SearchMediaOutput>() {
+
+    override fun getRefreshKey(state: PagingState<Int, SearchMediaOutput>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchMediaOutput> {
+        val page = params.key ?: STARTING_PAGE_INDEX
+        return try {
+            val results = searchMoviesUseCase.execute(query, page, filters)
+
+            LoadResult.Page(
+                data = results,
+                prevKey = if (page == STARTING_PAGE_INDEX) null else page - 1,
+                nextKey = if (results.isEmpty()) null else page + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    companion object {
+        private const val STARTING_PAGE_INDEX = 1
+    }
+}
