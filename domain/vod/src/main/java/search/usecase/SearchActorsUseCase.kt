023fc6269ac2782f57d26b1c86aa8145e458104e
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-17 02:59:19 +0300

    refactor use cass

--- domain/vod/src/main/java/search/usecase/SearchActorsUseCase.kt
+++ domain/vod/src/main/java/search/usecase/SearchActorsUseCase.kt
@@ -1,11 +1,11 @@
 package search.usecase
 
 import search.repository.SearchHistoryRepository
-import search.repository.SearchPagingRepository
+import search.repository.SearchRepository
 import search.usecase.search_param.SearchActorOutput
 
 class SearchActorsUseCase(
-    private val searchRepository: SearchPagingRepository,
+    private val searchRepository: SearchRepository,
     private val searchHistoryRepository: SearchHistoryRepository,
 ) {
     suspend fun execute(query: String, page: Int): List<SearchActorOutput> {
--- domain/vod/src/main/java/search/usecase/SearchMoviesUseCase.kt
+++ domain/vod/src/main/java/search/usecase/SearchMoviesUseCase.kt
@@ -1,17 +1,17 @@
 package search.usecase
 
 import search.repository.SearchHistoryRepository
-import search.repository.SearchPagingRepository
+import search.repository.SearchRepository
 import search.usecase.search_param.MediaFilters
 import search.usecase.search_param.MediaType
 import search.usecase.search_param.SearchMediaOutput
 
 class SearchMoviesUseCase(
-    private val searchRepository: SearchPagingRepository,
+    private val searchRepository: SearchRepository,
     private val searchHistoryRepository: SearchHistoryRepository,
 ) {
     suspend fun execute(query: String, page: Int, filters: MediaFilters?): List<SearchMediaOutput> {
         searchHistoryRepository.addSearchHistory(query)
-        return searchRepository.searchMovies(query, page, filters, MediaType.MOVIE)
+        return searchRepository.searchMedia(query, page, filters, MediaType.MOVIE)
     }
 }
