e6603feb993aa43f0638e3ef12e0a78c7a6cf306
haider <haideraljawasim@gmail.com>
2025-07-16 04:25:50 +0300

    add SeriesScreen interaction listiner

---
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/series/SeriesScreenUiState.kt
@@ -0,0 +1,77 @@
+package com.sanaa.presentation.screen.series
+
+import entity.Actor
+import entity.Episode
+import entity.Season
+import entity.TvSeries
+
+data class SeriesScreenUiState(
+    val isLoading: Boolean = false,
+    val series: SeriesUiModel = SeriesUiModel(),
+    val season: SeasonUiModel = SeasonUiModel(),
+    val cast: List<CastUiModel> = emptyList(),
+    val images: List<String> = emptyList(),
+    val error: String? = null,
+)
+
+data class SeriesUiModel(
+    val id: Int = 0,
+    val title: String = "",
+    val posterPath: String? = null,
+    val rating: String = "",
+    val overview: String = "",
+    val genres: List<String> = emptyList(),
+)
+
+data class SeasonUiModel(
+    val seasonNumber: Int = 0,
+    val episodeCount: Int = 0,
+    val episodes: List<EpisodeUiModel> = emptyList(),
+)
+
+data class EpisodeUiModel(
+    val episodeNumber: Int = 0,
+    val title: String = "",
+    val rating: String = "",
+    val airDate: String = "",
+    val stillPath: String? = null,
+    val duration: Int = 0,
+)
+
+data class CastUiModel(
+    val id: Int = 0,
+    val name: String = "",
+    val character: String = "",
+    val profilePath: String? = null,
+)
+
+fun TvSeries.toSeriesUiModel() = SeriesUiModel(
+    id = id,
+    title = title,
+    posterPath = posterImageUrl,
+    overview = overview,
+    rating = imdbRating.toString(),
+)
+
+fun Season.toSeasonUiModel() = SeasonUiModel(
+    seasonNumber = number,
+    episodeCount = episodes.size,
+    episodes = episodes.map { it.toEpisodeUiModel() },
+
+    )
+
+fun Episode.toEpisodeUiModel() = EpisodeUiModel(
+    episodeNumber = number,
+    title = title,
+    rating = imdbRating.toString(),
+    airDate = "Date",
+    stillPath = stillImagePath,
+    duration = durationMinutes,
+)
+
+fun Actor.toCastUiModel() = CastUiModel(
+    id = id,
+    name = name,
+    character = character.orEmpty(),
+    profilePath = imageUrl,
+)
