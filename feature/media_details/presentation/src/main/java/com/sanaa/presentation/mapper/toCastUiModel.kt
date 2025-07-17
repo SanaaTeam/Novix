21357626fa98500f59f1b618052320a1283b5680
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-17 18:04:22 +0300

    update toCastUiModel mapper

--- feature/media_details/presentation/src/main/java/com/sanaa/presentation/mapper/toCastUiModel.kt
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/mapper/toCastUiModel.kt
@@ -3,10 +3,16 @@ package com.sanaa.presentation.mapper
 import com.sanaa.presentation.model.CastMemberUiModel
 import entity.Actor
 
+private const val TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
+
 fun Actor.toCastUiModel(): CastMemberUiModel {
+    val fullImageUrl = imageUrl.fullPosterUrlOrEmpty()
     return CastMemberUiModel(
         name = name,
-        character = character ?: "Unknown",
-        imageUrl = imageUrl
+        character = character.orEmpty().ifBlank { "Unknown" },
+        imageUrl = fullImageUrl
     )
+}
+fun String?.fullPosterUrlOrEmpty(): String {
+    return if (!isNullOrBlank()) "$TMDB_IMAGE_BASE_URL$this" else ""
 }
