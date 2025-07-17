f0fb71812c930c94305bb73045b5d6fd3bd7d8c9
Ahmed Hosny <ahmedhosny6688@gmail.com>
2025-07-17 13:45:40 +0300

    applies common settings for Android library modules

--- build-logic/conventions/build.gradle.kts
+++ build-logic/conventions/build.gradle.kts
@@ -6,3 +6,12 @@ dependencies {
     compileOnly(libs.kotlin.gradle.plugin)
     compileOnly(libs.android.gradle.plugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidLibraryConvention") {
+            id = "plugins.android.library.convention"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+    }
+}
