800ead54a3dcd6dcab19d6b2b31499b09cdcc005
haider <haideraljawasim@gmail.com>
2025-07-17 05:10:57 +0300

    rename SereisTopScreen to SereiesHeaderSection

---
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/series/components/SeriesHeaderSection.kt
@@ -0,0 +1,106 @@
+package com.sanaa.presentation.screen.series.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.design_system.component.button.TextButton
+import com.sanaa.designsystem.design_system.theme.Theme
+import com.sanaa.presentation.R
+import com.sanaa.presentation.component.DotSeparator
+import com.sanaa.presentation.component.IconWithText
+import com.sanaa.presentation.component.ImageSlider
+import com.sanaa.presentation.component.InfoSection
+
+@Composable
+fun SeriesHeaderSection(
+    title: String,
+    rating: String,
+    season: String,
+    airDate: String,
+    imagesUrl: List<String>,
+    genres: List<String>,
+    modifier: Modifier = Modifier,
+    onReviewClicked: () -> Unit = {},
+) {
+    Box(
+        modifier = modifier
+    ) {
+        ImageSlider(
+            images = imagesUrl,
+            modifier = Modifier.align(Alignment.TopCenter),
+            contentDescription = null
+        )
+        InfoSection(
+            title = title,
+            modifier = Modifier
+                .padding(top = 208.dp, start = 16.dp, end = 16.dp)
+                .fillMaxWidth()
+                .align(Alignment.TopCenter)
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Bottom,
+            ) {
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                ) {
+                    genres.forEachIndexed { index, genre ->
+                        Text(
+                            text = genre,
+                            style = Theme.textStyle.label.small,
+                            color = Theme.colors.body,
+                            modifier = Modifier.padding(end = 8.dp)
+                        )
+                        if (index != genres.lastIndex) {
+                            DotSeparator()
+                        }
+                    }
+                }
+
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(bottom = 8.dp)
+                ) {
+                    IconWithText(
+                        iconRes = R.drawable.icon_star,
+                        text = rating,
+                        contentDescription = rating,
+                        tint = Theme.colors.statusColors.yellowAccent
+                    )
+                    DotSeparator()
+                    Text(
+                        text = airDate,
+                        style = Theme.textStyle.label.small,
+                        color = Theme.colors.hint
+                    )
+                    DotSeparator()
+                    IconWithText(
+                        text = season,
+                        iconRes = R.drawable.icon_seasons,
+                        contentDescription = season,
+                        tint = Theme.colors.hint
+                    )
+                }
+                TextButton(
+                    text = stringResource(R.string.view_reviews),
+                    onClick = onReviewClicked
+                )
+            }
+        }
+    }
+}
