4e73c498487715e99a653c8ec7864382ac8dbb12
raouf-kamel <raoufkamel2013@gmail.com>
2025-07-17 03:05:30 +0300

    remove SearchPagingRepositoryImpl

--- app/src/main/java/com/sanaa/novix/di/repositoryModule.kt
+++ app/src/main/java/com/sanaa/novix/di/repositoryModule.kt
@@ -4,19 +4,16 @@ import com.sanaa.actors.ActorRemoteDataSourceImpl
 import com.sanaa.actors.repository.ActorRepositoryImpl
 import com.sanaa.search.dataSource.local.LocalSearchHistoryDataSource
 import com.sanaa.search.repository.SearchHistoryRepositoryImpl
-import com.sanaa.search.repository.SearchPagingRepositoryImpl
 import com.sanaa.search.repository.SearchRepositoryImpl
 import com.sanaa.search.search_history.LocalSearchHistoryDataSourceImpl
 import details.repository.ActorRepository
 import org.koin.dsl.module
 import search.repository.SearchHistoryRepository
-import search.repository.SearchPagingRepository
 import search.repository.SearchRepository
 
 val repositoryModule = module {
     single<SearchRepository> { SearchRepositoryImpl(get(), get(), get()) }
     single { SearchRepositoryImpl(get(), get(), get()) }
-    single<SearchPagingRepository> { SearchPagingRepositoryImpl(get()) }
     single<SearchHistoryRepository> { SearchHistoryRepositoryImpl(get()) }
     single<LocalSearchHistoryDataSource> { LocalSearchHistoryDataSourceImpl(get(), get()) }
 
