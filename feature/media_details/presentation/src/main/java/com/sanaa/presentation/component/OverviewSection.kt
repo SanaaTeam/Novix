9fd9bc0fdebeafaae65068304449e39d808823d5
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-17 06:19:26 +0300

    replace Text with TextButton in OverviewSection

--- feature/media_details/presentation/src/main/java/com/sanaa/presentation/component/OverviewSection.kt
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/component/OverviewSection.kt
@@ -1,7 +1,9 @@
 package com.sanaa.presentation.component
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
@@ -11,13 +13,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.dp
-import com.sanaa.designsystem.design_system.theme.Theme
-import androidx.compose.animation.animateContentSize
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.tween
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.design_system.component.button.TextButton
+import com.sanaa.designsystem.design_system.theme.Theme
 import com.sanaa.presentation.R
 
 @Composable
@@ -49,20 +48,19 @@ fun OverviewSection(
             style = Theme.textStyle.body.medium,
             color = Theme.colors.body,
             maxLines = if (isExpanded) Int.MAX_VALUE else 4,
-            overflow = TextOverflow.Ellipsis,
             modifier = Modifier.padding(top = 8.dp)
         )
 
-        Text(
+        TextButton(
             text = stringResource(if (isExpanded) R.string.read_less else R.string.read_more),
-            style = Theme.textStyle.body.medium,
-            color = Theme.colors.primary,
-            modifier = Modifier
-                .padding(top = 4.dp)
-                .clickable {
-                    isExpanded = !isExpanded
-                    if (isExpanded) onReadMore()
-                }
+            onClick = {
+                isExpanded = !isExpanded
+                if (isExpanded) onReadMore()
+            },
+            textColor = Theme.colors.primary,
+            isEnabled = true,
+
         )
+
     }
 }
