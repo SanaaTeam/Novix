5d8ca1ebc0f4f5511d50904a8ac2697018af9c64
haider <haideraljawasim@gmail.com>
2025-07-17 13:25:17 +0300

    change navagtion commpose to 2.9.1 in toml file

--- feature/media_details/presentation/build.gradle.kts
+++ feature/media_details/presentation/build.gradle.kts
@@ -35,6 +35,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.kotlinx.serialization.json)
     implementation(projects.designSystem)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
