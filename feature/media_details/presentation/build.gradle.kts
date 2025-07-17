4309d291970510dc7aa10b16f5cc80f9b163d4b9
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-17 07:43:08 +0300

    add necessary dependencies for feature media details

--- feature/media_details/presentation/build.gradle.kts
+++ feature/media_details/presentation/build.gradle.kts
@@ -35,12 +35,40 @@ android {
 }
 
 dependencies {
+    implementation(project(":domain:vod"))
+    implementation(project(":image_viewer"))
 
+    implementation(projects.designSystem)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.ui.compose.tooling.preview)
     implementation(libs.coil.compose)
+
+    implementation(libs.androidx.material3.window.size.class1)
+    implementation(libs.androidx.material3.adaptive.navigation.suite)
+
+    implementation(libs.material3)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.ui.compose)
+    implementation(libs.androidx.ui.compose.graphics)
+    implementation(libs.androidx.compose.foundation)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(platform(libs.koin.bom))
+    implementation(libs.bundles.koin)
+
+    testImplementation(libs.junit.jupiter.api)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.truth)
+    testImplementation(kotlin("test"))
+    testImplementation(libs.turbine)
+    implementation(libs.kotlinx.datetime)
 }
