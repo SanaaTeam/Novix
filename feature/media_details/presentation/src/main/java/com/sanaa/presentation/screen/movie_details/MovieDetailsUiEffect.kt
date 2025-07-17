b4c5861a9cf8d7eeae08755fd399efd1ff357224
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-17 18:03:37 +0300

    add`NavigateToReviewsScreen`

--- feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/movie_details/MovieDetailsUiEffect.kt
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/movie_details/MovieDetailsUiEffect.kt
@@ -1,7 +1,7 @@
 package com.sanaa.presentation.screen.movie_details
 
 sealed class MovieDetailsUiEffect {
-    data class ShowErrorMessage(val message: String) : MovieDetailsUiEffect()
     object NavigateBack : MovieDetailsUiEffect()
-    data class OpenTrailer(val url: String) : MovieDetailsUiEffect()
+    data class NavigateToReviewsScreen(val movieId: Int) : MovieDetailsUiEffect()
+    data class OpenTrailer(val url: String?) : MovieDetailsUiEffect()
 }
