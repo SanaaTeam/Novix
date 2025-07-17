3144fe8f1198c43ec3ead73a6fdb6385d7079ef1
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-17 19:10:55 +0300

    add interaction listener to MovieCategoriesScreen

---
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/movie_categories/MovieCategoriesScreenInteractionListener.kt
@@ -0,0 +1,8 @@
+package com.sanaa.presentation.screen.movie_categories
+
+interface MovieCategoriesScreenInteractionListener {
+    fun onSaveIconClick()
+    fun onBackClick()
+    fun openMovie()
+    fun hideBottomSheet()
+}
