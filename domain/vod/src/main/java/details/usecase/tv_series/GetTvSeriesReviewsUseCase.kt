5efb72010e3b909636341eb8c3cd65f780e79f05
haider <haideraljawasim@gmail.com>
2025-07-17 18:21:45 +0300

    fix a bug in Geting reviewes

--- domain/vod/src/main/java/details/usecase/tv_series/GetTvSeriesReviewsUseCase.kt
+++ domain/vod/src/main/java/details/usecase/tv_series/GetTvSeriesReviewsUseCase.kt
@@ -1,10 +1,11 @@
 package details.usecase.tv_series
 
 import details.repository.TvSeriesRepository
+import entity.Review
 
 class GetTvSeriesReviewsUseCase(
     private val repository: TvSeriesRepository
 
 ) {
-    suspend fun execute(id: Int) = repository.getTvSeriesReviews(id)
+    suspend fun execute(id: Int): List<Review> = repository.getTvSeriesReviews(id)
 }
