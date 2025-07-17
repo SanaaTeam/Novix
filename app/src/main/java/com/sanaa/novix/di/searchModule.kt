d6b3b1196d724e89577e924ed0a1b34da7b00530
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-17 07:44:05 +0300

    remove temp search module

--- app/src/main/java/com/sanaa/novix/di/searchModule.kt
+++ app/src/main/java/com/sanaa/novix/di/searchModule.kt
@@ -1,28 +1,25 @@
-package com.sanaa.novix.di
-
-import com.sanaa.presentation.ActorViewModel
-import com.sanaa.presentation.filter_bottomsheet.FilterViewModel
-import com.sanaa.presentation.screen.SearchViewModel
-import kotlinx.coroutines.Dispatchers
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val searchModule = module {
-    viewModel { FilterViewModel(dispatcher = Dispatchers.IO) }
-    viewModel {
-        SearchViewModel(
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            get(),
-            dispatcher = Dispatchers.IO,
-        )
-    }
-
-    viewModel { ActorViewModel(get(), get(), get(), get(), get()) }
-}
+//package com.sanaa.novix.di
+//
+//import com.sanaa.presentation.filter_bottomsheet.FilterViewModel
+//import com.sanaa.presentation.screen.SearchViewModel
+//import kotlinx.coroutines.Dispatchers
+//import org.koin.androidx.viewmodel.dsl.viewModel
+//import org.koin.dsl.module
+//
+//val searchModule = module {
+//    viewModel { FilterViewModel(dispatcher = Dispatchers.IO) }
+//    viewModel {
+//        SearchViewModel(
+//            get(),
+//            get(),
+//            get(),
+//            get(),
+//            get(),
+//            get(),
+//            get(),
+//            get(),
+//            get(),
+//            dispatcher = Dispatchers.IO,
+//        )
+//    }
+//}
