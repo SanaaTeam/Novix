75f565a42623209fe021b173f9dff150bcf92757
haider <haideraljawasim@gmail.com>
2025-07-17 18:16:35 +0300

    add the navigation routs for details screen

--- feature/media_details/presentation/src/main/java/com/sanaa/presentation/navigation/detailsRouts.kt
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/navigation/detailsRouts.kt
@@ -7,10 +7,14 @@ interface Destination {
     fun route(): String
 }
 
-
 @Serializable
 data class SeriesDetailsScreenRoute(val seriesId: Int) : Destination {
     override fun route(): String = "series/$seriesId"
+
+    companion object {
+        const val PATTERN = "series/{seriesId}"
+        const val ARG_SERIES_ID = "seriesId"
+    }
 }
 
 @Serializable
@@ -18,30 +22,57 @@ data class EpisodeDetailsScreenRoute(
     val seriesId: Int, val seasonNumber: Int, val episodeNumber: Int
 ) : Destination {
     override fun route(): String = "series/$seriesId/season/$seasonNumber/episode/$episodeNumber"
+
+    companion object {
+        const val PATTERN = "series/{seriesId}/season/{seasonNumber}/episode/{episodeNumber}"
+        const val ARG_SERIES_ID = "seriesId"
+        const val ARG_SEASON_NUMBER = "seasonNumber"
+        const val ARG_EPISODE_NUMBER = "episodeNumber"
+    }
 }
 
 @Serializable
 data class MovieDetailsScreenRoute(val movieId: Int) : Destination {
     override fun route(): String = "movie/$movieId"
-}
 
+    companion object {
+        const val PATTERN = "movie/{movieId}"
+        const val ARG_MOVIE_ID = "movieId"
+    }
+}
 
 @Serializable
-data class ReviewsScreenRoute(val mediaId: Int, val mediaType: MediaTypeParam) : Destination {
-    override fun route(): String = "reviews/$mediaId/$mediaType"
-}
+data class ReviewsScreenRoute(val seriesId: Int) : Destination {
+    override fun route(): String = "reviews/$seriesId"
 
+    companion object {
+        const val PATTERN = "reviews/{seriesId}"
+        const val ARG_SERIES_ID = "seriesId"
+    }
+}
 
 @Serializable
 data class TopMediaActorPicksScreenRoute(val mediaId: Int, val mediaType: MediaTypeParam) :
     Destination {
     override fun route(): String = "top_media_actor_picks/$mediaId/$mediaType"
+
+    companion object {
+        const val PATTERN = "top_media_actor_picks/{mediaId}/{mediaType}"
+        const val ARG_MEDIA_ID = "mediaId"
+        const val ARG_MEDIA_TYPE = "mediaType"
+    }
 }
 
 @Serializable
 data class SimilarGenreMediaScreenRoute(val genreId: Int, val mediaType: MediaTypeParam) :
     Destination {
     override fun route(): String = "similar_genre_media/$genreId/$mediaType"
+
+    companion object {
+        const val PATTERN = "similar_genre_media/{genreId}/{mediaType}"
+        const val ARG_GENRE_ID = "genreId"
+        const val ARG_MEDIA_TYPE = "mediaType"
+    }
 }
 
 @Serializable
@@ -52,7 +83,6 @@ enum class MediaTypeParam {
     @SerialName("tv")
     TV
 }
-
 
 @Serializable
 data class ActorDetailsScreenRoute(val actorId: Int) : Destination {
