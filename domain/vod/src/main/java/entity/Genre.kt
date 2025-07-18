a0dc246cc9340a57c985ca036e2a51a9d5751b2a
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-18 12:15:28 +0300

    remove string values from genre enum

--- domain/vod/src/main/java/entity/Genre.kt
+++ domain/vod/src/main/java/entity/Genre.kt
@@ -1,36 +1,31 @@
 package entity
 
-enum class Genre(val value: String) {
-    ACTION("Action"),
-    ADVENTURE("Adventure"),
-    COMEDY("Comedy"),
-    DRAMA("Drama"),
-    HORROR("Horror"),
-    SCIENCE_FICTION("Science Fiction"),
-    FANTASY("Fantasy"),
-    ROMANCE("Romance"),
-    THRILLER("Thriller"),
-    DOCUMENTARY("Documentary"),
-    ANIMATION("Animation"),
-    CRIME("Crime"),
-    FAMILY("Family"),
-    HISTORY("History"),
-    KIDS("Kids"),
-    MYSTERY("Mystery"),
-    MUSIC("Music"),
-    NEWS("News"),
-    REALITY("Reality"),
-    SOAP("Soap"),
-    TALK("Talk"),
-    WAR("War"),
-    WAR_AND_POLITICS("War & Politics"),
-    WESTERN("Western"),
-    TV_MOVIE("TV Movie"),
-    ACTION_AND_ADVENTURE("Action & Adventure"),
-    SCI_FI_AND_FANTASY("Sci-Fi & Fantasy");
-    companion object {
-        fun fromValue(value: String): Genre? =
-            entries.find { it.value.equals(value, ignoreCase = true) }
-    }
+enum class Genre {
+    ACTION,
+    ADVENTURE,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    SCIENCE_FICTION,
+    FANTASY,
+    ROMANCE,
+    THRILLER,
+    DOCUMENTARY,
+    ANIMATION,
+    CRIME,
+    FAMILY,
+    HISTORY,
+    KIDS,
+    MYSTERY,
+    MUSIC,
+    NEWS,
+    REALITY,
+    SOAP,
+    TALK,
+    WAR,
+    WAR_AND_POLITICS,
+    WESTERN,
+    TV_MOVIE,
+    ACTION_AND_ADVENTURE,
+    SCI_FI_AND_FANTASY;
 }
-
