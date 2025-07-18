bbcc7b1c85d3f83aad158ccbc8bf7b35ef79e9df
AimanYosofi <=>
2025-07-18 18:13:34 +0300

    add unit test for SearchTvShowsPagingSource and SearchMoviesPagingSource

--- feature/search/presentation/src/test/java/com/sanaa/presentation/paging/SearchMoviesPagingSourceTest.kt
+++ feature/search/presentation/src/test/java/com/sanaa/presentation/paging/SearchMoviesPagingSourceTest.kt
@@ -11,67 +11,70 @@ import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import search.usecase.SearchActorsUseCase
+import search.usecase.SearchMoviesUseCase
+import search.usecase.search_param.MediaFilters
 import search.usecase.search_param.SearchActorOutput
+import search.usecase.search_param.SearchMovieOutput
 
-class SearchActorsPagingSourceTest {
-    private val searchActorsUseCase: SearchActorsUseCase = mockk(relaxed = true)
-    private val query: String = "Actor1"
-    private lateinit var searchActorsPagingSource: SearchActorsPagingSource
+class SearchMoviesPagingSourceTest {
+    private val searchMoviesUseCase: SearchMoviesUseCase = mockk(relaxed = true)
+    private val filters: MediaFilters = mockk(relaxed = true)
+    private val query: String = "Movie1"
+    private lateinit var searchMoviesPagingSource: SearchMoviesPagingSource
 
     @BeforeEach
     fun setUp() {
-        searchActorsPagingSource = SearchActorsPagingSource(searchActorsUseCase, query)
+        searchMoviesPagingSource = SearchMoviesPagingSource(searchMoviesUseCase, query, filters)
     }
 
     @Test
     fun `getRefreshKey() should return null when there is no anchorPosition`() {
         val state = getPageState(anchorPosition = null)
-        val refreshKey = searchActorsPagingSource.getRefreshKey(state)
+        val refreshKey = searchMoviesPagingSource.getRefreshKey(state)
         Truth.assertThat(refreshKey).isEqualTo(null)
     }
 
     @Test
     fun `getRefreshKey() should return null when there is no prevKey or nextKey`() {
         val state = getPageState(anchorPosition = 0)
-        val refreshKey = searchActorsPagingSource.getRefreshKey(state)
+        val refreshKey = searchMoviesPagingSource.getRefreshKey(state)
         Truth.assertThat(refreshKey).isEqualTo(null)
     }
 
     @Test
     fun `getRefreshKey() should return null when there is data`() {
         val state = getPageState(anchorPosition = 0, data = emptyList())
-        val refreshKey = searchActorsPagingSource.getRefreshKey(state)
+        val refreshKey = searchMoviesPagingSource.getRefreshKey(state)
         Truth.assertThat(refreshKey).isEqualTo(null)
     }
 
     @Test
     fun `getRefreshKey() should return first page when there is no prevKey or nextKey`() {
         val state = getPageState(anchorPosition = 1, nextKey = 2)
-        val refreshKey = searchActorsPagingSource.getRefreshKey(state)
+        val refreshKey = searchMoviesPagingSource.getRefreshKey(state)
         Truth.assertThat(refreshKey).isEqualTo(1)
     }
 
     @Test
     fun `getRefreshKey() should return second page when after prevKey 1 with anchorPosition 0`() {
         val state = getPageState(anchorPosition = 0, prevKey = 1)
-        val refreshKey = searchActorsPagingSource.getRefreshKey(state)
+        val refreshKey = searchMoviesPagingSource.getRefreshKey(state)
         Truth.assertThat(refreshKey).isEqualTo(2)
     }
 
     @Test
     fun `getRefreshKey() should return first page when after nextKey 2 with anchorPosition 0`() {
         val state = getPageState(anchorPosition = 0, nextKey = 2)
-        val refreshKey = searchActorsPagingSource.getRefreshKey(state)
+        val refreshKey = searchMoviesPagingSource.getRefreshKey(state)
         Truth.assertThat(refreshKey).isEqualTo(1)
     }
 
     @Test
     fun `load() should returns correct data when successful`() = runTest {
-        val mockData = FakeData.actorOutputs
+        val mockData = FakeData.moviesOutput
         val params = getPagingParams(key = null)
-        coEvery { searchActorsUseCase.execute(query, 1) } returns mockData
-        val result = searchActorsPagingSource.load(params)
+        coEvery { searchMoviesUseCase.execute(query, 1, filters) } returns mockData
+        val result = searchMoviesPagingSource.load(params)
 
         val page = result as LoadResult.Page
         Truth.assertThat(page.data).isEqualTo(mockData)
@@ -79,10 +82,10 @@ class SearchActorsPagingSourceTest {
 
     @Test
     fun `load() should returns empty list when there is no data`() = runTest {
-        val mockData = FakeData.actorOutputs
+        val mockData = FakeData.moviesOutput
         val params = getPagingParams(key = 2)
-        coEvery { searchActorsUseCase.execute(query, 1) } returns mockData
-        val result = searchActorsPagingSource.load(params)
+        coEvery { searchMoviesUseCase.execute(query, 1, filters) } returns mockData
+        val result = searchMoviesPagingSource.load(params)
 
         val page = result as LoadResult.Page
         Truth.assertThat(page.data).isEqualTo(emptyList<SearchActorOutput>())
@@ -101,8 +104,8 @@ class SearchActorsPagingSourceTest {
         anchorPosition: Int? = null,
         prevKey: Int? = null,
         nextKey: Int? = null,
-        data: List<SearchActorOutput> = FakeData.actorOutputs,
-    ): PagingState<Int, SearchActorOutput> {
+        data: List<SearchMovieOutput> = FakeData.moviesOutput,
+    ): PagingState<Int, SearchMovieOutput> {
         return PagingState(
             anchorPosition = anchorPosition,
             config = PagingConfig(pageSize = 10),
