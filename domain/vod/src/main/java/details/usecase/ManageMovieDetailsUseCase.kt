8f5f5ab4026afe9115095070e8611908090a27cd
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-18 09:18:28 +0300

    add manage episode details use case

---
+++ domain/vod/src/main/java/details/usecase/ManageMovieDetailsUseCase.kt
@@ -0,0 +1,32 @@
+package details.usecase
+
+import details.repository.MovieRepository
+import entity.Actor
+import entity.Movie
+import entity.Genre
+import entity.Review
+
+class ManageMovieDetailsUseCase(
+    private val movieRepo: MovieRepository)
+{
+    suspend fun getMovieDetails(id: Int): Movie =
+        movieRepo.getMovieDetails(id)
+
+    suspend fun getMovieCast(id: Int): List<Actor> =
+        movieRepo.getMovieCast(id)
+
+    suspend fun getMovieImages(id: Int): List<String> =
+        movieRepo.getImages(id)
+
+    suspend fun getMoviesByCategory(category: Genre): List<Movie> =
+        movieRepo.getMoviesByCategory(category)
+
+    suspend fun getMovieTrailer(id: Int): String? =
+        movieRepo.getMovieTrailer(id)
+
+    suspend fun getReviewsByMovieId(movieId: Int): List<Review> =
+        movieRepo.getReviewsByMovieId(movieId)
+
+    suspend fun getSimilarMoviesByMovieId(movieId: Int): List<Movie> =
+        movieRepo.getSimilarMoviesByMovieId(movieId)
+}
