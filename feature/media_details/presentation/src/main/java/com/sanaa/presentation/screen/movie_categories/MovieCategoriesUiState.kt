d6784f3626b5119d64269184efecf997be8f3d73
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-18 12:10:49 +0300

    change genre display name to localized string

--- feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/movie_categories/MovieCategoriesUiState.kt
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/movie_categories/MovieCategoriesUiState.kt
@@ -1,10 +1,13 @@
 package com.sanaa.presentation.screen.movie_categories
 
 import androidx.compose.material3.BottomAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.sanaa.presentation.R
 import entity.Genre
 
 data class MovieCategoriesScreenUiState(
-    val title: String = "",
+    val title: Genre = Genre.DRAMA,
     val movies: List<MovieCardUiModel> = emptyList(),
     val isLoading: Boolean = false,
     val error: String? = null,
@@ -18,32 +21,35 @@ data class MovieCardUiModel(
     val rating: Float = 0.0f,
 )
 
-val GenreDisplayNameMap = mapOf(
-    Genre.ACTION to "Action",
-    Genre.ADVENTURE to "Adventure",
-    Genre.COMEDY to "Comedy",
-    Genre.DRAMA to "Drama",
-    Genre.HORROR to "Horror",
-    Genre.SCIENCE_FICTION to "Science Fiction",
-    Genre.FANTASY to "Fantasy",
-    Genre.ROMANCE to "Romance",
-    Genre.THRILLER to "Thriller",
-    Genre.DOCUMENTARY to "Documentary",
-    Genre.ANIMATION to "Animation",
-    Genre.CRIME to "Crime",
-    Genre.FAMILY to "Family",
-    Genre.HISTORY to "History",
-    Genre.KIDS to "Kids",
-    Genre.MYSTERY to "Mystery",
-    Genre.MUSIC to "Music",
-    Genre.NEWS to "News",
-    Genre.REALITY to "Reality",
-    Genre.SOAP to "Soap",
-    Genre.TALK to "Talk Show",
-    Genre.WAR to "War",
-    Genre.WAR_AND_POLITICS to "War and Politics",
-    Genre.WESTERN to "Western",
-    Genre.TV_MOVIE to "TV Movie",
-    Genre.ACTION_AND_ADVENTURE to "Action & Adventure",
-    Genre.SCI_FI_AND_FANTASY to "Sci-Fi & Fantasy"
-)
+@Composable
+fun Genre.toLocalizedString(): String {
+    return when (this) {
+        Genre.DRAMA -> stringResource(R.string.genre_drama)
+        Genre.COMEDY -> stringResource(R.string.genre_comedy)
+        Genre.ADVENTURE -> stringResource(R.string.genre_adventure)
+        Genre.ACTION -> stringResource(R.string.genre_action)
+        Genre.ROMANCE -> stringResource(R.string.genre_romance)
+        Genre.FANTASY -> stringResource(R.string.genre_fantasy)
+        Genre.SCIENCE_FICTION -> stringResource(R.string.genre_science_fiction)
+        Genre.HORROR -> stringResource(R.string.genre_horror)
+        Genre.CRIME -> stringResource(R.string.genre_crime)
+        Genre.ANIMATION -> stringResource(R.string.genre_animation)
+        Genre.DOCUMENTARY -> stringResource(R.string.genre_documentary)
+        Genre.THRILLER -> stringResource(R.string.genre_thriller)
+        Genre.MUSIC -> stringResource(R.string.genre_music)
+        Genre.MYSTERY -> stringResource(R.string.genre_mystery)
+        Genre.KIDS -> stringResource(R.string.genre_kids)
+        Genre.HISTORY -> stringResource(R.string.genre_history)
+        Genre.FAMILY -> stringResource(R.string.genre_family)
+        Genre.WAR -> stringResource(R.string.genre_war)
+        Genre.TALK -> stringResource(R.string.genre_talk)
+        Genre.SOAP -> stringResource(R.string.genre_soap)
+        Genre.REALITY -> stringResource(R.string.genre_reality)
+        Genre.NEWS -> stringResource(R.string.genre_news)
+        Genre.TV_MOVIE -> stringResource(R.string.genre_tv_movie)
+        Genre.WESTERN -> stringResource(R.string.genre_western)
+        Genre.WAR_AND_POLITICS -> stringResource(R.string.genre_war_and_politics)
+        Genre.SCI_FI_AND_FANTASY -> stringResource(R.string.genre_sci_fi_and_fantasy)
+        Genre.ACTION_AND_ADVENTURE -> stringResource(R.string.genre_action_and_adventure)
+    }
+}
