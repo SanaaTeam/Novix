92f2f69e35848f530416287a44f5b21a8c6000a4
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-20 19:04:02 +0300

    add tests for SearchMediaMapper

---
+++ data/repositories/search/src/test/java/com/sanaa/search/mapper/SearchMediaMapperTest.kt
@@ -0,0 +1,11 @@
+package com.sanaa.search.mapper
+
+import com.sanaa.search.dataSource.local.dto.MovieLocalDto
+import com.sanaa.search.dataSource.local.dto.TvSeriesLocalDto
+import com.sanaa.search.dataSource.remote.dto.MovieSearchDto
+import com.sanaa.search.dataSource.remote.dto.TvShowSearchDto
+
+class SearchMediaMapperTest {
+
+    
+}
