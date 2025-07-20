fbf5bae3c25b5d837341d7ded72d658a727d61b5
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-07-20 21:11:52 +0300

    rename navigationModule to apiModule and change single of to factory of

---
+++ app/src/main/java/com/sanaa/novix/di/apiModule.kt
@@ -0,0 +1,14 @@
+package com.sanaa.novix.di
+
+import com.sanaa.api.MediaDetailsApi
+import com.sanaa.api.SearchFeatureApi
+import com.sanaa.presentation.navigation.SearchFeatureApiImpl
+import com.sanaa.presentation.ui.MediaDetailsApiImpl
+import org.koin.core.module.dsl.factoryOf
+import org.koin.dsl.bind
+import org.koin.dsl.module
+
+val apiModule = module {
+    factoryOf(::SearchFeatureApiImpl) bind SearchFeatureApi::class
+    factoryOf(::MediaDetailsApiImpl) bind MediaDetailsApi::class
+}
