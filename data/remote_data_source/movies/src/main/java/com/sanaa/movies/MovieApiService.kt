972e58ce0546e2caccaf592c1fb6cfc366686810
haider <haideraljawasim@gmail.com>
2025-07-21 03:46:45 +0300

    add MovieApiService for retrofit

---
+++ data/remote_data_source/movies/src/main/java/com/sanaa/movies/MovieApiService.kt
@@ -0,0 +1,35 @@
+package com.sanaa.movies
+
+import com.sanaa.movies.dataSource.remote.dto.MovieDto
+import com.sanaa.movies.dataSource.remote.dto.MovieVideoDto
+import com.sanaa.movies.dataSource.remote.dto.ReviewDto
+import com.sanaa.movies.response.MovieApiResponse
+import com.sanaa.movies.response.MovieCastResponse
+import com.sanaa.movies.response.MovieImagesResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface MovieApiService {
+
+    @GET("movie/{movie_id}")
+    suspend fun fetchMovieDetails(@Path("movie_id") id: Int): MovieDto
+
+    @GET("movie/{movie_id}/images")
+    suspend fun fetchImagesUrl(@Path("movie_id") id: Int): MovieImagesResponse
+
+    @GET("movie/{movie_id}/credits")
+    suspend fun fetchCast(@Path("movie_id") id: Int): MovieCastResponse
+
+    @GET("movie/{movie_id}/similar")
+    suspend fun fetchSimilarMoviesByMovieId(@Path("movie_id") id: Int): MovieApiResponse<MovieDto>
+
+    @GET("movie/{movie_id}/reviews")
+    suspend fun fetchReviewsByMovieId(@Path("movie_id") id: Int): MovieApiResponse<ReviewDto>
+
+    @GET("discover/movie")
+    suspend fun fetchMoviesByCategory(@Query("with_genres") category: Int): MovieApiResponse<MovieDto>
+
+    @GET("movie/{movie_id}/videos")
+    suspend fun fetchMovieTrailerUrl(@Path("movie_id") id: Int): MovieApiResponse<MovieVideoDto>
+}
