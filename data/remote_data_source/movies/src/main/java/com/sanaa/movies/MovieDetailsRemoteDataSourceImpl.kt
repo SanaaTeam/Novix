2e4297b48bb6f81911366f205b29734fe9c08793
haider <haideraljawasim@gmail.com>
2025-07-21 03:48:11 +0300

    use the MovieApiService for MovieRemoteDataSource

--- data/remote_data_source/movies/src/main/java/com/sanaa/movies/MovieDetailsRemoteDataSourceImpl.kt
+++ data/remote_data_source/movies/src/main/java/com/sanaa/movies/MovieDetailsRemoteDataSourceImpl.kt
@@ -6,54 +6,32 @@ import com.sanaa.movies.dataSource.remote.dto.MovieDto
 import com.sanaa.movies.dataSource.remote.dto.MovieImagesDto
 import com.sanaa.movies.dataSource.remote.dto.MovieVideoDto
 import com.sanaa.movies.dataSource.remote.dto.ReviewDto
-import com.sanaa.movies.response.MovieApiResponse
-import com.sanaa.movies.response.MovieCastResponse
-import com.sanaa.movies.response.MovieImagesResponse
 import com.sanaa.preferences.service.LanguageProvider
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.client.request.parameter
-import io.ktor.util.reflect.TypeInfo
-import io.ktor.util.reflect.typeInfo
 
 class MovieDetailsRemoteDataSourceImpl(
-    private val client: HttpClient, private val languageProvider: LanguageProvider
+    private val apiService: MovieApiService,
+    private val languageProvider: LanguageProvider
 ) : MovieDetailsRemoteDataSource {
 
-    override suspend fun fetchMovieDetails(id: Int): MovieDto = fetch("movie/$id")
+    override suspend fun fetchMovieDetails(id: Int): MovieDto = apiService.fetchMovieDetails(id)
 
     override suspend fun fetchImagesUrl(id: Int): List<MovieImagesDto> =
-        fetch<MovieImagesResponse>("movie/$id/images").backdrops
+        apiService.fetchImagesUrl(id).backdrops
 
+    override suspend fun fetchCast(id: Int): List<ActorDto> = apiService.fetchCast(id).cast
 
-    override suspend fun fetchCast(id: Int): List<ActorDto> =
-        fetch<MovieCastResponse>("movie/$id/credits").cast
 
     override suspend fun fetchSimilarMoviesByMovieId(id: Int): List<MovieDto> =
-        fetch<MovieApiResponse<MovieDto>>("movie/$id/similar").results
-
+        apiService.fetchSimilarMoviesByMovieId(id).results
 
     override suspend fun fetchReviewsByMovieId(id: Int): List<ReviewDto> =
-        fetch<MovieApiResponse<ReviewDto>>("movie/$id/reviews").results
+        apiService.fetchReviewsByMovieId(id).results
 
 
     override suspend fun fetchMoviesByCategory(category: Int): List<MovieDto> =
-        fetch<MovieApiResponse<MovieDto>>("discover/movie?with_genres=$category").results
-
+        apiService.fetchMoviesByCategory(category).results
 
     override suspend fun fetchMovieTrailerUrl(id: Int): List<MovieVideoDto> =
-        fetch<MovieApiResponse<MovieVideoDto>>("movie/$id/videos", withLang = false).results
+        apiService.fetchMovieTrailerUrl(id).results
 
-
-    private suspend inline fun <reified T> fetch(
-        path: String, withLang: Boolean = true
-    ): T = fetchInternal(path, withLang, typeInfo<T>())
-
-    private suspend fun <T> fetchInternal(
-        path: String, withLang: Boolean, type: TypeInfo
-    ): T = client.get("${BuildConfig.TMDB_URL}/$path") {
-        if (withLang) parameter("language", languageProvider.getCurrentLanguage())
-        parameter("api_key", BuildConfig.TMDB_API_KEY)
-    }.body(type)
 }
