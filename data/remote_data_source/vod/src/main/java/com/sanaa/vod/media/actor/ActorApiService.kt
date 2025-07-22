9f558cbdb87e0a7ad30285738695a1a4d8b2fcca
haider <haideraljawasim@gmail.com>
2025-07-22 18:49:48 +0300

    add ActorApiService

---
+++ data/remote_data_source/vod/src/main/java/com/sanaa/vod/media/actor/ActorApiService.kt
@@ -0,0 +1,24 @@
+package com.sanaa.vod.media.actor
+
+import com.sanaa.vod.dataSource.remote.dto.ActorDto
+import com.sanaa.vod.media.actor.response.ActorCastCreditsResponse
+import com.sanaa.vod.media.actor.response.ActorImagesResponse
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Path
+
+interface ActorApiService {
+    @GET("person/{actor_id}")
+    suspend fun fetchActorDetails(@Path("actor_id") id: Int): ActorDto
+
+    @GET("person/{actor_id}/images")
+    @Headers("Ignore-Language: true")
+    suspend fun fetchActorImages(@Path("actor_id") id: Int): ActorImagesResponse
+
+    @GET("person/{actor_id}/movie_credits")
+    suspend fun fetchActorTopMovies(@Path("actor_id") id: Int): ActorCastCreditsResponse
+
+    @GET("person/{actor_id}/tv_credits")
+    suspend fun fetchActorTopTvSeries(@Path("actor_id") id: Int): ActorCastCreditsResponse
+
+}
