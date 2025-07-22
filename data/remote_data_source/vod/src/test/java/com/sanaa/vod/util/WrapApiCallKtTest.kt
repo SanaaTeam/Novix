6f00868a722bf0c69fc265fee012de6e9f0c4386
haider <haideraljawasim@gmail.com>
2025-07-23 00:12:01 +0300

    add test for wrapApiCall function

--- data/remote_data_source/vod/src/test/java/com/sanaa/vod/util/WrapApiCallKtTest.kt
+++ data/remote_data_source/vod/src/test/java/com/sanaa/vod/util/WrapApiCallKtTest.kt
@@ -23,32 +23,50 @@ class WrapApiCallTest {
         val result = runCatching {
             wrapApiCall { "Success" }
         }
-
         assertThat(result.getOrNull()).isEqualTo("Success")
+    }
+
+    @Test
+    fun `should not throw exception when execute succeeds`() = runTest {
+        val result = runCatching {
+            wrapApiCall { "Success" }
+        }
         assertThat(result.exceptionOrNull()).isNull()
     }
 
     @Test
     fun `should throw NoNetworkException when UnknownHostException occurs`() = runTest {
         val exception = UnknownHostException()
-
         val result = runCatching {
             wrapApiCall { throw exception }
         }
-
         assertThat(result.exceptionOrNull()).isInstanceOf(NoNetworkException::class.java)
+    }
+
+    @Test
+    fun `NoNetworkException message should be No internet connection`() = runTest {
+        val exception = UnknownHostException()
+        val result = runCatching {
+            wrapApiCall { throw exception }
+        }
         assertThat(result.exceptionOrNull()!!.message).isEqualTo("No internet connection")
     }
 
     @Test
     fun `should throw TimeoutException when SocketTimeoutException occurs`() = runTest {
         val exception = SocketTimeoutException()
-
         val result = runCatching {
             wrapApiCall { throw exception }
         }
-
         assertThat(result.exceptionOrNull()).isInstanceOf(TimeoutException::class.java)
+    }
+
+    @Test
+    fun `TimeoutException message should be Request timed out`() = runTest {
+        val exception = SocketTimeoutException()
+        val result = runCatching {
+            wrapApiCall { throw exception }
+        }
         assertThat(result.exceptionOrNull()!!.message).isEqualTo("Request timed out")
     }
 
@@ -56,36 +74,55 @@ class WrapApiCallTest {
     fun `should throw ServerErrorException when HttpException occurs`() = runTest {
         val responseBody = "".toResponseBody("application/json".toMediaType())
         val exception = HttpException(Response.error<Any>(500, responseBody))
-
         val result = runCatching {
             wrapApiCall { throw exception }
         }
-
         assertThat(result.exceptionOrNull()).isInstanceOf(ServerErrorException::class.java)
+    }
+
+    @Test
+    fun `ServerErrorException message should be Server error`() = runTest {
+        val responseBody = "".toResponseBody("application/json".toMediaType())
+        val exception = HttpException(Response.error<Any>(500, responseBody))
+        val result = runCatching {
+            wrapApiCall { throw exception }
+        }
         assertThat(result.exceptionOrNull()!!.message).isEqualTo("Server error")
     }
 
     @Test
     fun `should throw ParsingException when SerializationException occurs`() = runTest {
         val exception = SerializationException("bad json")
-
         val result = runCatching {
             wrapApiCall { throw exception }
         }
-
         assertThat(result.exceptionOrNull()).isInstanceOf(ParsingException::class.java)
+    }
+
+    @Test
+    fun `ParsingException message should be Parsing error`() = runTest {
+        val exception = SerializationException("bad json")
+        val result = runCatching {
+            wrapApiCall { throw exception }
+        }
         assertThat(result.exceptionOrNull()!!.message).isEqualTo("Parsing error")
     }
 
     @Test
     fun `should throw UnknownDataSourceException when unknown Exception occurs`() = runTest {
         val exception = RuntimeException("unknown error")
-
         val result = runCatching {
             wrapApiCall { throw exception }
         }
-
         assertThat(result.exceptionOrNull()).isInstanceOf(UnknownDataSourceException::class.java)
+    }
+
+    @Test
+    fun `UnknownDataSourceException message should be Unknown error`() = runTest {
+        val exception = RuntimeException("unknown error")
+        val result = runCatching {
+            wrapApiCall { throw exception }
+        }
         assertThat(result.exceptionOrNull()!!.message).isEqualTo("Unknown error")
     }
 }
