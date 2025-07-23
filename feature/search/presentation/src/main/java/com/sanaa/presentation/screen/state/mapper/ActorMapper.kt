8834ddaa4053552417f27a959ee052bad7c89b48
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-23 08:06:56 +0300

    add ActorMapper for search feature

---
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/state/mapper/ActorMapper.kt
@@ -0,0 +1,12 @@
+package com.sanaa.presentation.screen.state.mapper
+
+import com.sanaa.presentation.screen.state.ActorUiModel
+import search.usecase.search_param.SearchActorOutput
+
+fun SearchActorOutput.toUiState(): ActorUiModel {
+    return ActorUiModel(
+        id= this.id,
+        name = this.name,
+        imageUrl = this.profileImageUrl
+    )
+}
