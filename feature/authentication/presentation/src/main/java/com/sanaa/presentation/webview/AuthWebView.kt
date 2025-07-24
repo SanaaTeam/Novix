bf2ad558244714be8b5217ac58568ff991a4d724
ElhamFadel <elham2000fadel@gmail.com>
2025-07-24 13:03:42 +0300

    built auth web view

---
+++ feature/authentication/presentation/src/main/java/com/sanaa/presentation/webview/AuthWebView.kt
@@ -0,0 +1,22 @@
+package com.sanaa.feature.authentication.presentation.webview
+
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun AuthWebView(webPageUrl: String, modifier: Modifier = Modifier) {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                webViewClient = WebViewClient()
+                loadUrl(webPageUrl)
+            }
+        },
+        modifier = modifier
+    )
+}
