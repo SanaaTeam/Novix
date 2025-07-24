371b3b74e91268701e5e3c2da624bab29b281e73
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-25 02:13:39 +0300

    remove material dependency from home modules

--- feature/home/presentation/build.gradle.kts
+++ feature/home/presentation/build.gradle.kts
@@ -19,7 +19,6 @@ dependencies {
     implementation(libs.bundles.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
     testImplementation(libs.bundles.test)
     androidTestImplementation(libs.androidx.junit)
     implementation(libs.androidx.navigation.compose)
