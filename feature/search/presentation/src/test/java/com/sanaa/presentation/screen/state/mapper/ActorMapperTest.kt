fa4c4c9d1356872564d5c8d58e79ee4e97723ca6
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-24 13:23:25 +0300

    remove commented lines in ActorMapperTest

--- feature/search/presentation/src/test/java/com/sanaa/presentation/screen/state/mapper/ActorMapperTest.kt
+++ feature/search/presentation/src/test/java/com/sanaa/presentation/screen/state/mapper/ActorMapperTest.kt
@@ -3,23 +3,22 @@ package com.sanaa.presentation.screen.state.mapper
 import com.google.common.truth.Truth.assertThat
 import com.sanaa.presentation.screen.state.ActorUiModel
 import org.junit.jupiter.api.Test
-import search.usecase.search_param.SearchActorOutput
+import usecase.search.search_param.SearchActorOutput
 
 class ActorMapperTest {
 
     @Test
     fun `toUiState should map SearchActorOutput to ActorUiModel correctly`() {
-        // Arrange
         val searchActorOutput = SearchActorOutput(
             id = 1,
             name = "Sanaa",
             profileImageUrl = "https://example.com/image.jpg"
         )
 
-        // Act
+
         val result = searchActorOutput.toUiState()
 
-        // Assert
+
         val expected = ActorUiModel(
             id = 1,
             name = "Sanaa",
