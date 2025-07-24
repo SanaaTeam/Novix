e9eb785ea29ee475c76661cad31909fbcf683820
AimanYosofi <=>
2025-07-24 22:02:01 +0300

    prepare repository exceptions

---
+++ data/repositories/identity/src/main/java/com/sanaa/identity/exceptions/Exceptions.kt
@@ -0,0 +1,17 @@
+package com.sanaa.identity.exceptions
+
+
+open class AuthenticationException(message: String?) : Exception(message ?: "Unknown Error")
+
+open class LoginErrorException(message: String?) : AuthenticationException(message)
+
+class InvalidUserOrPasswordException(
+    userName: String,
+) : LoginErrorException("Invalid username($userName) and/or password: You did not provide a valid login.")
+
+class EmailNotVerifiedException(
+    userName: String,
+) : LoginErrorException("Email not verified for ($userName): Your email address has not been verified.")
+
+class InvalidTokenException(message: String?) : AuthenticationException(message)
+class CreateGuestTokenException(message: String?) : AuthenticationException(message)
