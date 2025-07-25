8650615849f5d2b5b9c94d4f17415ea02cfb1020
AimanYosofi <=>
2025-07-25 10:57:13 +0300

    fix issues after merge pull from server

--- app/src/main/java/com/sanaa/novix/di/appModule.kt
+++ app/src/main/java/com/sanaa/novix/di/appModule.kt
@@ -2,7 +2,6 @@ package com.sanaa.novix.di
 
 import com.sanaa.novix.di.details_modules.detailsModule
 import com.sanaa.novix.di.identity_modules.identityModule
-import com.sanaa.novix.di.login_modules.authenticationModule
 import com.sanaa.novix.di.search_modules.searchModule
 import org.koin.dsl.module
 
@@ -10,6 +9,5 @@ val appModule = module {
     includes(
         firebaseModule, loggingModule, apiModule, networkModule, preferencesModule,
         detailsModule, searchModule, identityModule
-        detailsModule, searchModule, authenticationModule
     )
 }
--- app/src/main/java/com/sanaa/novix/di/login_modules/authenticationModule.kt
+++
@@ -1,7 +0,0 @@
-package com.sanaa.novix.di.login_modules
-
-import org.koin.dsl.module
-
-val authenticationModule = module {
-    viewModelLoginModule
-}
