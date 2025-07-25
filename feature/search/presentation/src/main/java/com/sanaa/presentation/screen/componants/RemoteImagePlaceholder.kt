892e917b77fe07051a6a4d8428239e346004f556
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-25 17:34:35 +0300

    update contentScale of RemoteImagePlaceholder

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/RemoteImagePlaceholder.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/RemoteImagePlaceholder.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
@@ -26,7 +27,8 @@ fun RemoteImagePlaceholder(modifier: Modifier = Modifier) {
         Image(
             painter = painterResource(id = placeholderResId),
             contentDescription = "Remote Image Placeholder",
-            modifier = modifier.size(56.dp)
+            modifier = modifier.size(56.dp),
+            contentScale = ContentScale.None
         )
     }
 }
