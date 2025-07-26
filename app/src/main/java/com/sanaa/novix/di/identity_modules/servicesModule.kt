2c55cf7b7efd65e97963e4d6f42860eb9a3fddd3
AimanYosofi <=>
2025-07-26 12:31:18 +0300

    fix di issue in the service module

--- app/src/main/java/com/sanaa/novix/di/identity_modules/servicesModule.kt
+++ app/src/main/java/com/sanaa/novix/di/identity_modules/servicesModule.kt
@@ -4,11 +4,14 @@ import com.sanaa.identity.dataSoruce.dataStore.PreferencesManagerImpl
 import com.sanaa.identity.dataSoruce.local.dataStore.PreferencesManager
 import com.sanaa.identity.service.AuthenticationService
 import com.sanaa.identity.service.AuthenticationServiceImpl
+import com.sanaa.novix.resourceProvider.StringProviderImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
+import service.StringProvider
 
 val servicesModule = module {
+    singleOf(::StringProviderImpl) bind StringProvider::class
     singleOf(::AuthenticationServiceImpl) bind AuthenticationService::class
     singleOf(::PreferencesManagerImpl) bind PreferencesManager::class
 }
