be677b62a32b544c1b19e296cd32adec10d718ef
AimanYosofi <=>
2025-07-26 12:30:04 +0300

    fix di issue in the domain

--- app/src/main/java/com/sanaa/novix/di/identity_modules/domainIdentityModule.kt
+++ app/src/main/java/com/sanaa/novix/di/identity_modules/domainIdentityModule.kt
@@ -1,7 +1,9 @@
 package com.sanaa.novix.di.identity_modules
 
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import usecase.LoginUseCase
 
 val domainIdentityModule = module {
-
+    singleOf(::LoginUseCase)
 }
