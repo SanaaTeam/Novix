c14c95372b33d8e0fda156ac4a299531a7f764b2
haider <haideraljawasim@gmail.com>
2025-07-28 16:42:49 +0300

    add paging data on top rated movies and tv shows

--- feature/home/presentation/src/main/java/com/sanaa/presentation/components/MediaListGrid.kt
+++ feature/home/presentation/src/main/java/com/sanaa/presentation/components/MediaListGrid.kt
@@ -1,33 +1,29 @@
 package com.sanaa.presentation.components
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
 import com.sanaa.designsystem.R
 import com.sanaa.designsystem.design_system.component.blur.OnBlurContent
-import com.sanaa.designsystem.design_system.theme.NovixTheme
 import com.sanaa.designsystem.design_system.theme.Theme
 import com.sanaa.image_viewer.component.RemoteBlurredHaramImageViewer
 import com.sanaa.presentation.components.cards.MediaPosterCard
 import com.sanaa.presentation.components.chips.SaveIconChip
 import com.sanaa.presentation.state.MediaItem
-import com.sanaa.presentation.state.MediaType
 
 @Composable
 fun MediaListGrid(
-    mediaList: List<MediaItem>,
+    mediaList: LazyPagingItems<MediaItem>,
     modifier: Modifier = Modifier,
     isScrollEnabled: Boolean = true,
     onMediaClick: (MediaItem) -> Unit = {},
@@ -41,7 +37,8 @@ fun MediaListGrid(
         horizontalArrangement = Arrangement.spacedBy(12.dp),
         userScrollEnabled = isScrollEnabled
     ) {
-        items (items = mediaList, key = { item -> item.id }) { media ->
+        items(mediaList.itemCount) { index ->
+            val media = mediaList[index] ?: return@items
             MediaPosterCard(
                 posterImage = {
                     RemoteBlurredHaramImageViewer(
@@ -61,7 +58,8 @@ fun MediaListGrid(
                         OnBlurContent(
                             hintText = stringResource(R.string.unsuitable_image),
                             textStyle = Theme.textStyle.body.small.copy(
-                                color = Color(0x99FFFFFF)),
+                                color = Color(0x99FFFFFF)
+                            ),
                             iconSize = 24.dp,
                             icon = painterResource(R.drawable.icon_eye_slash),
                         )
@@ -70,37 +68,5 @@ fun MediaListGrid(
                 topLeftContent = { SaveIconChip(onClick = { onSaveIconClick(media) }) },
                 onCardClick = { onMediaClick(media) })
         }
-    }
-}
-
-@PreviewLightDark
-@Composable
-fun MediaListGridPreview(modifier: Modifier = Modifier) {
-    NovixTheme (isDarkMode = isSystemInDarkTheme()) {
-        MediaListGrid(
-            mediaList = listOf(
-                MediaItem(
-                    id = 1,
-                    title = "Media 1",
-                    imageUrl = "https://example.com/image1.jpg",
-                    mediaType = MediaType.MOVIE,
-                ),
-                MediaItem(
-                    id = 2,
-                    title = "Media 2",
-                    imageUrl = "https://example.com/image2.jpg",
-                    mediaType = MediaType.TV_SHOW
-                ),
-                MediaItem(
-                    id = 3,
-                    title = "Media 3",
-                    imageUrl = "https://example.com/image3.jpg",
-                    mediaType = MediaType.MOVIE
-                )
-            ),
-            modifier = modifier,
-            onMediaClick = {},
-            onSaveIconClick = {}
-        )
     }
 }
