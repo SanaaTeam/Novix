5912ec333d271f93e2a29b717bbd220da7a6e816
haider <haideraljawasim@gmail.com>
2025-07-28 17:26:47 +0300

    add base paging source for detatils

---
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/details_base/BasePagingSource.kt
@@ -0,0 +1,36 @@
+package com.sanaa.presentation.details_base
+
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+
+class BasePagingSource<T : Any>(
+    private val fetchItems: suspend (page: Int) -> List<T>
+) : PagingSource<Int, T>() {
+
+    override fun getRefreshKey(state: PagingState<Int, T>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            val anchorPage = state.closestPageToPosition(anchorPosition)
+            anchorPage?.prevKey?.plus(1) ?: anchorPage?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, T> {
+        val page = params.key ?: STARTING_PAGE_INDEX
+
+        return try {
+            val items = fetchItems(page)
+            LoadResult.Page(
+                data = items,
+                prevKey = if (page == STARTING_PAGE_INDEX) null else page.minus(1),
+                nextKey = if (items.isEmpty()) null else page.plus(1)
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+    companion object {
+        private const val STARTING_PAGE_INDEX = 1
+    }
+}
