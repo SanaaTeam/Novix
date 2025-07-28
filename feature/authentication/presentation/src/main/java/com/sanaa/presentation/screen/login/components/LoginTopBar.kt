194d14b0e207dc7d8a105c63a77b2c1e6a94c541
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-28 18:29:19 +0300

    adjust LoginTopBar padding

--- feature/authentication/presentation/src/main/java/com/sanaa/presentation/screen/login/components/LoginTopBar.kt
+++ feature/authentication/presentation/src/main/java/com/sanaa/presentation/screen/login/components/LoginTopBar.kt
@@ -1,10 +1,12 @@
 package com.sanaa.presentation.screen.login.components
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.zIndex
 import com.sanaa.designsystem.design_system.component.top_bar.NovixTopBar
 import com.sanaa.designsystem.design_system.component.top_bar.TopBarClickableIcon
@@ -22,6 +24,7 @@ fun LoginTopBar(onBackClick: () -> Unit) {
         screenTitle = stringResource(R.string.login),
         modifier = Modifier
             .systemBarsPadding()
+            .padding(vertical = 8.dp)
             .zIndex(10f)
     )
 }
