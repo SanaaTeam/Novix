bbd4bc83988935b2a1539a0ef6bda902edaa1d77
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-07-29 05:33:32 +0300

    updating api key to v3 and update interceptor to use parameters instead of headers

--- app/src/main/java/com/sanaa/novix/MainActivity.kt
+++ app/src/main/java/com/sanaa/novix/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sanaa.novix
 
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -33,6 +34,7 @@ class MainActivity : ComponentActivity() {
         val sessionId: String? = runBlocking {
             preferenceManager.sessionId.firstOrNull()
         }
+        Log.d("API_KEY_CHECK", "Key: ${BuildConfig.TMDB_API_KEY}")
         setContent {
             if (TextUtils.isEmpty(sessionId))
                 authenticationApi.AuthenticationScreen(this)
--- data/remote_data_source/vod/src/main/java/com/sanaa/vod/network/interceptor/ApiKeyInterceptor.kt
+++ data/remote_data_source/vod/src/main/java/com/sanaa/vod/network/interceptor/ApiKeyInterceptor.kt
@@ -4,15 +4,34 @@ import com.sanaa.data.remotedatasource.vod.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class APIKeyInterceptor(val sessionId: () -> String?) :
-    Interceptor {
+//class APIKeyInterceptor(val sessionId: () -> String?) :
+//    Interceptor {
+//    override fun intercept(chain: Interceptor.Chain): Response {
+//        val originalRequest = chain.request()
+//        val newRequest = originalRequest.newBuilder()
+//
+//        sessionId()?.let { newRequest.addHeader("session_id", it) }
+//        newRequest.addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_KEY}")
+//        return chain.proceed(newRequest.build())
+//    }
+//}
+class APIKeyInterceptor(val sessionId: () -> String?) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
+        val originalUrl = originalRequest.url
+
+        // Add the v3 API key as a query parameter
+        val newUrl = originalUrl.newBuilder()
+            .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY) // this should now be your v3 key
+            .build()
+
         val newRequest = originalRequest.newBuilder()
+            .url(newUrl)
+            .apply {
+                sessionId()?.let { addHeader("session_id", it) }
+            }
+            .build()
 
-        sessionId()?.let { newRequest.addHeader("session_id", it) }
-        newRequest.addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_KEY}")
-
-        return chain.proceed(newRequest.build())
+        return chain.proceed(newRequest)
     }
 }
