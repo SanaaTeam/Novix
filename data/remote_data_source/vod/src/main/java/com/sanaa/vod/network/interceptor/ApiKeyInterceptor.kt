21d6cfb0880d2e04b0a29865fbbdfce4f44e9ba5
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-30 01:35:07 +0300

    Revert "delete NetworkProvider, LanguageInterceptor, and ApiKeyInterceptor"
    This reverts commit ea13bbe34f045063575bf5822a8a7be3b16429b8.

---
+++ data/remote_data_source/vod/src/main/java/com/sanaa/vod/network/interceptor/ApiKeyInterceptor.kt
@@ -0,0 +1,36 @@
+package com.sanaa.vod.network.interceptor
+
+import com.sanaa.data.remotedatasource.vod.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+//class APIKeyInterceptor(val sessionId: () -> String?) :
+//    Interceptor {
+//    override fun intercept(chain: Interceptor.Chain): Response {
+//        val originalRequest = chain.request()
+//        val newRequest = originalRequest.newBuilder()
+//
+//        sessionId()?.let { newRequest.addHeader("session_id", it) }
+//        newRequest.addHeader("Authorization", "Bearer ${BuildConfig.TMDB_API_KEY}")
+//        return chain.proceed(newRequest.build())
+//    }
+//}
+class APIKeyInterceptor(val sessionId: () -> String?) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalUrl = originalRequest.url
+
+        val newUrl = originalUrl.newBuilder()
+            .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
+            .build()
+
+        val newRequest = originalRequest.newBuilder()
+            .url(newUrl)
+            .apply {
+                sessionId()?.let { addHeader("session_id", it) }
+            }
+            .build()
+
+        return chain.proceed(newRequest)
+    }
+}
