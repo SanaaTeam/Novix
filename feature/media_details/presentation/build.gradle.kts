91cceceacdcbc076c061e70ebe741784fc376c06
ZainabAlnajjar <eng.zainab.alnajar@gmail.com>
2025-07-29 03:15:52 +0300

    add missing dependency for androidx runtime

--- feature/media_details/presentation/build.gradle.kts
+++ feature/media_details/presentation/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
 
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.bundles.compose)
     implementation(libs.androidx.ui.compose.foundation)
     implementation(platform(libs.androidx.compose.bom))
