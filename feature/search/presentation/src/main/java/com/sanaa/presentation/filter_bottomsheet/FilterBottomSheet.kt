4323e3070c78bf60138597921ca5fc7f2e837fcb
Hager Abobakr <hagarabobakr1@gmail.com>
2025-07-30 01:21:13 +0300

    center loading indicator in filter bottom sheet

--- feature/search/presentation/src/main/java/com/sanaa/presentation/filter_bottomsheet/FilterBottomSheet.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/filter_bottomsheet/FilterBottomSheet.kt
@@ -3,8 +3,8 @@ package com.sanaa.presentation.filter_bottomsheet
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -16,6 +16,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -102,9 +103,12 @@ fun FilterBottomSheetContent(
             BottomSheetHeader(onCancelClicked = onDismissRequest)
 
             AnimatedVisibility(visible = uiState.isLoading) {
-                WavyProgressIndicator()
-            }
+                Box(modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center){
+                    WavyProgressIndicator()
+                }
 
+            }
             AnimatedVisibility(visible = !uiState.isLoading) {
                 Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
                     CustomYearRangeSlider(
