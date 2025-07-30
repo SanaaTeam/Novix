0ede689b572c277eabff153f045202b5ec24801a
Radwa85 <radwa.mohamed5033@gmail.com>
2025-07-30 23:02:49 +0300

    Replace WavyProgressIndicator with NovixLoadingIndicator

--- feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/CategoryTabSection.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/screen/componants/CategoryTabSection.kt
@@ -20,6 +20,7 @@ import com.sanaa.designsystem.design_system.component.screen_state_content.Error
 import com.sanaa.designsystem.design_system.component.screen_state_content.NetworkDisconnectionContact
 import com.sanaa.designsystem.design_system.component.tab.NovixTab
 import com.sanaa.designsystem.design_system.component.indicator.WavyProgressIndicator
+import com.sanaa.designsystem.design_system.component.loading.NovixLoadingIndicator
 import com.sanaa.presentation.screen.SearchScreenInteractionsListener
 import com.sanaa.presentation.screen.state.ActorUiModel
 import com.sanaa.presentation.screen.state.MovieUiModel
@@ -58,7 +59,7 @@ fun CategoryTabSection(
         when {
             uiState.isLoading -> {
                 Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    WavyProgressIndicator()
+                    NovixLoadingIndicator()
                 }
             }
             uiState.noInternetConnection -> {
