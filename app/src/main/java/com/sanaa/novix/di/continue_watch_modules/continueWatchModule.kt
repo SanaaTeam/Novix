2d98bc8798b79facd583007d6daa584302cd26f5
EL-MANCY <abdelrahmanelmancy@gmail.com>
2025-07-31 19:10:44 +0300

    remove old modules

--- app/src/main/java/com/sanaa/novix/di/continue_watch_modules/continueWatchModule.kt
+++ app/src/main/java/com/sanaa/novix/di/continue_watch_modules/continueWatchModule.kt
@@ -4,6 +4,6 @@ import org.koin.dsl.module
 
 val continueWatchModule = module {
     includes(
-        continueWatchingDataModule, continueWatchingDomainModule, continueWatchingUseCaseModule
+        continueWatchingUseCaseModule
     )
 }
--- app/src/main/java/com/sanaa/novix/di/continue_watch_modules/continueWatchingDomainModule.kt
+++
@@ -1,8 +0,0 @@
-package com.sanaa.novix.di.continue_watch_modules
-
-import org.koin.dsl.module
-import repository.ContinueWatchingRepository
-
-val continueWatchingDomainModule = module {
-    single<ContinueWatchingRepository> { ContinueWatchingRepositoryImpl(get(), get()) }
-}
