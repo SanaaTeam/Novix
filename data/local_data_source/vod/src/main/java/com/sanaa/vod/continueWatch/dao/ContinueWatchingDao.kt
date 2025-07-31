00bb8c3bc08c43de76408151250a9ecec4b92d73
EL-MANCY <abdelrahmanelmancy@gmail.com>
2025-07-31 14:58:50 +0300

    use Upsert instead of insert

--- data/local_data_source/vod/src/main/java/com/sanaa/vod/continueWatch/dao/ContinueWatchingDao.kt
+++ data/local_data_source/vod/src/main/java/com/sanaa/vod/continueWatch/dao/ContinueWatchingDao.kt
@@ -4,13 +4,14 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Upsert
 import com.sanaa.vod.dataSource.local.continueWatch.dto.ContinueWatchingLocalDto
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ContinueWatchingDao {
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    @Upsert
     suspend fun insertOrUpdate(item: ContinueWatchingLocalDto)
 
     @Query("SELECT * FROM continue_watching WHERE username = :username LIMIT :limit")
