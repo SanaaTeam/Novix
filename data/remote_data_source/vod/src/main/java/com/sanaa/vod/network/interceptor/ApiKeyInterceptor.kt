2d6f5f2acfdbe037739bf4edfab93ecdae223ec3
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-01 21:56:47 +0300

    add Query Parameter session id

--- data/remote_data_source/vod/src/main/java/com/sanaa/vod/network/interceptor/ApiKeyInterceptor.kt
+++ data/remote_data_source/vod/src/main/java/com/sanaa/vod/network/interceptor/ApiKeyInterceptor.kt
@@ -4,20 +4,23 @@ import com.sanaa.data.remotedatasource.vod.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class APIKeyInterceptor(val sessionId: () -> String?) : Interceptor {
+class APIKeyInterceptor(
+    private val sessionId: () -> String?
+) : Interceptor {
+
     override fun intercept(chain: Interceptor.Chain): Response {
         val originalRequest = chain.request()
-        val originalUrl = originalRequest.url
-
-        val newUrl = originalUrl.newBuilder()
+        val originalUrlBuilder = originalRequest.url.newBuilder()
             .addQueryParameter("api_key", BuildConfig.TMDB_API_KEY)
-            .build()
+
+        sessionId()?.let {
+            originalUrlBuilder.addQueryParameter("session_id", it)
+        }
+
+        val newUrl = originalUrlBuilder.build()
 
         val newRequest = originalRequest.newBuilder()
             .url(newUrl)
-            .apply {
-                sessionId()?.let { addHeader("session_id", it) }
-            }
             .build()
 
         return chain.proceed(newRequest)
