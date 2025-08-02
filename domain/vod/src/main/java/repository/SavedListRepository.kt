d47c9e174564ac88fd70e1f82d6005917a744ee6
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-03 00:48:20 +0300

    create saved list repository

---
+++ domain/vod/src/main/java/repository/SavedListRepository.kt
@@ -0,0 +1,21 @@
+package repository
+
+import entity.Movie
+import entity.TvSeries
+import usecase.custom_list.custom_list_param.SavedItem
+import usecase.custom_list.custom_list_param.SavedList
+
+interface SavedListRepository {
+    suspend fun getSavedLists(): List<SavedList>
+    suspend fun createSavedList(list: SavedList): SavedList
+    suspend fun editSavedList(list: SavedList)
+    suspend fun deleteSavedList(listId: Int)
+
+    suspend fun getAllItemsInList(listId: Int): List<SavedItem>
+    suspend fun getMoviesInList(listId: Int): List<Movie>
+    suspend fun getTvSeriesInList(listId: Int): List<TvSeries>
+
+    suspend fun addMovieToList(listId: Int, movieId: Int): Boolean
+    suspend fun addTvSeriesToList(listId: Int, tvSeriesId: Int): Boolean
+    suspend fun removeItemFromList(itemId: Int)
+}
