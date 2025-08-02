c7b5b3fad219e28ec7dc8f7be0c056166b5a4b26
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-03 00:47:31 +0300

    add saved list and saved item models

---
+++ domain/vod/src/main/java/usecase/custom_list/custom_list_param/SavedItem.kt
@@ -0,0 +1,10 @@
+package usecase.custom_list.custom_list_param
+
+import usecase.search.search_param.MediaType
+
+data class SavedItem(
+    val id: Int,
+    val posterImageUrl: String,
+    val mediaType: MediaType,
+    val isSaved: Boolean,
+)
