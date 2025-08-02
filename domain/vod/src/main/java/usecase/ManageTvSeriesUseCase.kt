3a3befa011938a7cb6b1f9f2241a907d288bb861
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-02 23:12:54 +0300

    add `getUserRatedTvSeries` and `deleteTvSeriesRate` to `ManageTvSeriesUseCase`

--- domain/vod/src/main/java/usecase/ManageTvSeriesUseCase.kt
+++ domain/vod/src/main/java/usecase/ManageTvSeriesUseCase.kt
@@ -61,6 +61,14 @@ class ManageTvSeriesUseCase @Inject constructor(
         return tvSeriesRepo.addTvSeriesRate(seriesId = seriesId, rating = rating)
     }
 
+    suspend fun getUserRatedTvSeries(userId: String): List<TvSeries> {
+        return tvSeriesRepo.getUserRatedTvSeries(userId)
+    }
+
+    suspend fun deleteTvSeriesRate(seriesId: Int): Boolean {
+        return tvSeriesRepo.deleteTvSeriesRate(seriesId)
+    }
+
     private companion object {
         const val IMAGE_COUNT = 10
     }
