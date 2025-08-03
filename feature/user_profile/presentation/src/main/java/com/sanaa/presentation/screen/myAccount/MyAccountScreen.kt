ace398c315259faca8b1e2d8a490fcdb01973b98
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-08-03 07:07:11 +0300

    prepare profile screen components

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myAccount/MyAccountScreen.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myAccount/MyAccountScreen.kt
@@ -1,48 +1,29 @@
 package com.sanaa.presentation.screen.myAccount
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sanaa.designsystem.R
 import com.sanaa.designsystem.design_system.component.novix_scaffold.NovixScaffold
 import com.sanaa.designsystem.design_system.component.top_bar.TopBar
 import com.sanaa.designsystem.design_system.theme.NovixTheme
-import com.sanaa.designsystem.design_system.theme.Theme
 import com.sanaa.presentation.screen.myAccount.MyAccountScreenEffect.NavigateToChangePasswordSetting
 import com.sanaa.presentation.screen.myAccount.MyAccountScreenEffect.NavigateToContentRestrictionSetting
 import com.sanaa.presentation.screen.myAccount.MyAccountScreenEffect.NavigateToLanguageSetting
 import com.sanaa.presentation.screen.myAccount.MyAccountScreenEffect.NavigateToMyRating
 import com.sanaa.presentation.screen.myAccount.MyAccountScreenEffect.NavigateToWatchingHistory
+import com.sanaa.presentation.screen.myAccount.component.AccountOptionItem
+import com.sanaa.presentation.screen.myAccount.component.MyAccountUserInfo
+import com.sanaa.presentation.screen.myAccount.component.VerticalList
 import com.sanaa.presentation.util.Listen
 
 @Composable
@@ -74,7 +55,7 @@ fun MyAccountScreenContent(
             screenTitle = stringResource(R.string.my_account)
         )
 
-        UserInfo(uiState.username)
+        MyAccountUserInfo(uiState.username)
 
         VerticalList(
             items = listOf(
@@ -108,136 +89,7 @@ fun MyAccountScreenContent(
     }
 }
 
-@Composable
-fun Divider() {
-    Spacer(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp)
-            .height(1.dp)
-            .background(Theme.colors.stroke)
-    )
-}
-
-@Composable
-fun VerticalList(items: List<AccountOptionItem>) {
-    val verticalScroll = rememberScrollState()
-    Column(
-        modifier = Modifier
-            .padding(top = 8.dp)
-            .verticalScroll(verticalScroll)
-    ) {
-        items.forEachIndexed { index, item ->
-            AccountOption(
-                painter = item.painter,
-                title = item.title,
-                onClick = item.onClick
-            )
-
-            if (index != items.lastIndex)
-                Divider()
-        }
-    }
-}
-
-data class AccountOptionItem(
-    val painter: Painter,
-    val title: String,
-    val onClick: () -> Unit,
-)
-
-@Composable
-fun AccountOption(painter: Painter, title: String, onClick: () -> Unit) {
-    Row(
-        modifier = Modifier
-            .clickable {
-                onClick()
-            }
-            .padding(vertical = 16.dp, horizontal = 16.dp)
-            .fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(Theme.colors.iconBackgroundLow)
-                .border(
-                    1.dp,
-                    Theme.colors.stroke,
-                    RoundedCornerShape(12.dp)
-                ),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                modifier = Modifier.size(24.dp),
-                painter = painter,
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(Theme.colors.primary)
-            )
-        }
-
-        Text(
-            modifier = Modifier,
-            text = title,
-            color = Theme.colors.title,
-            style = Theme.textStyle.title.medium
-        )
-    }
-}
-
-@Composable
-private fun UserInfo(userName: String) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(vertical = 12.dp, horizontal = 16.dp),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        Box(
-            modifier = Modifier
-                .size(48.dp)
-                .clip(RoundedCornerShape(12.dp))
-                .background(Theme.colors.iconBackgroundLow)
-                .border(
-                    1.dp,
-                    Theme.colors.stroke,
-                    RoundedCornerShape(12.dp)
-                ),
-            contentAlignment = Alignment.Center
-        ) {
-            Image(
-                modifier = Modifier.size(28.dp),
-                painter = painterResource(id = R.drawable.user_profile),
-                contentDescription = null,
-                colorFilter = ColorFilter.tint(Theme.colors.hint)
-            )
-        }
-
-        Text(
-            modifier = Modifier.weight(1f),
-            text = userName,
-            color = Theme.colors.title,
-            style = Theme.textStyle.title.medium
-        )
-
-        Image(
-            modifier = Modifier
-                .height(24.dp)
-                .clip(RoundedCornerShape(4.dp))
-                .clickable {
-
-                },
-            painter = painterResource(id = R.drawable.more_vertical),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(Theme.colors.body)
-        )
-    }
-}
-
-@Preview
+@PreviewLightDark
 @Composable
 private fun AccountScreenContentPreview() {
     val interactionsListener = object : MyAccountScreenInteractionsListener {
