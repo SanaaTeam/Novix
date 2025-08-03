1a49a60a74ac35e1e879f3fead1e9f019fa3f1ec
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-03 19:04:52 +0300

    add deleteTvSeriesRate function to RemoteTvShowDataSource

--- data/repositories/vod/src/main/java/com/sanaa/vod/dataSource/remote/tvShow/RemoteTvShowDataSource.kt
+++ data/repositories/vod/src/main/java/com/sanaa/vod/dataSource/remote/tvShow/RemoteTvShowDataSource.kt
@@ -45,5 +45,5 @@ interface RemoteTvShowDataSource {
         sessionId: String,
         rating: Float
     ): RatingResponse
-
+    suspend fun deleteTvSeriesRate(seriesId: Int,sessionId: String): RatingResponse
 }
