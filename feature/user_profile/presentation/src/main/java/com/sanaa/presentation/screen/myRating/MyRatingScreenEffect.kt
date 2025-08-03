f3aaa8842a8f813364f790f9bc29c509fa572d30
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-04 00:42:06 +0300

    rename snackbar effects in MyRatingScreenEffect

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myRating/MyRatingScreenEffect.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myRating/MyRatingScreenEffect.kt
@@ -2,7 +2,9 @@ package com.sanaa.presentation.screen.myRating
 
 sealed interface MyRatingScreenEffect {
     object NavigateBack : MyRatingScreenEffect
-    data class ShowMessage(val message: String) : MyRatingScreenEffect
-    data class NavigateToMediaDetails(val mediaId: Int, val mediaType: String) : MyRatingScreenEffect
+    data object  ShowErrorSnackBar : MyRatingScreenEffect
+    data object ShowSuccessSnackBar : MyRatingScreenEffect
+
+//    data class NavigateToMediaDetails(val mediaId: Int, val mediaTypeUi: MediaTypeUi): MyRatingScreenEffect
 
 }
