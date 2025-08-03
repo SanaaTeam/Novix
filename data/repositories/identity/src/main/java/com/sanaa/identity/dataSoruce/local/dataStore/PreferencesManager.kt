45f17f112a30e16906c4cea57d8ec2b03a66260e
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-03 19:10:35 +0300

    add account id to PreferencesManager interface

--- data/repositories/identity/src/main/java/com/sanaa/identity/dataSoruce/local/dataStore/PreferencesManager.kt
+++ data/repositories/identity/src/main/java/com/sanaa/identity/dataSoruce/local/dataStore/PreferencesManager.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.flow.Flow
 interface PreferencesManager {
     val sessionId: Flow<String>
     val isGuest: Flow<Boolean>
+    val accountId: Flow<Long>
     suspend fun updateSessionId(value: String)
     suspend fun setIsGuest(value: Boolean)
+    suspend fun setAccountId(value: Long)
 }
