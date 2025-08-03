aa84996991a761719ef9c05d49ed5e28fc78cd68
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-03 21:49:16 +0300

    add `NavigateToMediaDetails` to `MyRatingScreenEffect`

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myRating/MyRatingScreenEffect.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myRating/MyRatingScreenEffect.kt
@@ -3,4 +3,6 @@ package com.sanaa.presentation.screen.myRating
 sealed interface MyRatingScreenEffect {
     object NavigateBack : MyRatingScreenEffect
     data class ShowMessage(val message: String) : MyRatingScreenEffect
+    data class NavigateToMediaDetails(val mediaId: Int, val mediaType: String) : MyRatingScreenEffect
+
 }
