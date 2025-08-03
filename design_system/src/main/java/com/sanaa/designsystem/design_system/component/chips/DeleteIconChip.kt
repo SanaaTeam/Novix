730efc24729e26469060c4a26e3cb68cc659706a
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-03 19:02:49 +0300

    add DeleteIconChip component to design system

---
+++ design_system/src/main/java/com/sanaa/designsystem/design_system/component/chips/DeleteIconChip.kt
@@ -0,0 +1,36 @@
+package com.sanaa.designsystem.design_system.component.chips
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.R
+import com.sanaa.designsystem.design_system.theme.Theme
+
+@Composable
+fun DeleteIconChip(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .background(Theme.colors.iconBackground, shape = RoundedCornerShape(8.dp))
+            .border(1.dp, color = Theme.colors.stroke)
+            .clickable(onClick = onClick)
+            .padding(4.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.icon_delete),
+            contentDescription = "Delete rating"
+        )
+    }
+}
