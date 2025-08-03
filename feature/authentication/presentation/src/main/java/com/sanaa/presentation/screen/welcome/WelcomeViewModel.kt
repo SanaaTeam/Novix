34ee98c0d8e9521c102e8785fe7b1fc53b983b68
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-08-04 00:39:45 +0300

    request guest session id and saving it

--- feature/authentication/presentation/src/main/java/com/sanaa/presentation/screen/welcome/WelcomeViewModel.kt
+++ feature/authentication/presentation/src/main/java/com/sanaa/presentation/screen/welcome/WelcomeViewModel.kt
@@ -1,16 +1,33 @@
 package com.sanaa.presentation.screen.welcome
 
+import android.util.Log
 import com.sanaa.presentation.screen.login_base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import usecase.CreateGuestSessionUseCase
 import javax.inject.Inject
 
 @HiltViewModel
-class WelcomeViewModel @Inject constructor() : BaseViewModel<Unit, WelcomeScreenEffects>(Unit),
+class WelcomeViewModel @Inject constructor(
+    private val createGuestSessionUseCase: CreateGuestSessionUseCase
+) : BaseViewModel<Unit, WelcomeScreenEffects>(Unit),
     WelcomeScreenInteractionListener {
 
     override fun onLoginClicked() = emitEffect(WelcomeScreenEffects.NavigateToLogin)
 
-    override fun onContinueClicked() = emitEffect(WelcomeScreenEffects.ReturnGuestResultCode)
+    override fun onContinueClicked()  {
+        tryToExecute(
+            callee = {
+                createGuestSessionUseCase.createGuestSession()
+            },
+            onSuccess = {
+                Log.d("test99", "onContinueClicked:success ")
+                emitEffect(WelcomeScreenEffects.ReturnGuestResultCode)
+            },
+            onError = {
+                Log.d("test99", "onContinueClicked: Error= :${it.message} ")
+            }
+        )
+    }
 
     override fun onExit() = emitEffect(WelcomeScreenEffects.ExitApp)
 }
