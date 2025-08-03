7b4ed7f0577ac23ad9e4017414ac6dc2507e0abb
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-04 02:23:08 +0300

    implement empty saved list screen and make it more generic
    The `EmptySavedListScreenContent` composable has been refactored to be more generic and reusable.
    
    It now accepts parameters for:
    - Image resource
    - Screen title
    - Message text
    - Visibility of FAB button
    - Visibility of Login button
    - Click handlers for FAB and Login buttons
    
    This allows the same composable to be used for different empty state scenarios, such as when a logged-in user has no saved lists or when a guest user needs to log in to create lists.
    
    Updated previews demonstrate the new functionality for both user and guest scenarios, including dark mode support.

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/EmptySavedListScreen.kt
@@ -0,0 +1,132 @@
+package com.sanaa.presentation.screen.saved
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.design_system.component.button.FabButton
+import com.sanaa.designsystem.design_system.component.button.OutlinedButton
+import com.sanaa.designsystem.design_system.component.novix_scaffold.NovixScaffold
+import com.sanaa.designsystem.design_system.component.text.AppText
+import com.sanaa.designsystem.design_system.component.top_bar.TopBar
+import com.sanaa.designsystem.design_system.theme.NovixTheme
+import com.sanaa.designsystem.design_system.theme.Theme
+import com.sanaa.feature.playlists.presentation.R
+
+@Composable
+fun EmptySavedListScreenContent(
+    @DrawableRes imageRes: Int,
+    screenTitle: String,
+    messageText: String,
+    showFab: Boolean = false,
+    showLoginButton: Boolean = false,
+    onFabClick: () -> Unit = {},
+    onLoginClick: () -> Unit = {}
+) {
+    NovixScaffold(
+        modifier = Modifier.padding(top = 12.dp),
+        topBar = { TopBar(screenTitle = screenTitle) },
+        floatingActionButton = {
+            if (showFab) {
+                FabButton(
+                    icon = painterResource(id = com.sanaa.designsystem.R.drawable.icon_plus),
+                    onClick = onFabClick,
+                    isLoading = false,
+                    isEnabled = true
+                )
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(imageRes),
+                contentDescription = null
+            )
+
+            AppText(
+                modifier = Modifier
+                    .padding(horizontal = 68.dp)
+                    .offset(y = (-30).dp),
+                text = messageText,
+                style = Theme.textStyle.body.small,
+                color = Theme.colors.body,
+                textAlign = TextAlign.Center
+            )
+
+            if (showLoginButton) {
+                OutlinedButton(
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .size(width = 104.dp, height = 48.dp),
+                    text = "Login",
+                    onClick = onLoginClick
+                )
+            }
+        }
+    }
+}
+
+
+@PreviewLightDark
+@Composable
+private fun EmptySavedListUserScreenContentPrev() {
+    val isDarkTheme = isSystemInDarkTheme()
+    NovixTheme(
+        isDarkMode = isDarkTheme
+    ) {
+        val myListImg = if (isDarkTheme) {
+            R.drawable.my_list_dark
+        } else {
+            R.drawable.my_list_light
+        }
+        EmptySavedListScreenContent(
+            screenTitle = stringResource(R.string.saved_list),
+            messageText = stringResource(R.string.there_is_no_saved_list_yet_click_on_button_to_add_a_new_list),
+            imageRes = myListImg,
+            showFab = true,
+            onFabClick = {}
+        )
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun EmptySavedListGuestScreenContentPrev() {
+    val isDarkTheme = isSystemInDarkTheme()
+    NovixTheme(
+        isDarkMode = isDarkTheme
+    ) {
+        val myListImg = if (isDarkTheme) {
+            R.drawable.no_login_my_list_dark
+        } else {
+            R.drawable.no_login_my_list_light
+        }
+        EmptySavedListScreenContent(
+            screenTitle = stringResource(R.string.my_lists),
+            messageText = stringResource(R.string.please_login_to_create_lists_and_add_your_favorite_items_to_it),
+            imageRes = myListImg,
+            showLoginButton = true,
+            onLoginClick = {}
+        )
+    }
+
+}
