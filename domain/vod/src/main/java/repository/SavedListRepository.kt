315eb888b224f16216b10e6b0c244cc838213ec0
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-04 18:04:51 +0300

    remove saved item and use movie entity

--- domain/vod/src/main/java/repository/SavedListRepository.kt
+++ domain/vod/src/main/java/repository/SavedListRepository.kt
@@ -1,6 +1,6 @@
 package repository
 
-import usecase.custom_list.custom_list_param.SavedItem
+import entity.Movie
 import usecase.custom_list.custom_list_param.SavedList
 
 interface SavedListRepository {
@@ -8,7 +8,7 @@ interface SavedListRepository {
     suspend fun createSavedList(list: SavedList): SavedList
     suspend fun deleteSavedList(listId: Int)
 
-    suspend fun getAllItemsInList(listId: Int): List<SavedItem>
+    suspend fun getAllItemsInList(listId: Int): List<Movie>
     suspend fun addMovieToList(listId: Int, movieId: Int): Boolean
     suspend fun removeMovieFromList(listId: Int, movieId: Int): Boolean
 }
--- domain/vod/src/main/java/usecase/custom_list/ManageSavedListItemsUseCase.kt
+++ domain/vod/src/main/java/usecase/custom_list/ManageSavedListItemsUseCase.kt
@@ -1,13 +1,13 @@
 package usecase.custom_list
 
+import entity.Movie
 import repository.SavedListRepository
-import usecase.custom_list.custom_list_param.SavedItem
 import javax.inject.Inject
 
 class ManageSavedListItemsUseCase @Inject constructor(
     private val savedListRepository: SavedListRepository
 ) {
-    suspend fun getAllItemsInSavedList(listId: Int): List<SavedItem> =
+    suspend fun getAllItemsInSavedList(listId: Int): List<Movie> =
         savedListRepository.getAllItemsInList(listId)
 
     suspend fun addMovieToSavedList(listId: Int, movieId: Int): Boolean =
