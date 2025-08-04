916a5ac5730af8ab9061dafd86b3cf533a6c8aab
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-04 08:53:45 +0300

    add `MyListItem` composable function
    This commit introduces a new composable function, `MyListItem`, designed to display an item in a list. It includes:
    - A title for the item.
    - A clickable section with a count and an arrow icon, triggering an `onItemClick` callback.
    - Styling with background color, border, and rounded corners, adapting to the current theme.
    - A preview function, `MyListItemPreview`, for visualizing the component in both light and dark themes.

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/componants/MyListItem.kt
@@ -0,0 +1,103 @@
+package com.sanaa.presentation.screen.saved.componants
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.design_system.component.text.AppText
+import com.sanaa.designsystem.design_system.theme.NovixTheme
+import com.sanaa.designsystem.design_system.theme.Theme
+import com.sanaa.feature.playlists.presentation.R
+
+@Composable
+fun MyListItem(
+    modifier: Modifier = Modifier,
+    onItemClick: () -> Unit = {},
+    title: String = stringResource(R.string.my_list)
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(
+                color = Theme.colors.surface,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = Theme.colors.stroke,
+                shape = RoundedCornerShape(12.dp)
+            )
+            .padding(vertical = 16.dp, horizontal = 12.dp)
+    ) {
+
+        AppText(
+            modifier = Modifier,
+            text = title,
+            style = Theme.textStyle.title.medium,
+            color = Theme.colors.title,
+            textAlign = TextAlign.Center
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(8.dp))
+                .clickable(
+                    onClick = { onItemClick() }
+                )
+                .background(
+                    color = Theme.colors.surface,
+
+                    )
+                .border(
+                    width = 1.dp,
+                    color = Theme.colors.stroke,
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(vertical = 4.dp, horizontal = 8.dp),
+            horizontalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            AppText(
+                modifier = Modifier,
+                text = "12",
+                style = Theme.textStyle.label.small,
+                color = Theme.colors.primary,
+                textAlign = TextAlign.Center
+            )
+            Icon(
+                painter = painterResource(R.drawable.icon_arrow_right),
+                contentDescription = stringResource(
+                    R.string.arrow_right
+                ),
+                tint = Theme.colors.primary
+            )
+
+        }
+
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun MyListItemPreview() {
+
+    NovixTheme(
+        isSystemInDarkTheme()
+    ) {
+        MyListItem()
+    }
+}
