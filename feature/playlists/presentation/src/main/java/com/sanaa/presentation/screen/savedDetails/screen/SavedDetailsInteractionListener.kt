a14e338e0615e2c4fdfb42a973c5bf0b0ef438c2
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-04 21:35:58 +0300

    add SavedDetailsInteractionListener interface

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/screen/SavedDetailsInteractionListener.kt
@@ -0,0 +1,10 @@
+package com.sanaa.presentation.screen.savedDetails.screen
+
+import com.sanaa.presentation.screen.savedDetails.state.MediaItem
+import com.sanaa.presentation.screen.savedDetails.state.MediaTypeUi
+
+interface SavedDetailsInteractionListener {
+    fun onMediaClick(mediaId: Int, mediaType: MediaTypeUi)
+    fun onSaveIconClick(mediaItem: MediaItem)
+    fun onMediaTypeClick(mediaType: MediaTypeUi)
+}
