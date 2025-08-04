79fe59be1e74bccd31d2400275632c8686a4946e
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-04 06:20:12 +0300

    enable launching PlaylistScreen from other modules

--- feature/playlists/api/src/main/java/com/sanaa/api/PlaylistsFeatureApi.kt
+++ feature/playlists/api/src/main/java/com/sanaa/api/PlaylistsFeatureApi.kt
@@ -1,8 +1,10 @@
 package com.sanaa.api
 
+import android.content.Context
 import androidx.compose.runtime.Composable
 
 interface PlaylistsFeatureApi {
     @Composable
     fun PlaylistsScreenApi()
+    fun launchPlaylistScreen(context: Context)
 }
--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/api/PlaylistsFeatureApiImpl.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/api/PlaylistsFeatureApiImpl.kt
@@ -1,8 +1,17 @@
 package com.sanaa.presentation.api
 
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
 import com.sanaa.api.PlaylistsFeatureApi
-import com.sanaa.presentation.screen.saved.EmptySavedListScreenContent
+import com.sanaa.designsystem.design_system.theme.NovixTheme
+import com.sanaa.presentation.screen.saved.PlaylistScreen
+import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,7 +19,28 @@ import javax.inject.Singleton
 class PlaylistsFeatureApiImpl @Inject constructor() : PlaylistsFeatureApi {
     @Composable
     override fun PlaylistsScreenApi() {
-        EmptySavedListScreenContent()
+        NovixTheme(isSystemInDarkTheme()) {
+            PlaylistScreen()
+        }
+    }
+
+    override fun launchPlaylistScreen(context: Context) {
+        val intent = Intent(context, PlaylistActivity::class.java)
+        context.startActivity(intent)
+    }
+}
+
+@AndroidEntryPoint
+class PlaylistActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NovixTheme(isSystemInDarkTheme()) {
+                PlaylistScreen()
+            }
+        }
     }
 }
 
