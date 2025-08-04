bfff8f90de0e5d96488cd33f8efbdda3849f8762
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-05 02:09:53 +0300

    add PlaylistsApiEntryPoint

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/api/navigation/PlaylistsApiEntryPoint.kt
@@ -0,0 +1,15 @@
+package com.sanaa.presentation.api.navigation
+
+import com.sanaa.api.AuthenticationApi
+import com.sanaa.api.MediaDetailsApi
+import dagger.hilt.EntryPoint
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@EntryPoint
+@InstallIn(SingletonComponent::class)
+interface PlaylistsApiEntryPoint {
+    fun detailsApi(): MediaDetailsApi
+    fun authenticationApi(): AuthenticationApi
+
+}
