3411e2fc72d1c700ad20311af2cf9dfef21373e4
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-04 05:34:59 +0300

    add playListScreenViewModel and playListScreenInteractionListener

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/PlayListScreenInteractionListener.kt
@@ -0,0 +1,9 @@
+package com.sanaa.presentation.screen.saved
+
+interface PlayListScreenInteractionListener {
+    fun onFabBottomSheetClicked()
+    fun onButtonLoginClicked()
+    fun onDismissAddBottomSheet()
+    fun onAddNewListClicked()
+
+}
---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/PlayListScreenViewModel.kt
@@ -0,0 +1,24 @@
+package com.sanaa.presentation.screen.saved
+
+import com.sanaa.presentation.savedBase.BaseViewModel
+
+class PlayListScreenViewModel() :
+    BaseViewModel<PlayListScreenUiState, PlayListScreenEffect>(PlayListScreenUiState()),
+    PlayListScreenInteractionListener
+{
+    override fun onFabBottomSheetClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onButtonLoginClicked() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDismissAddBottomSheet() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onAddNewListClicked() {
+        TODO("Not yet implemented")
+    }
+}
