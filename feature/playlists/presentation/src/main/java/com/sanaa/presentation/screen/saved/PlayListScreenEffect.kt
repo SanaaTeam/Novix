09e959f53ba9d5298805aabebd76d867f62b21eb
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-04 07:55:31 +0300

    add screen effects to PlaylistScreenEffect
    This commit adds three new sealed class objects to `PlayListScreenEffect`:
    - `NavigateToLogin`
    - `ShowErrorSnackBar`
    - `ShowSuccessSnackBar`

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/PlayListScreenEffect.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/PlayListScreenEffect.kt
@@ -1,5 +1,7 @@
 package com.sanaa.presentation.screen.saved
 
 sealed class PlayListScreenEffect{
-
+    object NavigateToLogin : PlayListScreenEffect()
+    data object  ShowErrorSnackBar : PlayListScreenEffect()
+    data object ShowSuccessSnackBar : PlayListScreenEffect()
 }
