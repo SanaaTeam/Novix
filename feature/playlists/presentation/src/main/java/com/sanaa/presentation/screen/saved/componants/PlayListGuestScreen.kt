50c022c38ac9d056ce0abee890198039bdfd4d1f
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-04 05:37:10 +0300

    implement guest screen for playlist

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/saved/componants/PlayListGuestScreen.kt
@@ -0,0 +1,35 @@
+package com.sanaa.presentation.screen.saved.componants
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.sanaa.designsystem.design_system.theme.NovixTheme
+import com.sanaa.feature.playlists.presentation.R
+
+@Composable
+fun PlayListGuestScreen() {
+    val isDarkTheme = isSystemInDarkTheme()
+    NovixTheme(
+        isDarkMode = isDarkTheme
+    ) {
+        val myListImg = if (isDarkTheme) {
+            R.drawable.no_login_my_list_dark
+        } else {
+            R.drawable.no_login_my_list_light
+        }
+        PlayListEmptyStateScreen(
+            screenTitle = stringResource(R.string.my_lists),
+            messageText = stringResource(R.string.please_login_to_create_lists_and_add_your_favorite_items_to_it),
+            imageRes = myListImg,
+            showLoginButton = true,
+            onLoginClick = {}
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PlayListGuestScreenPrev() {
+    PlayListGuestScreen()
+}
