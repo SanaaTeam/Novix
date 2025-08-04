ec31543c06684831d89221392b2285d3b18010c0
haider <haideraljawasim@gmail.com>
2025-08-04 19:25:31 +0300

    add view model for main actvity

--- app/src/main/java/com/sanaa/novix/MainActivity.kt
+++ app/src/main/java/com/sanaa/novix/MainActivity.kt
@@ -6,22 +6,22 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.os.LocaleListCompat
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import com.sanaa.api.AuthStartRoute
 import com.sanaa.api.AuthenticationApi
 import com.sanaa.api.HomeFeatureApi
+import com.sanaa.designsystem.design_system.theme.NovixTheme
 import com.sanaa.identity.dataSoruce.local.dataStore.PreferencesManager
+import com.sanaa.novix.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import java.util.Locale
 import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -35,56 +35,54 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var preferenceManager: PreferencesManager
 
-    @Inject
-    lateinit var userPreference: repository.UserPreference
+    private val viewModel: MainViewModel by viewModels()
 
     private lateinit var authLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-
-        applyAppLanguage()
-
-        authLauncher = registerForActivityResult(
-            ActivityResultContracts.StartActivityForResult()
-        ) { result ->
-            when (result.resultCode) {
-                AuthenticationApi.RESULT_LOGGED_WITH_SESSION_ID,
-                AuthenticationApi.RESULT_LOGGED_AS_GUEST -> {
-                    setContent { homeFeatureApi.HomeScreenApi() }
-                }
-
-                else -> finish()
-            }
-        }
-
         enableEdgeToEdge()
 
+        splashScreen.setKeepOnScreenCondition {
+            !viewModel.state.value.isReady
+        }
 
-        val sessionId = runBlocking { preferenceManager.sessionId.firstOrNull() }
-        if (sessionId.isNullOrEmpty()) {
-            val authIntent = authenticationApi.getLaunchIntent(this, AuthStartRoute.Welcome)
-            authLauncher.launch(authIntent)
-        } else {
-            setContent { homeFeatureApi.HomeScreenApi() }
+        authLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                when (result.resultCode) {
+                    AuthenticationApi.RESULT_LOGGED_WITH_SESSION_ID,
+                    AuthenticationApi.RESULT_LOGGED_AS_GUEST -> {
+                        setMainContent()
+                    }
+
+                    else -> finish()
+                }
+            }
+
+        lifecycleScope.launch {
+            viewModel.state.collect { state ->
+                if (state.isReady) {
+                    val sessionId = preferenceManager.sessionId.firstOrNull()
+                    if (sessionId.isNullOrEmpty()) {
+                        val authIntent = authenticationApi.getLaunchIntent(
+                            this@MainActivity,
+                            AuthStartRoute.Welcome
+                        )
+                        authLauncher.launch(authIntent)
+                    } else {
+                        setMainContent()
+                    }
+                }
+            }
         }
     }
 
-
-    private fun applyAppLanguage() {
-        lifecycleScope.launch {
-            userPreference.getLanguage().collect { language ->
-                val langCode = language.code
-                Locale.setDefault(Locale(langCode))
-                val localeList = LocaleListCompat.forLanguageTags(langCode)
-                val currentLocales = AppCompatDelegate.getApplicationLocales()
-                if (currentLocales != localeList) {
-                    AppCompatDelegate.setApplicationLocales(localeList)
-                    if (lifecycle.currentState.isAtLeast(Lifecycle.State.RESUMED)) {
-                        recreate()
-                    }
-                }
+    private fun setMainContent() {
+        setContent {
+            val state = viewModel.state.collectAsStateWithLifecycle()
+            NovixTheme(isDarkMode = state.value.isDarkTheme) {
+                homeFeatureApi.HomeScreenApi()
             }
         }
     }
--- app/src/main/java/com/sanaa/novix/NovixApp.kt
+++ app/src/main/java/com/sanaa/novix/NovixApp.kt
@@ -1,8 +1,16 @@
 package com.sanaa.novix
 
 import android.app.Application
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.compose.ui.platform.LocalView
+import androidx.core.os.LocaleListCompat
 import com.google.firebase.crashlytics.FirebaseCrashlytics
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import repository.UserPreference
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -15,9 +23,22 @@ class NovixApp : Application() {
     lateinit var timberTree: Timber.Tree
 
     @Inject
+    lateinit var userPreference: UserPreference
 
     override fun onCreate() {
         super.onCreate()
+        val theme = runBlocking { userPreference.getTheme().first() }
+
+        val language = runBlocking { userPreference.getLanguage().first() }
+
+        AppCompatDelegate.setDefaultNightMode(
+            if (theme == repository.Theme.DARK) AppCompatDelegate.MODE_NIGHT_YES
+            else AppCompatDelegate.MODE_NIGHT_NO
+        )
+
+        val localeList = LocaleListCompat.forLanguageTags(language.code)
+
+        AppCompatDelegate.setApplicationLocales(localeList)
         crashlytics.apply {
             isCrashlyticsCollectionEnabled = true
             setUserId("user_${System.currentTimeMillis()}")
