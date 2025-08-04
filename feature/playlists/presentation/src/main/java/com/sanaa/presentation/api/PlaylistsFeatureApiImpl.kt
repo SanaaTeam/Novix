92e8832b021292c4b70a19129ed246e8b0d987e6
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-05 02:16:18 +0300

    Refactor: Add PlaylistsFeatureApiImpl class

---
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/api/PlaylistsFeatureApiImpl.kt
@@ -0,0 +1,15 @@
+package com.sanaa.presentation.api
+
+import androidx.compose.runtime.Composable
+import com.sanaa.api.PlaylistsFeatureApi
+import com.sanaa.presentation.api.navigation.SavedNavHost
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class PlaylistsFeatureApiImpl @Inject constructor() : PlaylistsFeatureApi {
+    @Composable
+    override fun PlaylistsScreenApi() {
+        SavedNavHost()
+    }
+}
