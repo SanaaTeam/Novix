9dd7ccb7b47b64a9eb10eee665e0eaa5d0e23c88
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-05 17:40:24 +0300

    Refactor: Simplify SavedDetailsScreenUiState

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/state/SavedDetailsScreenUiState.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/state/SavedDetailsScreenUiState.kt
@@ -5,10 +5,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
 
 data class SavedDetailsScreenUiState(
-    val selectedMediaTypeUi: MediaTypeUi = MediaTypeUi.ALL,
     val movieList: Flow<PagingData<MediaItem>> = flowOf(PagingData.empty()),
-    val tvShowList: Flow<PagingData<MediaItem>> = flowOf(PagingData.empty()),
-    val allList: Flow<PagingData<MediaItem>> = flowOf(PagingData.empty()),
     val isLoading: Boolean = false,
     val error: String? = null,
     val showBottomSheet: Boolean = false
