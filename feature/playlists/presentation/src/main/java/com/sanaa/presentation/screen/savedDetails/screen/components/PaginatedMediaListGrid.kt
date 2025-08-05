3a2a975768ec2e00a58be8aabd8f885d39b5f622
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-05 16:58:39 +0300

    Update sensitive image component in PaginatedMediaListGrid

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/screen/components/PaginatedMediaListGrid.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/screen/components/PaginatedMediaListGrid.kt
@@ -17,7 +17,8 @@ import androidx.paging.compose.LazyPagingItems
 import com.sanaa.designsystem.R
 import com.sanaa.designsystem.design_system.component.blur.OnBlurContent
 import com.sanaa.designsystem.design_system.theme.Theme
-import com.sanaa.image_viewer.component.RemoteBlurredHaramImageViewer
+import com.sanaa.image_viewer.component.RemoteBlurredSensitiveImage
+import com.sanaa.presentation.providers.LocalSafeContentThreshold
 import com.sanaa.presentation.screen.savedDetails.screen.components.chips.SaveIconChip
 import com.sanaa.presentation.screen.savedDetails.state.MediaItem
 
@@ -41,11 +42,12 @@ fun PaginatedMediaListGrid(
             val media = mediaList[index] ?: return@items
             MediaPosterCard(
                 posterImage = {
-                    RemoteBlurredHaramImageViewer(
+                    RemoteBlurredSensitiveImage(
                         imageUrl = media.imageUrl.orEmpty(),
                         modifier = Modifier.fillMaxWidth(),
-                        haramThreshold = 0.2f,
-                        nonHaramThreshold = 0.7f,
+                        sensitiveContentThreshold = 0.2f,
+                        isBlurEnabled = LocalSafeContentThreshold.current != 0f,
+                        safeContentThreshold = LocalSafeContentThreshold.current,
                         contentDescription = media.title,
                         placeholderContent = {
                             RemoteImagePlaceholder(Modifier.fillMaxSize())
