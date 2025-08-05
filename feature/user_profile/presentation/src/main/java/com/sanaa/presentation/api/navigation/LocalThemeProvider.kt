230363e0188e24a40c2946516408caaf2f8e2c19
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-05 17:18:26 +0300

    remove LocalThemeProvider

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/api/navigation/LocalThemeProvider.kt
+++
@@ -1,7 +0,0 @@
-package com.sanaa.presentation.api.navigation
-
-import androidx.compose.runtime.staticCompositionLocalOf
-
-val LocalThemeProvider = staticCompositionLocalOf<Boolean> {
-    error("No theme provided")
-}
