8a2751e72e28a0950be5466d73504b681f5e9ad5
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-05 10:24:25 +0300

    refactor saved list api service for clarity

--- data/remote_data_source/vod/src/main/java/com/sanaa/vod/custom_list/SavedListApiService.kt
+++ data/remote_data_source/vod/src/main/java/com/sanaa/vod/custom_list/SavedListApiService.kt
@@ -1,11 +1,10 @@
 package com.sanaa.vod.custom_list
 
-import com.sanaa.vod.custom_list.request.AddItemBodyDto
+import com.sanaa.vod.custom_list.request.AddOrRemoveItemBodyDto
 import com.sanaa.vod.custom_list.request.CreateListBodyDto
-import com.sanaa.vod.custom_list.request.RemoveItemBodyDto
-import com.sanaa.vod.custom_list.request.TmdbStatusDto
 import com.sanaa.vod.custom_list.response.CreateListResponseDto
 import com.sanaa.vod.custom_list.response.ListApiResponse
+import com.sanaa.vod.custom_list.response.TmdbStatusResponseDto
 import com.sanaa.vod.dataSource.remote.dto.cutsom_list.SavedListDetailsDto
 import com.sanaa.vod.dataSource.remote.dto.cutsom_list.SavedListDto
 import retrofit2.http.Body
@@ -18,7 +17,6 @@ import retrofit2.http.Query
 
 interface SavedListApiService {
 
-    /* ---------- account lists ---------- */
     @GET("account/{account_id}/lists")
     suspend fun getUserLists(
         @Path("account_id") accountId: Long,
@@ -26,7 +24,6 @@ interface SavedListApiService {
         @Query("page") page: Int? = null
     ): ListApiResponse<SavedListDto>
 
-    /* ---------- list CRUD ---------- */
     @Headers("Ignore-Language: true")
     @POST("list")
     suspend fun createList(
@@ -38,29 +35,27 @@ interface SavedListApiService {
     suspend fun deleteList(
         @Path("list_id") listId: Int,
         @Query("session_id") sessionId: String
-    ): TmdbStatusDto
+    ): TmdbStatusResponseDto
 
-    /* ---------- list details ---------- */
     @GET("list/{list_id}")
     suspend fun getListDetails(
         @Path("list_id") listId: Int,
         @Query("page") page: Int? = null
     ): SavedListDetailsDto
 
-    /* ---------- items ---------- */
     @Headers("Ignore-Language: true")
     @POST("list/{list_id}/add_item")
     suspend fun addItem(
         @Path("list_id") listId: Int,
         @Query("session_id") sessionId: String,
-        @Body body: AddItemBodyDto
-    ): TmdbStatusDto
+        @Body body: AddOrRemoveItemBodyDto
+    ): TmdbStatusResponseDto
 
     @POST("list/{list_id}/remove_item")
     @Headers("Ignore-Language: true")
     suspend fun removeItem(
         @Path("list_id") listId: Int,
         @Query("session_id") sessionId: String,
-        @Body body: RemoveItemBodyDto
-    ): TmdbStatusDto
+        @Body body: AddOrRemoveItemBodyDto
+    ): TmdbStatusResponseDto
 }
