f2d6ceab679f32f98967ffe2a57243be90ed06bf
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-05 20:32:03 +0300

    add SaveIconChip and TvMediaPosterCard composable

---
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/searchScreen/componants/TvMediaPosterCard.kt
@@ -0,0 +1,98 @@
+package com.sanaa.tvapp.presentation.screens.searchScreen.componants
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.focusable
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.Text
+import com.sanaa.designsystem.R
+import com.sanaa.designsystem.design_system.theme.NovixTheme
+import com.sanaa.designsystem.design_system.theme.Theme
+
+@Composable
+fun TvMediaPosterCard(
+    modifier: Modifier = Modifier,
+    title: String = "",
+    onCardClick: () -> Unit = {},
+    topLeftContent: @Composable () -> Unit = {},
+    PosterImage: @Composable () -> Unit = {}
+) {
+    var isFocused by remember { mutableStateOf(false) }
+    Box(
+        modifier = modifier
+            .aspectRatio(
+                ratio = (158 / 210f)
+            )
+            .size(width = 231.dp, height = 153.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(Theme.colors.surface)
+            .focusable()
+            .onFocusChanged { isFocused = it.isFocused }
+            .clickable(onClick = onCardClick)
+    ) {
+        PosterImage()
+        Box(
+            modifier = Modifier
+                .border(1.dp, Theme.colors.stroke, RoundedCornerShape(12.dp))
+                .padding(8.dp)
+                .matchParentSize()
+                .align(Alignment.TopStart),
+        ) {
+            topLeftContent()
+        }
+        if (isFocused) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .background(Theme.colors.surface.copy(alpha = 0.8f))
+                    .padding(4.dp)
+            ) {
+                Text(
+                    text = title,
+                    color = Theme.colors.primary,
+                    style = Theme.textStyle.body.small
+                )
+            }
+        }
+    }
+}
+
+@PreviewLightDark
+@Composable
+private fun PreviewMediaPosterCard() {
+    NovixTheme(isSystemInDarkTheme()) {
+
+        TvMediaPosterCard(
+            title = "ghsjhvj",
+            PosterImage = {
+                Image(
+                    painter = painterResource(R.drawable.icon_placeholder_light),
+                    contentDescription = null,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
+        )
+    }
+}
