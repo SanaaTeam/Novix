1d875718679f47b8c39f1c23865de73aa868b5b9
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-05 10:23:48 +0300

    consolidate DTOs for adding and removing items from lists

--- data/remote_data_source/vod/src/main/java/com/sanaa/vod/custom_list/RemoteSavedListDataSourceImpl.kt
+++ data/remote_data_source/vod/src/main/java/com/sanaa/vod/custom_list/RemoteSavedListDataSourceImpl.kt
@@ -1,40 +1,39 @@
 package com.sanaa.vod.custom_list
 
-import com.sanaa.vod.custom_list.request.AddItemBodyDto
+import com.sanaa.vod.custom_list.request.AddOrRemoveItemBodyDto
 import com.sanaa.vod.custom_list.request.CreateListBodyDto
-import com.sanaa.vod.custom_list.request.RemoveItemBodyDto
 import com.sanaa.vod.dataSource.remote.custom_list.RemoteSavedListDataSource
 import com.sanaa.vod.dataSource.remote.dto.cutsom_list.SavedListDto
 
 import javax.inject.Inject
 
 class RemoteSavedListDataSourceImpl @Inject constructor(
-    private val api: SavedListApiService
+    private val savedListApiService: SavedListApiService
 ) : RemoteSavedListDataSource {
 
-    override suspend fun fetchUserLists(sessionId: String, accountId: Long) =
-        api.getUserLists(accountId, sessionId).results
+    override suspend fun fetchUserLists(sessionId: String, accountId: Long,  page: Int?) =
+        savedListApiService.getUserLists(accountId, sessionId, page).results
 
     override suspend fun createList(
         sessionId: String,
         name: String,
         description: String
     ): SavedListDto {
-        val res = api.createList(sessionId, CreateListBodyDto(name, description, "en"))
-        // Pull full object so we have counts & description
-        return api.getListDetails(res.listId).toListDto()
+        val response = savedListApiService
+            .createList(sessionId, CreateListBodyDto(name, description, "en"))
+        return savedListApiService.getListDetails(response.listId).toListDto()
     }
 
     override suspend fun deleteList(sessionId: String, listId: Int) {
-        api.deleteList(listId, sessionId)
+        savedListApiService.deleteList(listId, sessionId)
     }
 
     override suspend fun fetchListItems(listId: Int, page: Int?) =
-        api.getListDetails(listId, page).items
+        savedListApiService.getListDetails(listId, page).items
 
     override suspend fun addItem(sessionId: String, listId: Int, movieId: Int) =
-        api.addItem(listId, sessionId, AddItemBodyDto(mediaId = movieId)).success
+        savedListApiService.addItem(listId, sessionId, AddOrRemoveItemBodyDto(movieId)).success
 
     override suspend fun removeItem(sessionId: String, listId: Int, movieId: Int) =
-        api.removeItem(listId, sessionId, RemoveItemBodyDto(movieId)).success
+        savedListApiService.removeItem(listId, sessionId, AddOrRemoveItemBodyDto(movieId)).success
 }
