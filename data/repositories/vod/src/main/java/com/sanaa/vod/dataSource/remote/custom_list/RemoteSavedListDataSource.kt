5fe9210dd21ce8bb17e622063a640986f84cd7ae
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-05 10:26:11 +0300

    make the SavedListRepository use RemoteMovieDataSource for movie details

--- data/repositories/vod/src/main/java/com/sanaa/vod/dataSource/remote/custom_list/RemoteSavedListDataSource.kt
+++ data/repositories/vod/src/main/java/com/sanaa/vod/dataSource/remote/custom_list/RemoteSavedListDataSource.kt
@@ -4,16 +4,20 @@ import com.sanaa.vod.dataSource.remote.dto.cutsom_list.SavedItemDto
 import com.sanaa.vod.dataSource.remote.dto.cutsom_list.SavedListDto
 
 interface RemoteSavedListDataSource {
-    suspend fun fetchUserLists(sessionId: String, accountId: Long): List<SavedListDto>
+
+    suspend fun fetchUserLists(sessionId: String, accountId: Long, page: Int? = null): List<SavedListDto>
+
     suspend fun createList(
         sessionId: String,
         name: String,
-        description: String = "this is dummy description"
+        description: String = " "
     ): SavedListDto
 
     suspend fun deleteList(sessionId: String, listId: Int)
 
     suspend fun fetchListItems(listId: Int, page: Int? = null): List<SavedItemDto>
+
     suspend fun addItem(sessionId: String, listId: Int, movieId: Int): Boolean
+
     suspend fun removeItem(sessionId: String, listId: Int, movieId: Int): Boolean
 }
--- data/repositories/vod/src/main/java/com/sanaa/vod/repository/SavedListRepositoryImpl.kt
+++ data/repositories/vod/src/main/java/com/sanaa/vod/repository/SavedListRepositoryImpl.kt
@@ -1,9 +1,10 @@
 package com.sanaa.vod.repository
 
-import android.icu.text.CaseMap
 import com.sanaa.identity.dataSoruce.local.dataStore.PreferencesManager
+import com.sanaa.vod.dataSource.remote.RemoteMovieDataSource
 import com.sanaa.vod.dataSource.remote.custom_list.RemoteSavedListDataSource
-import com.sanaa.vod.mapper.custom_list.toDomain
+import com.sanaa.vod.repository.mapper.custom_list.toEntity
+import com.sanaa.vod.repository.mapper.media.toEntity
 import com.sanaa.vod.util.safeCall
 import entity.Movie
 import kotlinx.coroutines.flow.first
@@ -12,40 +13,45 @@ import usecase.custom_list.custom_list_param.*
 import javax.inject.Inject
 
 class SavedListRepositoryImpl @Inject constructor(
-    private val remote: RemoteSavedListDataSource,
-    private val prefs: PreferencesManager
+    private val remoteSavedListDataSource: RemoteSavedListDataSource,
+    private val remoteMovieDataSource: RemoteMovieDataSource,
+    private val preferencesManager: PreferencesManager
 ) : SavedListRepository {
 
-    private suspend fun sessionId(): String = prefs.sessionId.first()
+    private suspend fun sessionId(): String = preferencesManager.sessionId.first()
 
     override suspend fun getSavedLists(accountId: Long): List<SavedList> =
         safeCall("Failed to fetch saved lists") {
-            remote.fetchUserLists(sessionId(), accountId).map { it.toDomain() }
+            remoteSavedListDataSource.fetchUserLists(sessionId(), accountId).map { it.toEntity() }
         }
 
     override suspend fun createSavedList(title: String): SavedList =
         safeCall("Failed to create list") {
-            remote.createList(sessionId(), title).toDomain()
+            remoteSavedListDataSource.createList(sessionId(), title).toEntity()
         }
 
     override suspend fun deleteSavedList(listId: Int) {
         safeCall("Failed to delete list") {
-            remote.deleteList(sessionId(), listId)
+            remoteSavedListDataSource.deleteList(sessionId(), listId)
         }
     }
 
     override suspend fun getAllMoviesInList(listId: Int): List<Movie> =
         safeCall("Failed to fetch list items") {
-            remote.fetchListItems(listId).map { it.toDomain() }
+            remoteSavedListDataSource.fetchListItems(listId).map {
+                remoteMovieDataSource.fetchMovieDetails(
+                    it.toEntity().id
+                ).toEntity()
+            }
         }
 
     override suspend fun addMovieToList(listId: Int, movieId: Int): Boolean =
         safeCall("Failed to add movie to list") {
-            remote.addItem(sessionId(), listId, movieId)
+            remoteSavedListDataSource.addItem(sessionId(), listId, movieId)
         }
 
     override suspend fun removeMovieFromList(listId: Int, movieId: Int): Boolean =
         safeCall("Failed to remove item from list") {
-            remote.removeItem(sessionId(), listId, movieId)
+            remoteSavedListDataSource.removeItem(sessionId(), listId, movieId)
         }
 }
