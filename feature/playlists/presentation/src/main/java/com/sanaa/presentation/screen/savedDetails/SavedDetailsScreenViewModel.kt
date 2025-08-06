c3bf0869b1621466dcb274d1580110924a869173
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-06 03:13:53 +0300

    Load saved items in SavedDetailsScreenViewModel

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/SavedDetailsScreenViewModel.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/savedDetails/SavedDetailsScreenViewModel.kt
@@ -1,32 +1,97 @@
 package com.sanaa.presentation.screen.savedDetails
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import com.sanaa.presentation.model.toUiModel
+import com.sanaa.presentation.savedBase.BasePagingSource
 import com.sanaa.presentation.savedBase.BaseViewModel
 import com.sanaa.presentation.screen.savedDetails.state.MediaItem
 import com.sanaa.presentation.screen.savedDetails.state.SavedDetailsScreenUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import entity.Movie
+import exceptions.NoNetworkException
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import usecase.CheckIfUserIsLoggedInUseCase
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import usecase.custom_list.ManageSavedListItemsUseCase
 import javax.inject.Inject
 
 @HiltViewModel
 class SavedDetailsScreenViewModel @Inject constructor(
-    private val checkUserLogin: CheckIfUserIsLoggedInUseCase,
+    savedStateHandle: SavedStateHandle,
+    private val manageSavedListItemsUseCase: ManageSavedListItemsUseCase,
     private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) :
     BaseViewModel<SavedDetailsScreenUiState, SavedDetailsScreenEffect>(SavedDetailsScreenUiState()),
     SavedDetailsInteractionListener {
+    private val listId: Int = checkNotNull(savedStateHandle["listId"]) {
+        "listId is required in SavedStateHandle"
+    }
+
+    private val title: String = checkNotNull(savedStateHandle["title"]) {
+        "title is required in SavedStateHandle"
+    }
+
+    init {
+        loadItemsInSaved(listId)
+    }
+
+
+    private fun loadItemsInSaved(categoryId: Int) {
+        tryToCollect(
+
+            callee = {
+                loadSavedMovies(categoryId)
+            },
+            onCollect = { movies ->
+                updateState {
+                    it.copy(movieList = flowOf(movies), title = title, isLoading = false)
+                }
+            },
+            onError = { exception ->
+                if (exception is NoNetworkException) {
+                    updateState {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = null
+                        )
+                    }
+                } else {
+                    updateState {
+                        it.copy(errorMessage = exception.message, isLoading = false)
+                    }
+                }
+            }
+        )
+    }
+
     override fun onMediaClick(
         mediaId: Int,
     ) {
-        TODO("Not yet implemented")
+        //  TODO("Not yet implemented")
     }
 
     override fun onSaveIconClick(mediaItem: MediaItem) {
-        TODO("Not yet implemented")
+        //   TODO("Not yet implemented")
     }
 
     override fun onBackClick() {
         emitEffect(SavedDetailsScreenEffect.NavigateBack)
     }
+
+    private fun loadSavedMovies(listId: Int): Flow<PagingData<MediaItem>> {
+        return createPagingFlow(
+            pagingSourceFactory = { createSavedMoviesPagingSource(listId) },
+            mapper = Movie::toUiModel
+        )
+    }
+
+    private fun createSavedMoviesPagingSource(listId: Int): PagingSource<Int, Movie> {
+        return BasePagingSource { page ->
+            manageSavedListItemsUseCase.getAllItemsInSavedList(listId, page)
+        }
+    }
+
 }
