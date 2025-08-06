f82a2e69ca41103d97528db6c12a8bc0ea96d341
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-06 17:47:47 +0300

    Add navigateToMediaDetails effect to PlaylistDetailsScreenEffect

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlistDetails/PlaylistDetailsScreenEffect.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlistDetails/PlaylistDetailsScreenEffect.kt
@@ -1,7 +1,13 @@
 package com.sanaa.presentation.screen.playlistDetails
 
+import com.sanaa.presentation.screen.playlistDetails.state.MediaTypeUi
+
 sealed interface PlaylistDetailsScreenEffect {
     object NavigateBack : PlaylistDetailsScreenEffect
     data object ShowErrorSnackBar : PlaylistDetailsScreenEffect
     data object ShowSuccessSnackBar : PlaylistDetailsScreenEffect
+
+    data class NavigateToMediaDetails(val mediaId: Int, val mediaTypeUi: MediaTypeUi) :
+        PlaylistDetailsScreenEffect
+
 }
