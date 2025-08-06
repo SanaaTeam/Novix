7d3bcaf7ee1a5bf4bddd36ef004f677d4499b402
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-07 01:20:40 +0300

    Add genre click listeners to WatchingHistoryInteractionListener

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/watchingHistory/WatchingHistoryInteractionListener.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/watchingHistory/WatchingHistoryInteractionListener.kt
@@ -4,7 +4,9 @@ import com.sanaa.presentation.model.MediaItemUiModel
 import com.sanaa.presentation.screen.myRating.MediaTypeUi
 
 interface WatchingHistoryInteractionListener {
-    fun onMediaTabSelection(mediaTypeUi: MediaTypeUi?)
+    fun onMediaTabSelection(mediaTypeUi: MediaTypeUi)
+    fun onMovieGenreClick(genreId: Int?)
+    fun onTvShowGenreClick(genreId: Int?)
     fun onMediaClick(id: Int, mediaTypeUi: MediaTypeUi)
     fun onSaveIconClick(mediaItem: MediaItemUiModel)
     fun onBackClick()
