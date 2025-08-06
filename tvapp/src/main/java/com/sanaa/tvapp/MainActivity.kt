c9725470f47a3752bf98eac7827a327cb8dcea14
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-06 07:24:37 +0300

    apply NovixTheme and display SearchScreenContent

--- tvapp/src/main/java/com/sanaa/tvapp/MainActivity.kt
+++ tvapp/src/main/java/com/sanaa/tvapp/MainActivity.kt
@@ -3,13 +3,16 @@ package com.sanaa.tvapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.tv.material3.ExperimentalTvMaterial3Api
+import com.sanaa.designsystem.design_system.theme.NovixTheme
+import com.sanaa.tvapp.presentation.screens.searchScreen.SearchScreenContent
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalTvMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            NovixTheme {
+                SearchScreenContent()
+            }
 
         }
     }
