9021857a339d6f0314145540485e4c53a6899618
haider <haideraljawasim@gmail.com>
2025-08-06 17:00:06 +0300

    add theme for auth feature based on saved theme

---
+++ feature/authentication/presentation/src/main/java/com/sanaa/presentation/api/AuthenticationViewModel.kt
@@ -0,0 +1,43 @@
+package com.sanaa.presentation.api
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import repository.Theme
+import repository.UserPreferencesRepository
+import javax.inject.Inject
+
+@HiltViewModel
+class AuthenticationViewModel @Inject constructor(
+    private val userPreference: UserPreferencesRepository
+) : ViewModel() {
+    private val _state = MutableStateFlow(AuthenticationUiState())
+    val state = _state.asStateFlow()
+
+    init {
+        fetchUserPreference()
+    }
+
+    private fun fetchUserPreference() {
+        viewModelScope.launch {
+            launch {
+                userPreference.getTheme().collect { theme ->
+                    updateState { it.copy(isDarkTheme = theme == Theme.DARK) }
+                }
+            }
+        }
+    }
+
+    private fun updateState(block: (AuthenticationUiState) -> AuthenticationUiState) {
+        _state.value = block(_state.value)
+    }
+
+}
+
+
+data class AuthenticationUiState(
+    val isDarkTheme: Boolean = false,
+)
