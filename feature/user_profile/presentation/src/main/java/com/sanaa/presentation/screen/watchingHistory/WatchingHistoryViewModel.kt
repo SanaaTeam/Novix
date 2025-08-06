261ae25529dad91126b493bc93437dff394153d9
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-07 01:22:49 +0300

    update watching history screen

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/watchingHistory/WatchingHistoryViewModel.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/watchingHistory/WatchingHistoryViewModel.kt
@@ -1,38 +1,114 @@
 package com.sanaa.presentation.screen.watchingHistory
 
-import androidx.lifecycle.viewModelScope
-import androidx.paging.PagingData
-import androidx.paging.cachedIn
 import com.sanaa.presentation.model.MediaItemUiModel
 import com.sanaa.presentation.profileBase.BaseViewModel
+import com.sanaa.presentation.profileMapper.toGenreUiState
 import com.sanaa.presentation.profileMapper.toMediaItemUiModel
 import com.sanaa.presentation.screen.myRating.MediaTypeUi
 import dagger.hilt.android.lifecycle.HiltViewModel
+import entity.MediaHistoryItem
+import exceptions.NoLoggedInUserException
 import jakarta.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
-import kotlinx.coroutines.flow.map
 import usecase.GetLoggedInUserUseCase
-import usecase.history.ManageWatchingHistoryUseCase
+import usecase.ManageMovieUseCase
+import usecase.ManageTvSeriesUseCase
+import usecase.history.ManageWatchedMediaHistoryUseCase
 import usecase.search.search_param.MediaType
 
 @HiltViewModel
 class WatchingHistoryViewModel @Inject constructor(
-    private val manageWatchingHistoryUseCase: ManageWatchingHistoryUseCase,
+    private val manageWatchedMediaHistoryUseCase: ManageWatchedMediaHistoryUseCase,
     private val getLoggedInUserUseCase: GetLoggedInUserUseCase,
-    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
-    ) : BaseViewModel<WatchingHistoryUiState, WatchingHistoryScreenEffect>
-    (initialState = WatchingHistoryUiState(), defaultDispatcher = dispatcher),
-    WatchingHistoryInteractionListener {
+    private val manageMovieUseCase: ManageMovieUseCase,
+    private val manageTvSeriesUseCase: ManageTvSeriesUseCase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : BaseViewModel<WatchingHistoryUiState, WatchingHistoryScreenEffect>(
+    WatchingHistoryUiState(),
+    dispatcher
+), WatchingHistoryInteractionListener {
 
     init {
-        loadWatchingHistory()
+        fetchMovieGenres()
+        fetchTvShowGenres()
+        fetchMovies()
+        fetchTvShows()
     }
 
-    override fun onMediaTabSelection(mediaTypeUi: MediaTypeUi?) {
+    private fun fetchMovies(genreId: Int? = null) {
+        tryToCollect(
+            callee = {
+                loadWatchedHistoryMovies(genreId)
+            }, onCollect = { mediaList ->
+                updateState {
+                    it.copy(movieList = mediaList.map { it.toMediaItemUiModel() }, isLoading = false)
+                }
+            },
+            onError = { ::onLoadDataError }
+        )
+    }
+
+    private fun fetchTvShows(genreId: Int? = null) {
+        tryToCollect(
+            callee = {
+                loadWatchedHistoryTvSeries(genreId)
+            }, onCollect = { mediaList ->
+                updateState {
+                    it.copy(tvShowList = mediaList.map { it.toMediaItemUiModel() }, isLoading = false)
+                }
+            },
+            onError = { ::onLoadDataError }
+        )
+    }
+
+    private fun fetchMovieGenres() {
+        tryToExecute(
+            callee = {
+                manageMovieUseCase.getMovieGenres().map { it.toGenreUiState() }
+            },
+            onSuccess = { genres ->
+                updateState {
+                    it.copy(movieGenres = genres)
+                }
+            },
+            onError = { ::onLoadDataError }
+        )
+    }
+
+    private fun fetchTvShowGenres() {
+        tryToExecute(
+            callee = {
+                manageTvSeriesUseCase.getSeriesGenres().map { it.toGenreUiState() }
+            },
+            onSuccess = { genres ->
+                updateState {
+                    it.copy(tvShowGenres = genres)
+                }
+            },
+            onError = { ::onLoadDataError }
+        )
+    }
+
+    override fun onMediaTabSelection(mediaTypeUi: MediaTypeUi) {
         updateState { it.copy(selectedMediaTypeUi = mediaTypeUi) }
-        loadWatchingHistoryByType(mediaTypeUi)
+    }
+
+
+    override fun onMovieGenreClick(genreId: Int?) {
+        if (genreId != state.value.movieSelectedGenreId) {
+            updateState { it.copy(movieSelectedGenreId = genreId) }
+            fetchMovies(genreId)
+        }
+    }
+
+    override fun onTvShowGenreClick(genreId: Int?) {
+        if (genreId != state.value.tvShowSelectedGenreId) {
+            updateState { it.copy(tvShowSelectedGenreId = genreId) }
+            fetchTvShows(genreId)
+        }
     }
 
     override fun onMediaClick(id: Int, mediaTypeUi: MediaTypeUi) {
@@ -40,75 +116,54 @@ class WatchingHistoryViewModel @Inject constructor(
     }
 
     override fun onSaveIconClick(mediaItem: MediaItemUiModel) {
-        // TODO: Implement save/unsave functionality
+
     }
 
     override fun onBackClick() {
         emitEffect(WatchingHistoryScreenEffect.NavigateBack)
     }
 
-    private fun loadWatchingHistory() {
-        tryToExecute(
-            callee = {
-                val user = getLoggedInUserUseCase.getLoggedInUser()
-                manageWatchingHistoryUseCase.getWatchingHistory(user.username, null)
-            },
-            onSuccess = { historyFlow ->
-                val pagingFlow = historyFlow.map { mediaHistoryItems ->
-                    mediaHistoryItems.map {
-                        it.toMediaItemUiModel()
-                    }.let { PagingData.from(it) }
-                }
-                updateState {
-                    it.copy(
-                        watchingHistory = pagingFlow.cachedIn(viewModelScope),
-                        error = null
-                    )
-                }
-            },
-            onError = { exception ->
-                updateState {
-                    it.copy(
-                        watchingHistory = flowOf(PagingData.empty()),
-                        error = exception.message
-                    )
-                }
-            }
+    private suspend fun loadWatchedHistoryMovies(
+        genreId: Int?
+    ): Flow<List<MediaHistoryItem>> {
+        updateState { it.copy(isLoading = true) }
+        val user = try {
+            getLoggedInUserUseCase.getLoggedInUser()
+        } catch (_: NoLoggedInUserException) {
+            null
+        }
+        if (user == null) return flowOf(emptyList())
+        return manageWatchedMediaHistoryUseCase.getMediaHistory(
+            genreId = genreId,
+            mediaType = MediaType.MOVIE,
+            username = user.username
         )
     }
 
-    private fun loadWatchingHistoryByType(mediaTypeUi: MediaTypeUi?) {
-        tryToExecute(
-            callee = {
-                val user = getLoggedInUserUseCase.getLoggedInUser()
-                val mediaType = when (mediaTypeUi) {
-                    MediaTypeUi.MOVIE -> MediaType.MOVIE
-                    MediaTypeUi.TV_SHOW -> MediaType.TV_SERIES
-                    null -> null
-                }
-                manageWatchingHistoryUseCase.getWatchingHistory(user.username, mediaType)
-            },
-            onSuccess = { historyFlow ->
-                val pagingFlow = historyFlow.map { mediaHistoryItems ->
-                    mediaHistoryItems.map {
-                        it.toMediaItemUiModel()
-                    }.let { PagingData.from(it) }
-                }
-                updateState {
-                    it.copy(
-                        watchingHistory = pagingFlow.cachedIn(viewModelScope),
-                        error = null
-                    )
-                }
-            },
-            onError = { exception ->
-                updateState {
-                    it.copy(
-                        watchingHistory = flowOf(PagingData.empty()),
-                        error = exception.message
-                    )
-                }
-            }
+    private suspend fun loadWatchedHistoryTvSeries(
+        genreId: Int?
+    ): Flow<List<MediaHistoryItem>> {
+        updateState { it.copy(isLoading = true) }
+        val user = try {
+            getLoggedInUserUseCase.getLoggedInUser()
+        } catch (_: NoLoggedInUserException) {
+            null
+        }
+        if (user == null) return flowOf(emptyList())
+
+        return manageWatchedMediaHistoryUseCase.getMediaHistory(
+            genreId = genreId,
+            mediaType = MediaType.TV_SERIES,
+            username = user.username
         )
+    }
+
+    private fun onLoadDataError(exception: Throwable) {
+        updateState {
+            it.copy(
+                error = exception.message,
+                isLoading = false
+            )
+        }
     }
 }
