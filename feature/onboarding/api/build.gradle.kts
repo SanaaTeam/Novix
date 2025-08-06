baddd926e5c7eed5fca070017ab3f7ce0548ca42
shahad-97 <shos8406@gmail.com>
2025-08-06 15:07:07 +0300

    remove unused libraries and redundant test dependencies

--- feature/onboarding/api/build.gradle.kts
+++ feature/onboarding/api/build.gradle.kts
@@ -9,14 +9,8 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 
     testImplementation(libs.bundles.test)
-
 }
