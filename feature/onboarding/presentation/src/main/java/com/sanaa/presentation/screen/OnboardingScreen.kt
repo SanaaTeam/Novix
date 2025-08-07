024d856d4cf40f9fa26a361d57c762d0c2b45c77
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-08-08 02:11:58 +0300

    fix navigation bar padding and screens content

--- feature/onboarding/presentation/src/main/java/com/sanaa/presentation/screen/OnboardingScreen.kt
+++ feature/onboarding/presentation/src/main/java/com/sanaa/presentation/screen/OnboardingScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
@@ -80,7 +81,8 @@ fun OnBoardingScreenContent(
         NovixScaffold(
             modifier = modifier
                 .fillMaxSize()
-                .statusBarsPadding(),
+                .statusBarsPadding()
+                .navigationBarsPadding(),
             topBar = {
                 if (pagerState.currentPage != state.pageList.lastIndex) {
                     Row (
