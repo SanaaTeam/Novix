4ce54ad07369d462dd1ad67b889df644f07b154f
shahad-97 <shos8406@gmail.com>
2025-08-07 03:41:30 +0300

    Convert Onboarding API module to use Compose

--- feature/onboarding/api/build.gradle.kts
+++ feature/onboarding/api/build.gradle.kts
@@ -1,16 +1,15 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.novix.android.compose)
 }
 
 android {
     namespace = "com.sanaa.feature.onboarding.api"
 }
-
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
-
+    implementation(libs.material)
     testImplementation(libs.bundles.test)
+    androidTestImplementation(libs.androidx.junit)
+    implementation(libs.androidx.navigation.compose)
 }
