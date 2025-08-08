3ecbd44f97f9da5dd0fc38d74a9b61c4290443d8
ElhamFadel <elham2000fadel@gmail.com>
2025-08-09 01:41:33 +0300

    delete depulcatied localNavController files
    Signed-off-by: ElhamFadel <elham2000fadel@gmail.com>

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/api/navigationSaved/LocalNavControllerProvider.kt
+++
@@ -1,8 +0,0 @@
-package com.sanaa.presentation.api.navigationSaved
-
-import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.navigation.NavHostController
-
-val LocalNavControllerProvider = staticCompositionLocalOf<NavHostController> {
-    error("No navigation provider found")
-}
