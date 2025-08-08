c92d40e3f24f595ce0771290447230462a8aba38
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-08-08 16:52:46 +0300

    fix gradle dependencies unresolve issue

--- tvapp/build.gradle.kts
+++ tvapp/build.gradle.kts
@@ -1,10 +1,23 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kover)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.hilt.android)
     alias(libs.plugins.ksp)
+}
+
+val localProperties = Properties()
+val keysFile = rootProject.file("keys.properties")
+val defaultFile = rootProject.file("default.properties")
+when {
+    keysFile.exists() -> localProperties.load(FileInputStream(keysFile))
+    defaultFile.exists() -> localProperties.load(FileInputStream(defaultFile))
+    else -> throw GradleException("No keys.properties or default.properties found!")
 }
 
 android {
@@ -18,6 +31,10 @@ android {
         versionCode = libs.versions.versionCode.get().toInt()
         versionName = libs.versions.versionName.get()
 
+        val apiKey = localProperties["TMDB_API_KEY"]?.toString() ?: ""
+        buildConfigField("String", "TMDB_API_KEY", "\"${apiKey.trim()}\"")
+        buildConfigField("String", "TMDB_URL", "\"https://api.themoviedb.org/3/\"")
+
     }
 
     buildTypes {
@@ -29,6 +46,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -38,10 +56,55 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
 dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.kotlinx.datetime)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.bundles.ktor)
+
+    implementation(libs.androidx.datastore)
+    implementation(libs.bundles.room)
+
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.bundles.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.coil.compose)
+    implementation(libs.androidx.tv.foundation)
+    implementation(libs.androidx.tv.material)
+    debugImplementation(libs.androidx.ui.compose.tooling)
+    debugImplementation(libs.androidx.ui.compose.test.manifest)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.compose.test.junit4)
+    androidTestImplementation(libs.androidx.junit)
+
+    implementation(libs.androidx.paging.runtime)
+    implementation(libs.androidx.paging.compose)
+
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics.ndk)
+    implementation(libs.firebase.perf)
+
+    implementation(libs.tensorflow.lite.task.vision)
+
+    implementation(libs.timber)
+
     implementation(projects.designSystem)
     implementation(projects.imageViewer)
 
@@ -69,57 +132,7 @@ dependencies {
     implementation(projects.feature.authentication.presentation)
     implementation(projects.preferences)
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui.compose)
-    implementation(libs.androidx.ui.compose.graphics)
-    implementation(libs.androidx.ui.compose.tooling.preview)
-    implementation(libs.androidx.tv.foundation)
-    implementation(libs.androidx.tv.material)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.compose.test.junit4)
-    debugImplementation(libs.androidx.ui.compose.tooling)
-    debugImplementation(libs.androidx.ui.compose.test.manifest)
-
-    implementation(libs.coil.compose)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.tensorflow.lite.task.vision)
-
-    implementation(libs.bundles.compose)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.bundles.test)
-    androidTestImplementation(libs.androidx.junit)
-    implementation(libs.androidx.navigation.compose)
-    implementation(libs.coil.compose)
-    implementation(libs.kotlinx.datetime)
-
-    androidTestImplementation(libs.androidx.junit)
-
-    implementation(libs.bundles.compose)
-    implementation(libs.androidx.ui.compose.foundation)
-    implementation(platform(libs.androidx.compose.bom))
-
-    implementation(libs.androidx.lifecycle.viewmodel.compose)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-
-    // Paging 3
-    implementation(libs.androidx.paging.runtime)
-    implementation(libs.androidx.paging.compose)
-    testImplementation(libs.androidx.paging.testing)
-
     testImplementation(libs.bundles.test)
     testImplementation(libs.bundles.test.runtime)
-
     testImplementation(libs.turbine)
-
-    implementation(libs.hilt.android)
-    ksp(libs.hilt.android.compiler)
-    implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.slf4j.api)
 }
