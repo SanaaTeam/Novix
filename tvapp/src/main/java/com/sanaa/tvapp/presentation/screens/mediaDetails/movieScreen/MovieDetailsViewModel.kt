97a4e1673a57268006e15f3079ee0d7f32400565
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-08-08 20:22:31 +0300

    set up movie details view model

--- tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/mediaDetails/movieScreen/MovieDetailsViewModel.kt
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/mediaDetails/movieScreen/MovieDetailsViewModel.kt
@@ -1,4 +1,162 @@
 package com.sanaa.tvapp.presentation.screens.mediaDetails.movieScreen
 
-class MovieDetailsViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import androidx.paging.cachedIn
+import androidx.paging.map
+import com.sanaa.presentation.details_base.BasePagingSource
+import com.sanaa.presentation.model.MovieUiModel
+import com.sanaa.presentation.model.mapper.toActorUiModel
+import com.sanaa.presentation.model.mapper.toUiModel
+import com.sanaa.tvapp.base.TvBaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import entity.Movie
+import exceptions.NoNetworkException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import usecase.CheckIfUserIsLoggedInUseCase
+import usecase.GetLoggedInUserUseCase
+import usecase.ManageMovieUseCase
+import usecase.history.ManageWatchedMediaHistoryUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieDetailsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val manageMovieDetails: ManageMovieUseCase,
+    private val checkUserLogin: CheckIfUserIsLoggedInUseCase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) : TvBaseViewModel<MovieDetailsScreenUiState,MovieDetailsScreenUiEffect>(
+    initialState = MovieDetailsScreenUiState(),
+    defaultDispatcher = dispatcher
+) ,MovieDetailsScreenInteractionListener{
+
+    private val movieId: Int = checkNotNull(savedStateHandle["movieId"]) {
+        "movieId is required in SavedStateHandle"
+    }
+
+    init {
+        updateUserStatus()
+    }
+
+
+
+    private fun fetchMovieDetails(movieId: Int) {
+        updateState { it.copy(isLoading = true, errorMessage = null) }
+        tryToExecute(
+            callee = { loadMovieDetails(movieId) },
+            onSuccess = {
+                updateState { it.copy(isLoading = false, errorMessage = null) }
+            },
+            onError = { exception ->
+                if (exception is NoNetworkException) {
+                    updateState {
+                        it.copy(
+                            noInternetConnection = true,
+                            isLoading = false,
+                            errorMessage = null
+                        )
+                    }
+                } else {
+                    updateState {
+                        it.copy(
+                            isLoading = false,
+                            errorMessage = exception.message,
+                            noInternetConnection = false
+                        )
+                    }
+                }
+            },
+            dispatcher = dispatcher
+        )
+    }
+
+    private suspend fun loadMovieDetails(movieId: Int) = coroutineScope {
+        val movieDeferred = async { manageMovieDetails.getMovieDetails(movieId) }
+        val castDeferred = async { manageMovieDetails.getMovieCast(movieId) }
+        val imagesDeferred = async { manageMovieDetails.getMovieImages(movieId) }
+        val trailerDeferred = async { manageMovieDetails.getMovieTrailer(movieId) }
+        val similarDeferred = async { loadSimilarMovies(movieId) }
+
+        val movie = movieDeferred.await()
+        val cast = castDeferred.await()
+        val images = imagesDeferred.await()
+        val trailerUrl = trailerDeferred.await()
+        val similar = similarDeferred.await()
+
+        updateState {
+            it.copy(
+                movieDetails = movie.toUiModel(isBookmarked = false, trailerUrl = trailerUrl),
+                cast = cast.map { it.toActorUiModel() },
+                imagesUrls = images,
+                similarMovies = similar,
+            )
+        }
+
+    }
+
+    private fun loadSimilarMovies(movieId: Int): Flow<PagingData<MovieUiModel>> {
+        return createPagingFlow(
+            pagingSourceFactory = { createSimilarMoviesPagingSource(movieId) },
+            mapper = Movie::toUiModel
+        )
+    }
+
+
+    private fun createSimilarMoviesPagingSource(movieId: Int): PagingSource<Int, Movie> {
+        return BasePagingSource { page ->
+            manageMovieDetails.getSimilarMoviesByMovieId(movieId, page)
+        }
+    }
+
+
+    override fun onBackClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onWatchTrailerClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSimilarMovieClick(movieId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onDismissLoginBottomSheet() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onLoginButtonClick() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onActorCardClick(actorId: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRetryLoadDetails() {
+        TODO("Not yet implemented")
+    }
+
+
+    private suspend fun updateUserLoginState() {
+        val isUserLoggedIn = checkUserLogin.isLoggedIn()
+        updateState { it.copy(isUserLoggedIn = isUserLoggedIn) }
+    }
+
+    fun updateUserStatus() {
+        tryToExecute(callee = ::updateUserLoginState)
+    }
+
+    companion object{
+        private const val PAGE_SIZE = 20
+    }
 }
