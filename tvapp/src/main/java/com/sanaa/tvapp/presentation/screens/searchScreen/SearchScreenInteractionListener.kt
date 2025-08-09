1b76708bc61a135ab0282412f6e759efaf1fd32f
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-09 16:42:08 +0300

    add search result media click

--- tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/searchScreen/SearchScreenInteractionListener.kt
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/searchScreen/SearchScreenInteractionListener.kt
@@ -1,9 +1,12 @@
 package com.sanaa.tvapp.presentation.screens.searchScreen
 
+import com.sanaa.presentation.screen.state.RecentViewedUiModel
+
 interface SearchScreenInteractionListener {
     fun onTabSelected(index: Int)
     fun onSearchQueryChanged(query: String)
     fun retrySearch()
     fun onActorClicked(id: Int)
     fun onLoginButtonClick()
+    fun onSearchResultMediaClicked()
 }
