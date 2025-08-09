e4c7823464badc0b156fa0cb1fa50eb848fea297
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-09 16:37:31 +0300

    create tv content composables for search results
    New composables:
    - `ActorTvContent` for displaying actor search results.
    - `MovieTvContent` for displaying movie search results.
    - `TvShowTvContent` for displaying TV show search results.
    
    All composables use `TvLazyRow` with `FocusableMediaCard` for individual items.

---
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/searchScreen/componants/MovieTvContent.kt
@@ -0,0 +1,31 @@
+package com.sanaa.tvapp.presentation.screens.searchScreen.componants
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import androidx.tv.foundation.lazy.list.TvLazyRow
+import com.sanaa.tvapp.presentation.screens.searchScreen.MovieUiModel
+
+
+@Composable
+fun MovieTvContent(
+    movies: LazyPagingItems<MovieUiModel>,
+    onClick: (MovieUiModel) -> Unit
+) {
+    TvLazyRow(
+        horizontalArrangement = Arrangement.spacedBy(20.dp),
+        contentPadding = PaddingValues(horizontal = 36.dp, vertical = 24.dp)
+    ) {
+        items(movies.itemCount) { index ->
+            movies[index]?.let { movie ->
+                FocusableMediaCard(
+                    imageUrl = movie.imageUrl,
+                    titleText = movie.title,
+                    onClick = { onClick(movie) }
+                )
+            }
+        }
+    }
+}
