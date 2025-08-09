ed3d25b23c12a72fdd8ade66c78b20c5405216c3
Hager Abobakr <hagarabobakr1@gmail.com>
2025-08-09 09:11:40 +0300

    enable edge to edge display

--- tvapp/src/main/java/com/sanaa/tvapp/MainActivity.kt
+++ tvapp/src/main/java/com/sanaa/tvapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sanaa.tvapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.sanaa.designsystem.design_system.theme.NovixTheme
 import com.sanaa.tvapp.presentation.screens.searchScreen.SearchScreen
 import com.sanaa.tvapp.presentation.screens.searchScreen.SearchScreenContent
@@ -12,6 +13,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        actionBar?.hide()
         setContent {
             NovixTheme {
                 SearchScreen()
