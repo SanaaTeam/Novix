e04e241e4933c448529f074d55cc11efa4eb8041
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-09 10:54:51 +0300

    add login screen effects and interaction listener

---
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/login/LoginScreenEffects.kt
@@ -0,0 +1,6 @@
+package com.sanaa.tvapp.presentation.screens.login
+
+sealed interface LoginScreenEffects {
+    data class ShowError(val message: String) : LoginScreenEffects
+    data class ShowSuccess(val message: String) : LoginScreenEffects
+}
---
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/login/LoginScreenInteractionListener.kt
@@ -0,0 +1,8 @@
+package com.sanaa.tvapp.presentation.screens.login
+
+interface LoginScreenInteractionListener {
+    fun onUsernameChanged(newUsername: String)
+    fun onPasswordChanged(newPassword: String)
+    fun onTogglePasswordVisibility()
+    fun onLoginClicked()
+}
