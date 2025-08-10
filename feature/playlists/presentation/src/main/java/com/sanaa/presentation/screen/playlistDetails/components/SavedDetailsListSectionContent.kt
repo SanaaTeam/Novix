d2f6c1e52de34531878885584db0c01f02248bdd
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-10 23:09:08 +0300

    Display loading indicator in SavedDetailsListSectionContent

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlistDetails/components/SavedDetailsListSectionContent.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlistDetails/components/SavedDetailsListSectionContent.kt
@@ -2,11 +2,16 @@ package com.sanaa.presentation.screen.playlistDetails.components
 
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.paging.LoadState
 import androidx.paging.compose.LazyPagingItems
+import com.sanaa.designsystem.design_system.component.loading.LoadingIndicator
 import com.sanaa.presentation.screen.playlistDetails.state.MediaItem
 
 @Composable
@@ -17,16 +22,28 @@ fun SavedDetailsListSectionContent(
     onSaveIconClick: (MediaItem) -> Unit = {},
     isScrollEnabled: Boolean = true,
 ) {
-    Column(
-        modifier = modifier,
-        verticalArrangement = Arrangement.spacedBy(12.dp)
-    ) {
-        PaginatedMediaListGrid(
-            mediaList = mediaList,
-            onMediaClick = onMediaClick,
-            onSaveIconClick = onSaveIconClick,
-            isScrollEnabled = isScrollEnabled,
-        )
+    when (mediaList.loadState.refresh) {
+        is LoadState.Loading -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                LoadingIndicator()
+            }
+        }
+        else -> {
+            Column(
+                modifier = modifier,
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                PaginatedMediaListGrid(
+                    mediaList = mediaList,
+                    onMediaClick = onMediaClick,
+                    onSaveIconClick = onSaveIconClick,
+                    isScrollEnabled = isScrollEnabled,
+                )
+            }
+        }
     }
 }
 
