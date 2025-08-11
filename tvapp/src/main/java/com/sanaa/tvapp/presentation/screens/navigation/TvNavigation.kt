db2785efacf33b36adfc436c8d399fd2a9d039df
AimanYosofi <AimanYosofiUlt@gmail.com>
2025-08-11 16:37:50 +0300

    add top app icon to the home screen

--- tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/navigation/TvNavigation.kt
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/navigation/TvNavigation.kt
@@ -3,6 +3,7 @@ package com.sanaa.tvapp.presentation.screens.navigation
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
@@ -35,44 +36,49 @@ fun TvNavigation(content: @Composable () -> Unit) {
 
     NavigationDrawer(
         drawerContent = {
-            Column(
+            Box(
                 modifier = Modifier
-                    .background(color = Theme.colors.surface)
                     .fillMaxHeight()
-                    .padding(start = 12.dp, end = 12.dp, top = 133.dp)
-                    .selectableGroup(),
-                horizontalAlignment = Alignment.Start,
-                verticalArrangement = Arrangement.spacedBy(10.dp)
+                    .padding(end = 12.dp),
+                contentAlignment = Alignment.Center
             ) {
-                screens.forEach { screen ->
-                    val isSelected = currentRoute == screen::class.qualifiedName
-                    NavigationDrawerItem(
-                        colors = NavigationDrawerItemDefaults.colors(
-                            containerColor = Theme.colors.surface,
-                            contentColor = Theme.colors.title,
-                            focusedContainerColor = Theme.colors.stroke,
-                            focusedContentColor = Theme.colors.title,
-                            selectedContainerColor = Theme.colors.primary,
-                            selectedContentColor = Theme.colors.onPrimary
-                        ),
-                        selected = isSelected,
-                        onClick = {
-                            navController.navigate(screen) {
-                                popUpTo<NavBarRoute.Home>()
-                                launchSingleTop = true
+                Column(
+                    modifier = Modifier
+                        .background(color = Theme.colors.surface)
+                        .selectableGroup(),
+                    horizontalAlignment = Alignment.Start,
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    screens.forEach { screen ->
+                        val isSelected = currentRoute == screen::class.qualifiedName
+                        NavigationDrawerItem(
+                            colors = NavigationDrawerItemDefaults.colors(
+                                containerColor = Theme.colors.surface,
+                                contentColor = Theme.colors.title,
+                                focusedContainerColor = Theme.colors.stroke,
+                                focusedContentColor = Theme.colors.title,
+                                selectedContainerColor = Theme.colors.surface,
+                                selectedContentColor = Theme.colors.onPrimary
+                            ),
+                            selected = isSelected,
+                            onClick = {
+                                navController.navigate(screen) {
+                                    popUpTo<NavBarRoute.Home>()
+                                    launchSingleTop = true
+                                }
+                            },
+                            leadingContent = {
+                                NavBarItem(
+                                    modifier = Modifier,
+                                    isSelected = isSelected,
+                                    onClick = {},
+                                    iconRes = screen.unselectedIcon,
+                                    selectedIconRes = screen.selectedIcon,
+                                )
                             }
-                        },
-                        leadingContent = {
-                            NavBarItem(
-                                modifier = Modifier.weight(1f),
-                                isSelected = isSelected,
-                                onClick = {},
-                                iconRes = screen.unselectedIcon,
-                                selectedIconRes = screen.selectedIcon,
-                            )
+                        ) {
+                            Text(text = stringResource(id = screen.titleRes))
                         }
-                    ) {
-                        Text(text = stringResource(id = screen.titleRes))
                     }
                 }
             }
