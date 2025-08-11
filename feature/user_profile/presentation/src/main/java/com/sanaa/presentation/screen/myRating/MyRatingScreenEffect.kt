f5ef2d5fbcc047bd1dec84eb3d9314bc16a63086
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-12 01:53:24 +0300

    add success and error snackbar messages

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myRating/MyRatingScreenEffect.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/myRating/MyRatingScreenEffect.kt
@@ -2,9 +2,8 @@ package com.sanaa.presentation.screen.myRating
 
 sealed interface MyRatingScreenEffect {
     object NavigateBack : MyRatingScreenEffect
-    data object  ShowErrorSnackBar : MyRatingScreenEffect
-    data object ShowSuccessSnackBar : MyRatingScreenEffect
-
+    data class ShowErrorSnackBar(val message: String) : MyRatingScreenEffect
+    data class ShowSuccessSnackBar(val message: String) : MyRatingScreenEffect
     data class NavigateToMediaDetails(val mediaId: Int, val mediaTypeUi: MediaTypeUi): MyRatingScreenEffect
 
 }
