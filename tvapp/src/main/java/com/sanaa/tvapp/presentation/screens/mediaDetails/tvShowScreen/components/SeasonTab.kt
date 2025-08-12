0d6e0b2151c74502db5a12d320b5b773a6b71d13
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-08-12 12:50:29 +0300

    revert design system changes

--- tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/mediaDetails/tvShowScreen/components/SeasonTab.kt
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/mediaDetails/tvShowScreen/components/SeasonTab.kt
@@ -1,26 +1,19 @@
 package com.sanaa.tvapp.presentation.screens.mediaDetails.tvShowScreen.components
 
-import android.util.Log
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.focusable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.sanaa.designsystem.design_system.component.chips.ToggleableChip
 import com.sanaa.designsystem.design_system.component.text.AppText
 import com.sanaa.designsystem.design_system.theme.Theme
 import com.sanaa.tvapp.R
+import com.sanaa.tvapp.presentation.screens.sharedComponents.TvToggleableChip
 
 @Composable
 fun SeasonTab(
@@ -45,12 +38,11 @@ fun SeasonTab(
             items = (1..seasonCounts).toList(),
             key = { _, item -> item }
         ) { index, item ->
-            ToggleableChip(
+            TvToggleableChip(
                 modifier = Modifier,
                 text = stringResource(R.string.season_number, item),
                 onClick = {
                     onClick(item)
-                    Log.d("test99", "SeasonTab: from itemsIndex with item : $item")
                           },
                 isSelected = currentSeason == item,
             )
