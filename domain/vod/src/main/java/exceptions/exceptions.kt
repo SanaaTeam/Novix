e0a8bcec3e8e7f91cae5df514260d24327f20d8e
haider <haideraljawasim@gmail.com>
2025-08-14 02:47:56 +0300

    delete unused exception

--- domain/vod/src/main/java/exceptions/exceptions.kt
+++ domain/vod/src/main/java/exceptions/exceptions.kt
@@ -3,4 +3,3 @@ package exceptions
 open class NovixAppException(message: String? = null) : Exception(message)
 
 class NoNetworkException : NovixAppException("No network connection")
-class OperationFailureException : NovixAppException("Operation failed")
