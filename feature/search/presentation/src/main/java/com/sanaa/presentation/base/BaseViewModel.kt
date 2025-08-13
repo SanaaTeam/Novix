2b638eccb513f41c7bf6b590f3c8f9eb77942fe9
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-08-13 14:30:49 +0300

    refactor try to execute and to collect

--- feature/search/presentation/src/main/java/com/sanaa/presentation/base/BaseViewModel.kt
+++ feature/search/presentation/src/main/java/com/sanaa/presentation/base/BaseViewModel.kt
@@ -17,6 +17,7 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import timber.log.Timber
+import kotlin.coroutines.cancellation.CancellationException
 
 abstract class BaseViewModel<T, E>(
     initialState: T,
@@ -34,16 +35,29 @@ abstract class BaseViewModel<T, E>(
         _state.update(updater)
     }
 
+    private fun createExceptionHandler(onError: (Throwable) -> Unit) =
+        CoroutineExceptionHandler { _, throwable ->
+            Timber.e(throwable, "Unhandled coroutine exception")
+            onError(throwable)
+        }
+
     protected fun <T> tryToExecute(
         block: suspend () -> T,
         onSuccess: (T) -> Unit = {},
         onError: (exception: Throwable) -> Unit = {},
-        dispatcher: CoroutineDispatcher = defaultDispatcher,
+        dispatcher: CoroutineDispatcher = defaultDispatcher
     ) {
-        val handler = createExceptionHandler(onError)
-        viewModelScope.launch(dispatcher + handler) {
-            val result = block()
-            onSuccess(result)
+        val exceptionHandler = createExceptionHandler(onError)
+
+        viewModelScope.launch(dispatcher + exceptionHandler) {
+            try {
+                val result = block()
+                onSuccess(result)
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Throwable) {
+                onError(e)
+            }
         }
     }
 
@@ -51,17 +65,29 @@ abstract class BaseViewModel<T, E>(
         block: suspend () -> Flow<T>,
         onCollect: suspend (T) -> Unit,
         onError: (exception: Throwable) -> Unit = {},
-        dispatcher: CoroutineDispatcher = defaultDispatcher,
+        dispatcher: CoroutineDispatcher = defaultDispatcher
     ) {
-        val handler = createExceptionHandler(onError)
-        viewModelScope.launch(dispatcher + handler) {
-            block()
-                .collectLatest { result ->
-                    onCollect(result)
-                }
+        val exceptionHandler = createExceptionHandler(onError)
+        viewModelScope.launch(dispatcher + exceptionHandler) {
+            try {
+                block()
+                    .catch { e ->
+                        onError(e)
+                    }
+                    .collectLatest { value ->
+                        try {
+                            onCollect(value)
+                        } catch (e: Throwable) {
+                            onError(e)
+                        }
+                    }
+            } catch (e: CancellationException) {
+                throw e
+            } catch (e: Throwable) {
+                onError(e)
+            }
         }
     }
-
 
     protected fun emitEffect(effect: E) {
         viewModelScope.launch {
@@ -69,8 +95,4 @@ abstract class BaseViewModel<T, E>(
         }
     }
 
-    private fun createExceptionHandler(onError: (Throwable) -> Unit) =
-        CoroutineExceptionHandler { _, throwable ->
-            onError(throwable)
-        }
 }
