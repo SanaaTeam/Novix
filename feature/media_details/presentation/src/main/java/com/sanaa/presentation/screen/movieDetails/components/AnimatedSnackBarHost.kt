3ab1dd3674b4cbde05a6fe639abfa6ab9addd9dc
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-14 06:35:06 +0300

    add `AnimatedSnackBarHost` and `MovieDetailsTopBar` composables

---
+++ feature/media_details/presentation/src/main/java/com/sanaa/presentation/screen/movieDetails/components/AnimatedSnackBarHost.kt
@@ -0,0 +1,44 @@
+package com.sanaa.presentation.screen.movieDetails.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sanaa.designsystem.design_system.component.animation.FadeSlideInVerticallyFromTop
+import com.sanaa.designsystem.design_system.component.animation.FadeSlideOutVerticallyToTop
+import com.sanaa.designsystem.design_system.component.snack_bar.SnackBar
+import com.sanaa.presentation.screen.movieDetails.SnackData
+import kotlinx.coroutines.delay
+
+@Composable
+fun AnimatedSnackBarHost(
+    data: SnackData?,
+    onDismiss: () -> Unit,
+    durationMillis: Long = 2500
+) {
+    AnimatedVisibility(
+        visible = data != null,
+        enter = FadeSlideInVerticallyFromTop,
+        exit = FadeSlideOutVerticallyToTop,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 12.dp)
+            .statusBarsPadding()
+    ) {
+        if (data != null) {
+            LaunchedEffect(data) {
+                delay(durationMillis)
+                onDismiss()
+            }
+            SnackBar(
+                message = data.message,
+                isErrorMessage = data.isError,
+                modifier = Modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
