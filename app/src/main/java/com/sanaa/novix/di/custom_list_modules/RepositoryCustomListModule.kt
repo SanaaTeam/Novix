89ab7de83b036529e8ff7980b9b0f60c38fcef8e
Ahmad Kharfan <plamoplamo12@gmail.com>
2025-08-14 09:53:28 +0300

    use LocalSavedListDataSource in hilt module

--- app/src/main/java/com/sanaa/novix/di/custom_list_modules/RepositoryCustomListModule.kt
+++ app/src/main/java/com/sanaa/novix/di/custom_list_modules/RepositoryCustomListModule.kt
@@ -1,15 +1,13 @@
 package com.sanaa.novix.di.custom_list_modules
 
-import com.sanaa.vod.cache.LocalSavedMovieDataSourceImpl
-import com.sanaa.vod.dataSource.local.cache.LocalSavedMovieDataSource
+import com.sanaa.vod.cache.LocalSavedListDataSourceImpl
+import com.sanaa.vod.dataSource.local.cache.LocalSavedListDataSource
 import com.sanaa.vod.repository.SavedListRepositoryImpl
-import com.sanaa.vod.repository.SavedListsStatusProviderImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import repository.SavedListRepository
-import repository.SavedListsStatusProvider
 import javax.inject.Singleton
 
 @Module
@@ -24,14 +22,7 @@ abstract class RepositoryCustomListModule {
 
     @Binds
     @Singleton
-    abstract fun bindSavedListsStatusProvider(
-        savedMovieStatusProviderImpl: SavedListsStatusProviderImpl,
-    ): SavedListsStatusProvider
-
-    @Binds
-    @Singleton
-    abstract fun bindLocalSavedMovieDataSource(
-        impl: LocalSavedMovieDataSourceImpl,
-    ): LocalSavedMovieDataSource
-
+    abstract fun bindLocalSavedListDataSource(
+        localSavedListDataSourceImpl: LocalSavedListDataSourceImpl,
+    ): LocalSavedListDataSource
 }
