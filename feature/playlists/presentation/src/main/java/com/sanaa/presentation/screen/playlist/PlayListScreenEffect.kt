7f5d632803b27ed15a21fe66dfd0ee25857de394
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-17 03:07:58 +0300

    Replaced specific snackbar messages with generic success/error messages

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlist/PlayListScreenEffect.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlist/PlayListScreenEffect.kt
@@ -1,10 +1,8 @@
 package com.sanaa.presentation.screen.playlist
 
-sealed class PlayListScreenEffect{
+sealed class PlayListScreenEffect {
     object NavigateToLogin : PlayListScreenEffect()
-    data object  ShowErrorToAddListSnackBar : PlayListScreenEffect()
-    data object ShowSuccessToDeleteListSnackBar : PlayListScreenEffect()
-    data object  ShowErrorToDeleteListSnackBar : PlayListScreenEffect()
-    data object ShowSuccessToAddListSnackBar : PlayListScreenEffect()
     data class NavigateToSavedDetails(val listId: Int, val title: String) : PlayListScreenEffect()
+    data class ShowSuccess(val message: String) : PlayListScreenEffect()
+    data class ShowError(val message: String) : PlayListScreenEffect()
 }
