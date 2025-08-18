6659f92c26af82d867c48dbfe93557d032811799
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-18 19:10:38 +0300

    Rename EmptyListScreen to EmptyItemsScreen and remove imageRes parameter

--- feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlistDetails/components/EmptyItemsScreen.kt
+++ feature/playlists/presentation/src/main/java/com/sanaa/presentation/screen/playlistDetails/components/EmptyItemsScreen.kt
@@ -1,15 +1,16 @@
 package com.sanaa.presentation.screen.playlistDetails.components
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -22,8 +23,7 @@ import com.sanaa.designsystem.design_system.theme.Theme
 import com.sanaa.feature.playlists.presentation.R
 
 @Composable
-fun EmptyListScreen(
-    @DrawableRes imageRes: Int,
+fun EmptyItemsScreen(
     messageText: String,
 ) {
     NovixScaffold {
@@ -34,19 +34,27 @@ fun EmptyListScreen(
             verticalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource(imageRes),
-                contentDescription = null
+                painter = painterResource(
+                    R.drawable.no_items
+                ),
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(bottom = 12.dp)
+                    .size(128.dp),
+                contentScale = ContentScale.Fit
+
             )
 
             AppText(
-                modifier = Modifier.padding(start = 32.dp, end = 32.dp, bottom = 24.dp),
+                modifier = Modifier.padding(start = 32.dp, end = 32.dp),
                 text = messageText,
                 style = Theme.textStyle.body.small,
                 color = Theme.colors.body,
                 textAlign = TextAlign.Center
             )
-        }
 
+
+        }
     }
 }
 
@@ -58,14 +66,9 @@ private fun EmptyListScreenLightPreview() {
     NovixTheme(
         isDarkMode = isDarkTheme
     ) {
-        val myListImg = if (isDarkTheme) {
-            R.drawable.no_items
-        } else {
-            R.drawable.no_items
-        }
-        EmptyListScreen(
+
+        EmptyItemsScreen(
             messageText = stringResource(R.string.the_list_is_empty),
-            imageRes = myListImg,
         )
     }
 }
@@ -77,14 +80,9 @@ private fun EmptyListScreenDarkPreview() {
     NovixTheme(
         isDarkMode = isDarkTheme
     ) {
-        val myListImg = if (isDarkTheme) {
-            R.drawable.no_items
-        } else {
-            R.drawable.no_items
-        }
-        EmptyListScreen(
+
+        EmptyItemsScreen(
             messageText = stringResource(R.string.the_list_is_empty),
-            imageRes = myListImg
         )
     }
 
