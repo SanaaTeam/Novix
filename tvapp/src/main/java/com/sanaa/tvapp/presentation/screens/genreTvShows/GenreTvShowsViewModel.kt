a80101e036a92081642e15e5b0b41f10a0170f0d
raouf-kamel <raoufkamel2013@gmail.com>
2025-08-18 20:21:45 +0300

    Add ViewModel for genre TV shows

---
+++ tvapp/src/main/java/com/sanaa/tvapp/presentation/screens/genreTvShows/GenreTvShowsViewModel.kt
@@ -0,0 +1,130 @@
+package com.sanaa.tvapp.presentation.screens.genreTvShows
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.paging.PagingData
+import androidx.paging.PagingSource
+import com.sanaa.tvapp.base.BasePagingSource
+import com.sanaa.tvapp.base.BaseViewModel
+import com.sanaa.tvapp.presentation.screens.searchScreen.TvShowUiModel
+import com.sanaa.tvapp.presentation.screens.searchScreen.mapper.toUiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import entity.TvShow
+import exceptions.NoNetworkException
+import exceptions.NovixAppException
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import usecase.CheckIfUserIsLoggedInUseCase
+import usecase.ManageTvShowUseCase
+import javax.inject.Inject
+
+@HiltViewModel
+class GenreTvShowsViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val manageTvShowUseCase: ManageTvShowUseCase,
+    private val checkIfUserIsLoggedInUseCase: CheckIfUserIsLoggedInUseCase,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : BaseViewModel<GenreTvShowsScreenUiState, GenreTvShowsEffects>(
+    initialState = GenreTvShowsScreenUiState(),
+    defaultDispatcher = dispatcher
+), GenreTvShowsScreenInteractionListener {
+
+    private val genreId: Int = checkNotNull(savedStateHandle["genreId"])
+    private val genreName: String = checkNotNull(savedStateHandle["genreName"])
+
+    init {
+        updateUserLoggingStatus()
+        getTvShowsByGenreId(genreId)
+    }
+
+    fun updateUserLoggingStatus() {
+        tryToCollect(
+            block = { checkIfUserIsLoggedInUseCase.isLoggedIn() },
+            onCollect = ::onCollectLoggedFlag,
+        )
+    }
+
+    private fun onCollectLoggedFlag(isLogged: Boolean) {
+        updateState { copy(userIsLoggedIn = isLogged) }
+    }
+
+    override fun onSaveIconClick() {
+        if (!state.value.userIsLoggedIn) {
+            updateState {
+                copy(
+                    showBottomSheet = true
+                )
+            }
+        }
+    }
+
+    override fun onRetryClick() {
+        updateState {
+            copy(
+                noInternetConnection = false,
+                isLoading = true,
+                error = null
+            )
+        }
+        getTvShowsByGenreId(genreId)
+    }
+
+    override fun onBottomSheetDismiss() {
+        updateState { copy(showBottomSheet = false) }
+    }
+
+    override fun onLoginButtonClick() {
+        updateState { copy(showBottomSheet = false) }
+        emitEffect(GenreTvShowsEffects.NavigateToLogin)
+    }
+
+    override fun onBackClick() {
+        emitEffect(GenreTvShowsEffects.NavigateBack)
+    }
+
+    override fun onTvShowClick(id: Int) {
+        emitEffect(GenreTvShowsEffects.NavigateToTvShowDetails(id))
+    }
+
+    private fun getTvShowsByGenreId(genreId: Int) {
+        tryToCollect(
+            block = { loadTvShowsByGenreId(genreId) },
+            onCollect = ::onCollectTvShowsByGenreId,
+            //     onError = ::onGetShowsByGeneraIdFailed
+        )
+    }
+
+    private fun onCollectTvShowsByGenreId(tvShows: PagingData<TvShowUiModel>) {
+        updateState {
+            copy(
+                title = genreName,
+                genreid = genreId,
+                tvShows = flowOf(tvShows),
+                isLoading = false
+            )
+        }
+    }
+
+    private fun onGetShowsByGeneraIdFailed(exception: NovixAppException) {
+        if (exception is NoNetworkException) {
+            updateState { copy(noInternetConnection = true, isLoading = false, error = null) }
+        } else {
+            updateState { copy(error = exception.message, isLoading = false) }
+        }
+    }
+
+    private fun loadTvShowsByGenreId(genreId: Int) = createPagingFlow(
+        pagingSourceFactory = { createTvShowsPagingDataSource(genreId) },
+        mapper = TvShow::toUiState
+    )
+
+    private fun createTvShowsPagingDataSource(
+        genreId: Int,
+    ): PagingSource<Int, TvShow> {
+        return BasePagingSource { page ->
+            manageTvShowUseCase.getTvShowsByGenre(
+                genreId = genreId, page = page
+            )
+        }
+    }
+}
