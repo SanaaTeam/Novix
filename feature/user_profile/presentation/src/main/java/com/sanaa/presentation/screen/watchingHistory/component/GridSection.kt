3c666f4ffa047497d86c8194f4ed9086344f323c
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-20 05:00:48 +0300

    display empty state for watching history

--- feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/watchingHistory/component/GridSection.kt
+++ feature/user_profile/presentation/src/main/java/com/sanaa/presentation/screen/watchingHistory/component/GridSection.kt
@@ -3,13 +3,16 @@ package com.sanaa.presentation.screen.watchingHistory.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.design_system.component.chips.ToggleableChip
+import com.sanaa.designsystem.design_system.component.screen_state_content.EmptyStateComponent
 import com.sanaa.feature.userprofile.presentation.R
 import com.sanaa.presentation.model.MediaItemUiModel
 import com.sanaa.presentation.screen.watchingHistory.GenreState
@@ -49,10 +52,21 @@ fun GridSection(
                 )
             }
         }
-        WatchingHistoryGrid(
-            mediaList = mediaList,
-            onMediaClick = onMediaClick,
-            onSaveIconClick = onSaveIconClick,
-        )
+
+        if (mediaList.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                EmptyStateComponent(messageText = stringResource(R.string.empty_watching_history))
+            }
+        } else {
+            WatchingHistoryGrid(
+                mediaList = mediaList,
+                onMediaClick = onMediaClick,
+                onSaveIconClick = onSaveIconClick,
+            )
+        }
     }
 }
