fcf5e477ff503735b3177378016a1531f1faed1c
Radwa85 <radwa.mohamed5033@gmail.com>
2025-08-20 06:01:11 +0300

    add empty state for watching history at home

--- feature/home/presentation/src/main/java/com/sanaa/presentation/components/MediaListSectionContent.kt
+++ feature/home/presentation/src/main/java/com/sanaa/presentation/components/MediaListSectionContent.kt
@@ -3,17 +3,19 @@ package com.sanaa.presentation.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.sanaa.designsystem.design_system.component.chips.ToggleableChip
+import com.sanaa.designsystem.design_system.component.screen_state_content.EmptyStateComponent
 import com.sanaa.feature.home.presentation.R
 import com.sanaa.presentation.state.GenreUiState
 import com.sanaa.presentation.state.MediaItemUiState
-
 @Composable
 fun MediaListSectionContent(
     genres: List<GenreUiState>,
@@ -51,11 +53,21 @@ fun MediaListSectionContent(
                 )
             }
         }
-        MediaListGrid(
-            mediaList = mediaList,
-            onMediaClick = onMediaClick,
-            onSaveIconClick = onSaveIconClick,
-            isScrollEnabled = isScrollEnabled,
-        )
+        if (mediaList.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                EmptyStateComponent(messageText = stringResource(R.string.empty_watching_history))
+            }
+        } else {
+            MediaListGrid(
+                mediaList = mediaList,
+                onMediaClick = onMediaClick,
+                onSaveIconClick = onSaveIconClick,
+                isScrollEnabled = isScrollEnabled,
+            )
+        }
     }
 }
